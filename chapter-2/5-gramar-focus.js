8c9e8d495385455d7c98b5233bebaa1bef3c88b2
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-05 04:18:04 -0300

    ingles chapter 2 | 5 grammar focus

---
+++ chapter-2/5-gramar-focus.js
@@ -0,0 +1,31 @@
+// GRAMAR FOCUS
+
+// It's = It is
+// They're = They are
+
+/*  This is a laptop
+    What's this?        It's a flash drive.
+
+    These are laptops.  What are these?
+                        They're flash drives.
+
+*/
+
+/* Complete these conversations. Then practice with a partner
+
+1.  A: What | [ are these ] ?
+    B: | [ They're keys ]
+
+2.  A: What | [ are these ] ?
+    B: | [ They're backpacks ]
+
+3.  A: What | [ 's this ] ?
+    B: [ it's an umbrella ]
+
+4.  A: What | [ 's this ] ?
+    B: | [ it's a sunglass ]
+
+5.  A: What | [ 's this ] ?
+    B: | [ it's a wallet ]
+
+*/
