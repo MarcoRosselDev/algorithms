9edbc759f391b450d9c01cea5036cddd60013407
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-05 00:25:44 -0400

    ingles chapter 7 | repass chapter 1

---
+++ chapter-7/chapter-7.js
@@ -0,0 +1,6 @@
+/* Does it have a view ?  
+
+students will be able to discuss furniture, appliances, and dream homes using statements  with there is/there are.
+
+los estudiantes podran hablar sobre electrodomesticos y casas de ensuegnos usando afirmaciones con is-->es | there --> ella
+*/
