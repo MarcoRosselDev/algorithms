c735d3e0dce624766e6bfc6a04d29bddef711201
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-05 01:48:30 -0400

    ingles chapter 1 | 12 listening

---
+++ chapter-1/12-Listening.js
@@ -0,0 +1,8 @@
+/* 12-Listening     | Contact Information
+
+Ben Durant      718-555-8241        bendurant@cup.org
+Cara Ruiz       247-555-7645        carar@cambridge.org
+Andrea Clark    212-645-5960        aclark134@cup.org
+Akira Hayashi   917-555-2807        akinah@cambridge.org
+
+*/
