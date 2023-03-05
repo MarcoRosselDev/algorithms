e466d7da7b314eb9c887101ea72cbc71c45cfed6
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-04 23:45:59 -0300

    ingles chapter 2 | articles

---
+++ chapter-2/2-articles.js
@@ -0,0 +1,15 @@
+// ARTICLES | Classroom aobjects
+
+/* A Liste. Compolete the sentences with a or an.
+
+1.  This is a book.
+2.  This is an English book.
+3.  This is an eraser.
+4.  This is a notebook.
+5.  This is a pen.
+6.  This is a clok.
+
+---------si termina en vocal aplicamos [an]
+---------si temrina en consonante aplicamos [a]
+
+*/
