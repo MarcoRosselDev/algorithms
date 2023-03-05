bc2f178cfab331e7ab437b4bedb1fd9a46e7b96a
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-05 00:42:09 -0400

    ingles chapter 1 | 9 grammar focus

---
+++ chapter-1/9-grammar-focus.js
@@ -0,0 +1,17 @@
+/* grammar focus ---- | Enfoque gramatical */
+
+/* The verb be
+
+    I'm     Lena Garza.
+    You're  in my class.
+    She's   over there. (Andrea is over there.)
+    He's    in our class. (Ben is in our class.)
+    It's    Garza. (My last name is Garza.)
+
+    Are you Andrea Clark?       |   I'm = I am
+    Yes, I am. (!== Yes, I'm)   |   You're = You are
+    No, I'm not.                |   He's = He is
+    How are you?                |   It's = It is
+    I'm fine, thenks.
+
+    */
--- chapter-7/chapter-7.js
+++ chapter-7/chapter-7.js
@@ -1,6 +1,6 @@
 /* Does it have a view ?  
 
-students will be able to discuss furniture, appliances, and dream homes using statements  with there is/there are.
+students will be able to discuss furniture, appliances, and dr-eam homes using statements  with there is/there are.
 
 los estudiantes podran hablar sobre electrodomesticos y casas de ensuegnos usando afirmaciones con is-->es | there --> ella
 */
