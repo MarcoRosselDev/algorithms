0122497d5393c896b935904421cf1647f6e11226
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-06 04:08:30 -0300

    chapter 3 | 8 grammar focus

---
+++ chapter-3/8-grammar-focus.js
@@ -0,0 +1,32 @@
+// GRAMMAR FOCUS
+
+/* A    Complete the conversation with Wh-questions.
+
+1-  A- Look! Who's that
+    B- Oh, she's a new student.
+    A- What's her name ?
+    B- I think her name is Yoon-jin.
+    A- Yoo-jin?. Where is she from ?
+    B- She's from South Korea.
+
+2-  A- Hi, Brittany. How are you today ?
+    B- I'm fine, thanks. My friend Leandro is here this week - from Argentina.
+    A- Oh, cool. What's he like ?
+    B- He's really friendly.
+    A- How old is he ?
+    B- He's twenty-five years old.
+
+3-  A- Azra , Where are you from ?
+    B- I'm from Turkey. From Ankara.
+    A- What's Ankara like ?
+    B- Well, Ankara is the capital of Turkey. It's very old.
+    A- What's your last name ?
+    B- My last name is Ganim.
+
+4-  A- Good morning, Luke. How are you today ?
+    B- I'm great, thanks.
+    A- Cool, who are they ?
+    B- They're  my friends from school.
+    A- Where are they from
+    B- They're from Miami, like me.
+*/
