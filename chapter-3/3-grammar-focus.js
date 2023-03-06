e1043f8e7726e151c0b4029fa6d6b2c0517baff7
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-06 02:59:23 -0300

    chapter 3 | 3 grammar focus

---
+++ chapter-3/3-grammar-focus.js
@@ -0,0 +1,25 @@
+// GRAMMAR FOCUS
+
+/* A    Complete the conversations.
+
+1.  A- Are Diana and Mario from Ecuador ?
+    B- No, they're not. They're from Mexico.
+    A- Are you from Mexico, too ?
+    B- No, I'm not. I'm from Colombia.
+    A- So, Is your first language Spanish ?
+    B- Yes, it is.
+
+2.  A- Is Meera from England ?
+    B- No, she's not. She's from Australia.
+    A- Is she from Sydney ?
+    B- Yes, she is. But her parents are from India.
+       They're not from Australia originally.
+    A- Is meera's first language Hindi ?
+    B- No, It's not. It's English.
+
+3.  A- Ji-hye, are you and Kwang-ho froma South Korea ?
+    B- Yes, we are.
+    A- And are you from Seoul ?
+    B- No, we're not. We're from Busan.
+
+*/
