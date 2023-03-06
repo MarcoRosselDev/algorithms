51df1ccb7fcdb7c06f4d7354caea8d67279a74da
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-06 01:02:56 -0300

    chapter 2 | 8 grammar focus A

--- chapter-2/8-grammar-focus.js
+++ chapter-2/8-grammar-focus.js
@@ -21,6 +21,21 @@
         A: -  Are   -   these your car keys ?
         B" Yes, -  they  -   are. Thanks!.
 
-    
+    2.  A: Where    -  Are  -   my glasses ?
+        B: Are  -  they -   your glasses ?
+        A: No, they're  -  not  -   .
+        B: Look!    -  Are  -   they in your pocket ?
+        A: Yes, -  they are  -   .Thanks!
+
+    3.  A: Where    -  are  -   your headphones ?
+        B: -  They're  -   on the table.
+        A: No.-  They're  -   not. They're my headphones!
+        B: You're right. My headphones -  Are  -   in my backpack.
+
+    4.  A: -  Is  -   this my umbrella ?
+        B: No, -  it's  -   not. It's my umbrella.
+        A: Sorry.-   This  -   is my umbrella ?
+        B: -  It's  -   on your chair.
+        A: Oh, you're right!
 
 */
