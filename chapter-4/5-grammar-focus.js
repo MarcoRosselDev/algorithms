f07df74f7d4412a676704f2c310bd7d7934755d2
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-07 01:33:30 -0300

    chapter 4 | 5 grammar focus - A

--- chapter-4/5-grammar-focus.js
+++ chapter-4/5-grammar-focus.js
@@ -3,7 +3,7 @@
 /* Possessives |
 
 Adjetives--------------------------------
-
+    el objeto se nombra por primera vez
             my
             your
 These are   his    shoes.
@@ -12,7 +12,7 @@ These are   his    shoes.
             their
 
 Pronouns----------------------------------
-
+    ya se nombro el objeto
                 mine
                 yours
 These shoes are his
@@ -28,6 +28,18 @@ Alex's coat.
 Whose tie is this ?         It's Greg's.
 Whose shoes are these ?     They're Taylor's
 
+Diferencias de adjetivo y pronombres---->
+    ¿Y la tuya? La diferencia entre adjetivos y pronombres estriba en que los primeros acompañan a un sustantivo (aparece antes del nombre común o nombre propio), mientras que los segundos van en su lugar (lo remplazan), de ahí que exista forma neutra en los pronombres y no en los adjetivos.
+
 */
 
-/*  */
+/* A Compolete the conversation with the correct words in parenteses.
+
+1.  A-  This isn't my (my\mine) raincoat.
+        Is it _yours_(your\yours)
+    B-  No, it's not _mine_(my\mine).
+        Ask Emma, Maybe it's _hers_(her\hers)
+    
+2.  A-  Hey! These aren't _our_(our\ours) sneakers!
+    B-  You're right._ours_(our\ours) are over there
+*/
