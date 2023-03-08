fbf03fb0762f8fb5b9c254d9f155c544d03cb4bf
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-08 13:36:21 -0300

    chapter 6|4-grammar focus B

--- chapter-6/4-grammar-focus.js
+++ chapter-6/4-grammar-focus.js
@@ -5,4 +5,9 @@ My wife and I _work_(work/ works) near here, so we _walk_(walk/ walks) to work.
 Our daughter Megan _works_(work/works) downtown, so she _drives_(drive/drives) to work.
 Our son _doesn't_(don't/doesn't) drive.
 He _rides_(ride/rides) his bike to school.
+
+    B   My parents _live_(live/lives) in the city.
+    My mother _takes_(take/takes) the subway to work.
+    My father is retired, so he _doesn't_(don't/doesn't) work now.
+    He also _uses_(use/uses) public transportation, so they _don't_(don't/doesn't) need a car.
 */
