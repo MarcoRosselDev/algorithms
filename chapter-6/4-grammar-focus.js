657c5966215fa9b535dce40e9693a879078074ed
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-08 13:42:42 -0300

    chapter 6|4-grammar focus B 1

--- chapter-6/4-grammar-focus.js
+++ chapter-6/4-grammar-focus.js
@@ -1,13 +1,26 @@
 //  GRAMMAR FOCUS
 
-/* A    My family and I _live_(live/lives) in the suburbs.
-My wife and I _work_(work/ works) near here, so we _walk_(walk/ walks) to work.
-Our daughter Megan _works_(work/works) downtown, so she _drives_(drive/drives) to work.
-Our son _doesn't_(don't/doesn't) drive.
-He _rides_(ride/rides) his bike to school.
+/* A  Tom Mitchell is talking about his family. 
+    Complete the sentences with the correct verb forms.
 
-    B   My parents _live_(live/lives) in the city.
+    1.My family and I _live_(live/lives) in the suburbs.
+    My wife and I _work_(work/ works) near here, so we _walk_(walk/ walks) to work.
+    Our daughter Megan _works_(work/works) downtown, so she _drives_(drive/drives) to work.
+    Our son _doesn't_(don't/doesn't) drive.
+    He _rides_(ride/rides) his bike to school.
+
+    2.My parents _live_(live/lives) in the city.
     My mother _takes_(take/takes) the subway to work.
     My father is retired, so he _doesn't_(don't/doesn't) work now.
     He also _uses_(use/uses) public transportation, so they _don't_(don't/doesn't) need a car.
+*/
+
+/*  B   Yuto is talding about his family and his friend Austin.
+    Complete thes sentences.
+    
+    1.My parents __(have/has) a house in the suburbs.
+    My mom and dad __(go/goes) downtown to work.
+    My parents are very busy, so I __(do/does) a lot of work at home.
+
+
 */
