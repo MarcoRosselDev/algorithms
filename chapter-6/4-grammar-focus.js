cfa5b396daa1af77c3e34fad0e6ba5e9e831a1a3
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-08 13:31:03 -0300

    chapter 6|4-grammar focus

---
+++ chapter-6/4-grammar-focus.js
@@ -0,0 +1,8 @@
+//  GRAMMAR FOCUS
+
+/* A    My family and I _live_(live/lives) in the suburbs.
+My wife and I _work_(work/ works) near here, so we _walk_(walk/ walks) to work.
+Our daughter Megan _works_(work/works) downtown, so she _drives_(drive/drives) to work.
+Our son _doesn't_(don't/doesn't) drive.
+He _rides_(ride/rides) his bike to school.
+*/
