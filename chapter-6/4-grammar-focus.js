3c04a914885b3358e0833ddc175a9240f0ac4313
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-08 13:54:32 -0300

    chapter 6|4-grammar focus B 2,3

--- chapter-6/4-grammar-focus.js
+++ chapter-6/4-grammar-focus.js
@@ -18,9 +18,16 @@
 /*  B   Yuto is talding about his family and his friend Austin.
     Complete thes sentences.
     
-    1.My parents __(have/has) a house in the suburbs.
-    My mom and dad __(go/goes) downtown to work.
-    My parents are very busy, so I __(do/does) a lot of work at home.
+    1.My parents _have_(have/has) a house in the suburbs.
+    My mom and dad _go_(go/goes) downtown to work.
+    My parents are very busy, so I _do_(do/does) a lot of work at home.
 
+    2.My brother doesn't live with us.
+    He _has_(have/has) an apartment in the city.
+    He _goes_(go/goes) to school all day, and he _does_(do/does) his homework at night.
+
+    3.I _have_(have/has) a new firend.
+    His name is Austin.
+    We _go_(go/goes) to the same school, and sometimes we _do_(do/does) our homework together.
 
 */
