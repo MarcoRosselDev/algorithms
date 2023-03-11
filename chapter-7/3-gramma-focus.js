7b439b287d3efa9b62067fb15cf9f3f98f69ff97
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-11 11:59:50 -0300

    chapter 7 | 3 grammar focus

---
+++ chapter-7/3-gramma-focus.js
@@ -0,0 +1,20 @@
+/* Complete the conversation, Then practice with a partner.
+
+Julia   _Do_ you _live_ in an apartment? +
+Ethan   No, I _don't_, I _live_ in a house. +
+Julia   _Does_ it _have_ a yard? +
+Ethan   yes, it _does_. +
+Julia   Taht sounds nice. _Do_ you _live_ alone ? +
+
+Ethan   No, I _don't_. I _live_ with my family. +
+Julia   _Do_ you _have_ any brothers or sisters ? +
+
+Ethan   Yes, I _do_. I _have_ four sisters. + 
+Julia   Really? _Does_ your house _have_ many bedrooms ? +
+
+Ethan   Yes, it _does_. It _have_ four. x -----> Yes, it does. It has four.
+Julia   _Do_ you _have_ your own bedroom?
+
+Ethan   Yes, I _do_. I'm really lucky.
+
+*/
