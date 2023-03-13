9d074f177d2eac03a1db5f98aa81db03a3ecf8ee
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-12 21:52:57 -0300

    grammar plus | unit 1 | 1

---
+++ grammar-plus/chapter-1.js
@@ -0,0 +1,32 @@
+/* My, your, his, her
+
+    use his with males and her with females:
+        His name is Travis. -----> Not: Her name is Yravis
+        Her name is Nicole. -----> Not: His name is Nicole.
+
+*/
+
+// Complete the voncersation with my, your, his , or her.
+
+/* 1
+
+    A- Hello. My name is Carlos.
+    B- Hi, Carlos. What's your last name ?
+    A- It's Gonzales.
+    B- How do you spell your last name ? is it G-O-N-Z-A-L-E-Z ?
+    A- No, it's G-O-N-Z-A-L-E-S. And what's your name ?
+    B- My name is Bill Powers. Nice to meet you.
+
+*/
+
+/* 2
+
+    A- What's Ms. Robinson's first name?
+    B- her first name is Elizabeth. Her nickname is Liz.
+    A- I'm sorry. What's her first name again ?
+    B- It's Elizabeth. And what's Mr. Weber's first name ?
+    A- His first name is Peter.
+    B- That's right. And his nickname is Pete.
+    A- That's right, too.
+
+*/
