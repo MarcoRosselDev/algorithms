afe576d1469241d850aa8b03d4c297f9dc7a03e7
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-13 17:19:04 -0300

    grammar plus | unit 2 | 1

---
+++ grammar-plus/chapter-2.js
@@ -0,0 +1,16 @@
+/* 1    This/these; it/they; plurals
+
+- Don't use a contraction with What + are; What are these ? --->xxxxx not; What're these? xxxxxx
+- Use this with singular nouns: This is a laptop. Use these with plural nouns: These are flash drives.
+
+Choose the correct words.
+
+1-  A- What are these ?
+    B- They're my flash drives.
+
+2-  A- What's this ?
+    B- It's a cell phone.
+
+3-  A- What's this?
+    B- It's an English book.
+*/
