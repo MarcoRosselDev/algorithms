a241622e438aeb362c14ab6aabe1a386952889fe
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-13 17:10:28 -0300

    grammar plus | unit 1 | 2

--- grammar-plus/chapter-1.js
+++ grammar-plus/chapter-1.js
@@ -6,9 +6,9 @@
 
 */
 
-// Complete the voncersation with my, your, his , or her.
+/* 1    Complete the voncersation with my, your, his , or her.
 
-/* 1
+    1
 
     A- Hello. My name is Carlos.
     B- Hi, Carlos. What's your last name ?
@@ -17,9 +17,7 @@
     A- No, it's G-O-N-Z-A-L-E-S. And what's your name ?
     B- My name is Bill Powers. Nice to meet you.
 
-*/
-
-/* 2
+    2
 
     A- What's Ms. Robinson's first name?
     B- her first name is Elizabeth. Her nickname is Liz.
@@ -28,5 +26,21 @@
     A- His first name is Peter.
     B- That's right. And his nickname is Pete.
     A- That's right, too.
+
+*/
+
+/* 2    The verb be
+
+    complete the conversations with the words in the box
+
+1   A- Excuse me Are you Layla Moore ?
+    B- No, I'm not. she's over there.
+    A- OK. Thanks.
+
+2   A- Hi. Are you Layla Moore ?
+    B- Yes, I am.
+    A- Nice to meet you. I'm Sergio Oliveira.
+        You're in my english class.
+    B- Yes, I am. It's nice to meet you too, Sergio.
 
 */
