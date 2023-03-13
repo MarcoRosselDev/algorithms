4cece7f6615bdbebb443aec5fba07e7f9372d3a9
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-13 17:25:28 -0300

    grammar plus | unit 2 | 2

--- grammar-plus/chapter-2.js
+++ grammar-plus/chapter-2.js
@@ -14,3 +14,25 @@ Choose the correct words.
 3-  A- What's this?
     B- It's an English book.
 */
+
+/* 2 Yes/No and whare questions with be
+
+A   Match the questions with thes answers.
+
+1.  Is that your wallet?        c.  Oh, yes, it its!
+2.  Are these your glasses?     e.  No, they're not.
+3.  Where are my keys?          a.  They're in your backpack.
+4.  Is this your bicycle?       b.  No, it's not.
+5.  Where's your tablet ?       d.  It's on my desk.
+
+B   Complete the conversation. Use the words in the box.
+
+A-  Where's my dictionary?
+B-  I don't know. Is it in your backpack?
+A-  No, it's not.
+B-  Is this your dictionary ?
+A-  Yes, it is, Tahnks! Now, where are my glasses?
+B-  Are they on your desk ?
+A-  Yes, they are. Thank you!
+
+*/
