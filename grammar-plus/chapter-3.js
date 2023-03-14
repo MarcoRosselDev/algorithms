fbea687858647e6c341ec359527b8573a5d4f04e
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-14 13:45:38 -0300

    grammar plus | unit 3 | 1

---
+++ grammar-plus/chapter-3.js
@@ -0,0 +1,26 @@
+/* A Unscramble the words to write negative statments
+
+1-  Toronto is not the capital of Canada.
+2-  We're not from Buenos Aires
+3-  You and Ashley are not in my class
+4-  Korean is not my first lenguage
+5-  My mother is not from Italy
+6-  They are not my parents
+
+*/
+
+/* B Complete the conversations.
+
+1-  A- Are you and your firend from Costa Rica?
+    B- No, are not. We are from the Dominican Republic.
+
+2-  A- Is your first lenguage Spanish ?
+    B- Yes, It is. My parents are from Ecuador.
+
+3-  A- Are Nadia and Rayan Lebanese ?
+    B- Yes, they are. But they're in France now.
+
+4-  A- Are my friens and I late ?
+    B- No, you are not. You are early!
+
+*/
