69fb9833242206243bc035dc504a0b3a8e205bd2
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-14 23:35:51 -0300

    grammar plus | unit 4 | 2

--- grammar-plus/chapter-4.js
+++ grammar-plus/chapter-4.js
@@ -1,4 +1,4 @@
-/* possessives
+/* 1    possessives
 
 1   A- Whose jacket is this? Is it your, Ethan?
     B- No, it;s not mine. Ask Matt. I think it's his.
@@ -8,5 +8,15 @@
 
 3   A- Whose sweaters are these? Are they Rachel's?
     B- No. they're not her sweaters. But these shorts are hers
+
+*/
+
+/* 2    Present coninuous statements; conjuntions
+
+1- Mr. and Mrs. Liu are wearing green caps.
+2- It isn't snowing.
+3- I'm wearing a winter coat.
+4- You're wearing David's sunglasses.
+5- Ayumi isn't wearing a scarf.
 
 */
