5712cabbde16c48c390e817862a83818f75cbb76
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-15 15:12:51 -0300

    grammar plus | unit 5 | 2

--- grammar-plus/chapter-5.js
+++ grammar-plus/chapter-5.js
@@ -10,3 +10,31 @@
 8   It's midnight.                      It's twelve o'clock at night
 
 */
+
+/* 2    Present conitnuous Wh-questinos
+
+1   A- What's Matt doing?
+    B- He's swimming.
+    
+2   A- What are Jon and Megan doing?
+    B- They're shopping.
+
+3   A- What are you doing?
+    B- I am writing a message.
+
+4   A- What's Chris doing?
+    B- He's cooking dinner.
+
+5   A- What are you and Tyler doing?
+    B- Whe're watching a movie.
+
+6   A- What's Sara doing?
+    B- She's having pizza.
+
+7   A- What are you and Joseph doing?
+    B- Whe're studying for a test.
+
+8   A- What's Laura and Paulo doing?
+    B- They're chatting online.
+
+*/
