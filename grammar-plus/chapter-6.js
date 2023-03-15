4280f27d99f261ad7e95b5ff7fe50dd10122d360
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-15 18:30:15 -0300

    grammar plus | unit 6 | 2 A

--- grammar-plus/chapter-6.js
+++ grammar-plus/chapter-6.js
@@ -13,3 +13,21 @@ And me? well, I don't work far from our apartment, so I don't need a car or publ
 I ride my bike to work!
 
 */
+
+/* 2    simple present questions
+
+A Write questions to complete the conversations.
+
+1   A- Do you use public transportation?
+    B- Yes, I use public transportation.
+
+2   A- Does your family eat dinner at 5:00 ?
+    B- No, my family doesn't eat dinner at 5:00.
+
+3   A- Does your brother take the bus to work ?
+    B- No, my brother doesn't take the bus to work.
+
+4   A- Do you get up late on weekends ?
+    B- No, I don't get up late on weekends.
+
+*/
