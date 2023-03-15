1c2330deb56c34827c392019546dd0455b2dcfe6
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-14 23:33:16 -0300

    grammar plus | unit 4 | 1

---
+++ grammar-plus/chapter-4.js
@@ -0,0 +1,12 @@
+/* possessives
+
+1   A- Whose jacket is this? Is it your, Ethan?
+    B- No, it;s not mine. Ask Matt. I think it's his.
+
+2   A- These aren't my bloves. Are they yours ?
+    B- No, they're not mine. Maybe they are Young-min's.
+
+3   A- Whose sweaters are these? Are they Rachel's?
+    B- No. they're not her sweaters. But these shorts are hers
+
+*/
