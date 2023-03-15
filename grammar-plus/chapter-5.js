a51656d70cf86b26301ace2fdc68569825b38e99
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-15 15:05:20 -0300

    grammar plus | unit 5 | 1

---
+++ grammar-plus/chapter-5.js
@@ -0,0 +1,12 @@
+/* 1    write each sentence in different way
+
+1   It's a quarter to four              It's three forty-five
+2   It's 7:00 P.M                       It's seven in the evening
+3   It's six-fifteen                    It's a quarter after six
+4   It's 10 o'clock at night.           It's ten P.M
+5   It's three-oh-five.                 It's five after three.
+6   It's twenty-five to eleven.         It's ten theery-five.
+7   It's one o'clock in the morning.    It's 1:00 A.M
+8   It's midnight.                      It's twelve o'clock at night
+
+*/
