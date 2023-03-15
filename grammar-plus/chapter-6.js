e50df9d2029c8ad4486c0886574de01a3feaefd1
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-15 18:22:59 -0300

    grammar plus | unit 6 | 1

---
+++ grammar-plus/chapter-6.js
@@ -0,0 +1,15 @@
+/* 1    simple presents statements
+
+Elena is talking about her family.
+Complete the sentences with the correct form of the verbs in parentheses.
+
+My family and I live in the city.
+We have an apartment on First Avenue.
+My sister goes to school near our apartment, so she walks to school.
+My father works in the suburbs, so he drives to his job.
+My mother uses public transportation - she takes the bus to her office downtowen.
+She has a new job, but she dosn't like much.
+And me? well, I don't work far from our apartment, so I don't need a car or public transportation. 
+I ride my bike to work!
+
+*/
