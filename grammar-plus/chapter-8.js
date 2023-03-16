db62be3e230a7ce6d07547b2ed913998ee5be339
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-15 23:23:54 -0300

    grammar plus | unit 8 | 1

---
+++ grammar-plus/chapter-8.js
@@ -0,0 +1,27 @@
+/* 1    simple present Wh-questions
+
+1
+A   What does your husband do ?
+B   My husband ? Oh, he's a nurse.
+A   Really ? where does he work ?
+B   He works at Mercy Hospital.
+
+2
+A   Where do you work ?
+B   I work in a restaurant.
+A   Nice! What do you do ?
+B   I'm a chef.
+
+3
+A   How do you like your job ?
+B   My job ? I don't really like it very much.
+A   That's too bad. What do you do ?
+B   I'm a cashier. I work at a clothing store.
+
+4
+A   What do your brother and sister do ?
+B   My brother is a doctor, and my suster is a lawyer.
+A   How do they like their jobs.
+B   They work very hard, but they love theri jobs.
+
+*/
