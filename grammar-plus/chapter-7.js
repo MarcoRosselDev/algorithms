ea9634e935cfdebaeb346e41cf5feba827fbb132
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-15 22:51:37 -0300

    grammar plus | unit 7 | 1

---
+++ grammar-plus/chapter-7.js
@@ -0,0 +1,17 @@
+/* 1    Simple present short answers
+
+Choose the correct words.
+
+A   Does your family live in an apartment ?
+B   No, we don't. We have a house.
+A   That's nice. Does your house have two floors ?
+B   Yes, it does. It has four rooms on the first floor. 
+    And we have three bedrooms and a bathroom on the second floor.
+A   And do you and your family have a yard ?
+B   Yes, we do. And how about you, Tim ?
+    Do you live in a house, too?
+A   No, I don't. My wife and I have a small apartment in the city.
+B   Oh. Do you like the city ?
+A   Yes, I do. But my wife doesn't.
+
+*/
