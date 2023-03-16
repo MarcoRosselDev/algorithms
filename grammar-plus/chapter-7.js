6526b327483bd0d53f60089c6798c2b0a243d9e9
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-15 23:01:49 -0300

    grammar plus | unit 7 | 2

--- grammar-plus/chapter-7.js
+++ grammar-plus/chapter-7.js
@@ -15,3 +15,30 @@ B   Oh. Do you like the city ?
 A   Yes, I do. But my wife doesn't.
 
 */
+
+/* 2    There is | There are
+
+Read the information about the Perez family's new house.
+Write sentences with the phrases in the box.
+
+1   A living room ?                 yes
+2   A dining room ?                 No
+3   A microwave in the kitchen ?    No
+4   A table in the kitchen ?        Yes
+5   Curtains on the windows ?       Yes
+6   Rugs on the floors ?            No
+7   Closets in the bedrooms ?       Yes
+8   Bookcases in the bedrooms ?     No.
+
+----------------------------------------------
+
+1   There's a living room.
+2   There isn't a dining room.
+3   There isn't a microwave in the kitchen.
+4   There's a table in the  kitchen.
+5   There are some curtains on the windows.
+6   There aren't any rugs on the floors.
+7   There are closets in the bedrooms.
+8   There aren't any bookcases in the bedroom.
+
+*/
