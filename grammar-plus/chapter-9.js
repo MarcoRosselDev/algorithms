0cee031e02449eb254de9b6167c275cadea6ad73
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-16 23:05:15 -0300

    grammar plus | unit 9 | 1  B

--- grammar-plus/chapter-9.js
+++ grammar-plus/chapter-9.js
@@ -1,5 +1,6 @@
-/* Complete the conversations with some or any.
+/* 1    Complete the conversations with some or any.
 
+1   -----------------------------------
 A   What do you want for lunch?
 B   Let's make some sandwiches.
 A   Good idea!  We have some bread. Do we have any cheese ?
@@ -10,5 +11,16 @@ A   Well, let's go to the store. We need some milk, too.
 B   Yes, we do. There's some lettuce here, and there are some tomatoes, too.
 A   Do we have any mayonnaise ? I love some mayonnaise on my snadwiches.
 B   Me, too. But there isn't any here. Let's buy some.
+
+2   -----------------------------------
+
+A   Let's make a big breakfast tomorrow morning.
+B   Good idea! What do we need ? Are there any eggs ?
+A   There are some but I think we need to buy some more.
+B   OK. And let's get some cereal, too. We dont't have any. and I love ceral for breakfast.
+A   Me, too. Do we have any blueberry yogurt?
+B   Yes, there's some in the refrigerator.
+A   Great! So we don't need to buy any at the store.
+B   That's right. Just eggs and cereal!
 
 */
