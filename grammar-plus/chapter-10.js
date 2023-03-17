07eef83586b3523a5a75157732d0f9d35261c49c
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-17 02:22:05 -0300

    grammar plus | unit 10 | 1

---
+++ grammar-plus/chapter-10.js
@@ -0,0 +1,13 @@
+/* 1    Simple present Wh-questions
+
+Complete the questions with the correct question word and do or does.
+Then match the questions with the answer.
+
+1   What sport do you like ?            Soccer. I love to watch my team.
+2   Who you go to games with ?          My father and my two brothers.
+3   How often does your team play ?     Once or twice a month.
+4   Where do they play ?                At the Olympic Stadium.
+5   When do they play ?                 On Sunday afternoons.
+6   What time do the games start ?      Usually at four o'clock.
+
+*/
