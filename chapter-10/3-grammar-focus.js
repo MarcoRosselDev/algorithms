c950743d7e4fbc0c477cfbadfe43562ec8654df7
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-16 23:40:41 -0300

    chapter 10 | 3 grammar focus

---
+++ chapter-10/3-grammar-focus.js
@@ -0,0 +1,24 @@
+/* A    Complete the conversation with the correct Wh-question words.
+
+1
+
+A   How often do you go bike riding ?
+B   Oh, about once or twice a week.
+A   I love to go bike riding. I go every sunday.
+B   Really ? what time do you go ?
+A   Usually at about ten in the morning.
+B   Oh, yeah? who do you go with ?
+A   A group of friends. Come with us next time!
+
+2
+
+A   I watch sports on TV every weekend.
+B   Really ? What sports do you like to watch ?
+A   Soccer. It's my favorite !
+B   When do you usually watch soccer ?
+A   In the evening or on weekends.
+B   And where do you usually watch ir ?
+    At home ?
+A   No, at my brother's house. He has a home theater!
+
+*/
