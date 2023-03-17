d5f9ecf1e76275815576f905645d5fb17b68dd33
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-16 23:48:55 -0300

    chapter 10 | 3 grammar focus B

--- chapter-10/3-grammar-focus.js
+++ chapter-10/3-grammar-focus.js
@@ -22,3 +22,18 @@ B   And where do you usually watch ir ?
 A   No, at my brother's house. He has a home theater!
 
 */
+
+/* B    Compete the conversation with Wh-questions.
+
+A   What sport s do you like ?
+B   I like a lot of sports, but I really love volleyball!
+A   Who do you play volleyball with ? 
+B   I usually play with my sister and some friends.
+A   When do you practice ?
+B   We practice on Saturdays.
+A   What time do you start ?
+B   We start at about noon.
+A   Where do you play ?
+B   We usually play at a sports club, but sometimes we play on the beach.
+
+*/
