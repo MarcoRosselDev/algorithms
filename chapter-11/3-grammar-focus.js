03c2dc05633b710711ab1a12c1fca05674bf5196
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-17 17:14:10 -0300

    chapter 11 | 3 grammar focus A

---
+++ chapter-11/3-grammar-focus.js
@@ -0,0 +1,15 @@
+/* 3    going to
+
+A   What are these people going to do this weekend ?
+    write sentences.
+
+1   He's going to go biking.
+2   He's going to swim.
+3   She's going to shopping.
+4   They're going to watch a football game.
+5   She's going to work.
+6   She's going to dance.
+7   He's going to play drums.
+8   They're going to go to the movie/ see a movie
+
+*/
