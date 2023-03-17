2520846a040177e713bfe4987515a15c45ba293b
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-16 22:10:16 -0300

    chapter 9 | 7 grammar focus A

--- chapter-9/7-grammar-focus.js
+++ chapter-9/7-grammar-focus.js
@@ -10,3 +10,14 @@ never       nunca.
 sometimes I eat breackfast.
 
 */
+
+/* A    Put the adverbs in the correct places.
+
+A   Do you usually have a big breakfast ?
+B   Well, on Sunday I always have a big breakfast with my friends.
+A   Do you ever eat breakfast at work on weekdays ?
+B   Yes, I sometimes have breakfast at the officec restaurant.
+A   Do you often eat breakfast at you desk ?
+B   No, I hardly ever eat breakfast at my desk.
+
+*/
