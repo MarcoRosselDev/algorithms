f11d4a7ab577b5099ee383cee968a99bbac36ae8
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-16 22:52:05 -0300

    grammar plus | unit 9 | 1  A

---
+++ grammar-plus/chapter-9.js
@@ -0,0 +1,14 @@
+/* Complete the conversations with some or any.
+
+A   What do you want for lunch?
+B   Let's make some sandwiches.
+A   Good idea!  We have some bread. Do we have any cheese ?
+B   Yes, I think there's some in the refrigerator. Let me see...
+    No, I don't se any.
+A   Well, let's go to the store. We need some milk, too.
+    And do we have any cheese ?
+B   Yes, we do. There's some lettuce here, and there are some tomatoes, too.
+A   Do we have any mayonnaise ? I love some mayonnaise on my snadwiches.
+B   Me, too. But there isn't any here. Let's buy some.
+
+*/
