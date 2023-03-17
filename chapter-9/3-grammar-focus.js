f2fd933eb6b2f968010446e181ac397d7fc40305
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-16 21:43:12 -0300

    chapter 9 | 3 grammar focus

---
+++ chapter-9/3-grammar-focus.js
@@ -0,0 +1,18 @@
+/*  GRAMMAR FOCUS
+
+    some === afirmative
+    any  === negative and questions
+
+    A   Complete the conversation with some or any
+
+    Keith   Oh, they don't have any potato salad.
+    Jane    But we have lots of potatoes at homee. Let's make some.
+    Keith   Great. Do we have any mayonnaise ?
+    Jane    No. We need to buy some. And we need some onions.
+    Keith   Oh, no, I don't want any onions in the salad.
+    Jane    IK, don't worry. Let's get some celery, then.
+    Keith   No, I don't want any celery. I have an idea.
+            Let's put some apples in it.
+    Jane    Are you serious ? Apples in potato salad ? Well, OK...
+
+*/
