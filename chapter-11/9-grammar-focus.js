b0f7793e344b741a3fc5d99d5e83307d71a2b935
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-17 17:20:21 -0300

    chapter 11 | 9\ grammar focus A

---
+++ chapter-11/9-grammar-focus.js
@@ -0,0 +1,18 @@
+/* 9    Complete these conversations with the correct form of be going to.
+
+1   A   Where are you going to spend Thanksgiving ?
+    B   My parents and I are going to visit my grandparents.
+
+2   A   Who are you going to invite to your Independence Day picnic ?
+    B   I'm going to ask my family and some good friends.
+
+3   A   What are you going to do for New Year's day ?
+    B   I don't know. I'm not going to do anything special.
+
+4   A   How are your children going to celebrate Halloween ?
+    B   They're going to go to their school's party.
+
+5   A   What si your sister going to do for Valintine's Day ?
+    B   Her boyfriend is going to take her out to dinner.
+
+*/
