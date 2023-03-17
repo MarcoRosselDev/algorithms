d5a2ee3b3fb07344750306aa56915250e11b2c28
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-17 02:46:51 -0300

    grammar plus | unit 10 | 2 B

--- grammar-plus/chapter-10.js
+++ grammar-plus/chapter-10.js
@@ -23,6 +23,16 @@ A   Write sentences about the things people can and can't do.
     4   Alicia can snowboard, but she can't ice-skate.
     5   Ben can take good photos, and he can edit videos.
     6   Corinne can't write poems, but she can tell good jokes.
-    
+
+B   Look at part A. 
+    Answer the questions.
+    Write short sentences.
+
+    1   Can Matt and Drew sing ?    No, they can't.
+    2   Who can tell good jokes ?   Corinne can.
+    3   Can Olivia drive a car ?    No, she can't.
+    4   Can Juan play the piano ?   Yes, he can.
+    5   Who can snowboard ?         Alicia can.
+    6   What can Matt do ?          He can act.
 
 */
