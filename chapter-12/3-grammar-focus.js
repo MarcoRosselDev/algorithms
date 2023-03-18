68e79b6f577d2719ede0070139944e64a006eefa
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-17 23:34:07 -0300

    chapter 12 | 3 grammar focus

---
+++ chapter-12/3-grammar-focus.js
@@ -0,0 +1,15 @@
+/* have + noun: feel + adjective
+
+What's the matter ?
+What's wrong ?
+    I have a stomachache.
+    I have a headach.
+    I have the flu.
+
+How are you ? 
+How do you feel ?
+    I feel sick.
+    I feel better.
+    I don't feel well.
+
+*/
