7d81db505f8d41d3dd7e6bae08d26ce6b23d82af
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-17 21:36:21 -0300

    grammar plus | unit 11 | 1 A

---
+++ grammar-plus/chapter-11.js
@@ -0,0 +1,15 @@
+/* The future with be going to
+
+A   Complete matthew's story. Use the correct form of be going to and the verbs in parentheses.
+
+Tomorrow is going to be a very exciting day. It's my birhday,
+and my friends and I are going to celebrate.
+In the morning, Stephen and I are going to drive to the beach.
+Our friend Rosa is going to meet us there.
+We're going to stay at the beach for a few hours.
+Then we're going to have lunch at my favorite restaurant.
+After lunch, Stephen is going to go to work, and Rosa and I are going to see a movie.
+After the movie, we are going to go to our firend Philip's house.
+He is going to cook his special homemade pizza for Rosa and me.
+
+*/
