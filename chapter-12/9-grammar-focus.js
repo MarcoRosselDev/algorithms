086e189cd3a486af6f0be2e4fcb9a5ac54e9c8b8
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-21 06:30:02 -0300

    chapter 12 | 9  grammar focus

---
+++ chapter-12/9-grammar-focus.js
@@ -0,0 +1,21 @@
+/* 9    Complete these enctences. Use the correct forms of the words in the box.
+
+call
+stay
+not go
+not drink
+see
+take
+not worry
+not eat
+
+1   Call a dentist.
+2   Don't worry too much.
+3   Take a hot bath.
+4   Don't go to school.
+5   Stay in bed.
+6   See a doctor.
+7   Don't drink coffe.
+8   Don't eat any candy.
+
+*/
