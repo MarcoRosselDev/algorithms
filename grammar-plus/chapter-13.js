e674708784e7a8ff94d3576d9ead722a2193213f
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-21 12:33:04 -0300

    grammar plus | unit 13 | 1

---
+++ grammar-plus/chapter-13.js
@@ -0,0 +1,14 @@
+/* 1    Choose the correct words.
+
+A   Excuse me. Is there a post office around here ?
+B   Yes, there is. It's on Maple Street.
+A   Where on Maple ?
+B   It's on the corner of Maple Street and Second Avenue.
+A   Next to Charlie's Restraurant ?
+B   Yes, that's right. It's across the street from the Windsor Hotel.
+A   Thanks. Oh, and where is the bank?
+B   It's on Oak Street - between the hospital and police station.
+A   Great. Thanks very much.
+B   You're welcome.
+
+*/
