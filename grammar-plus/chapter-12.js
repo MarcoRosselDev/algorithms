93f3126be3d046e9969d65aaf16d21564b6eccf5
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-21 07:01:27 -0300

    grammar plus | unit 12 | 1

---
+++ grammar-plus/chapter-12.js
@@ -0,0 +1,15 @@
+/* 1    Complete the conversation. Use the sentences in the box.
+
+a   Hi, Vanessa. How are you?
+b   I'm terrific, thanks. How about you?
+a   I feel awful, actually.
+b   Oh, no! What's the matter ?
+a   I think I have a fever.
+b   That's to bad. Do you have a headache ?
+a   Yes, I do. And I have a stomachache, too.
+b   Are you going to see a doctor ?
+a   Yes, I'm going to call my doctor in a few minutes.
+b   Well, feel better soon.
+a   Thanks.
+
+*/
