e290c5bea75812f8bd2b2a517454ffbd01ed122a
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-25 11:17:27 -0300

    grammar plus | unit 14 | 1

---
+++ grammar-plus/chapter-14.js
@@ -0,0 +1,20 @@
+/* 1    Maya wrote an email to a friend. Complete the sentences with the simple past form of the verbs in parentheses.
+
+Hi!
+I didn't do anything special this weekend, but I had a lot of fun.
+I didn't go out on Friday night. I stayed home.
+I cleaned my room and did the laundry.
+I helped my sister with her homework, and then we watched our favorite series.
+On Saturday, my friend Lori come over. 
+She needded some new shoes, so we took the bus downtowen to Todd's Shoes Store.
+We shopped at the and exercised. 
+We didn't ecercise very hard.
+I invited Lori for dinner, and my dad cooked hamburgers in the yard.
+After dinner, Lori and I talked and played video games.
+She didn't stay very late - my mother drove her home at around ten.
+On Sunday, my shole family visited my mother's best friend and her family.
+They have a swimming pool, so my sister and I went swimming all afternoon.
+Tell me about your wekkend !
+Maya.
+
+*/
