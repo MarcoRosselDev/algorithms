7a5ab1d88c6b3dc0d49de031eb5b4e59614335be
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-18 18:03:39 -0400

    ingles 1 | unit 1 | 10 listening

---
+++ ingles-1-interchange/unit-1/10-listening.js
@@ -0,0 +1,23 @@
+/* 10 Listening     Everyone calls me Bill
+
+Listen to the conversations. Complete the information about each person.
+
+1--------------------------------------------------
+First name              William
+Last name               Davis
+Where from?             Chicago
+What do they study?     History
+
+2--------------------------------------------------
+First name              Josefina
+Last name               Ortiz
+Where from?             Mexico
+What do they study?     Business
+
+3--------------------------------------------------
+First name              Min-soo
+Last name               Park
+Where from?             South-Korea
+What do they study?     Math
+
+*/
