d57952c32b123c1c66f350c67705c598a4dc68ad
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-18 00:21:45 -0400

    ingles 1 | unit 1 | 5 grammar focus

--- ingles-1-interchange/unit-1/5-grammar-focus.js
+++ ingles-1-interchange/unit-1/5-grammar-focus.js
@@ -1,4 +1,14 @@
 /* 5    grammar focus
 
+A   Complete these sentences. Then tell a partner about yourself.
 
+1.  My_  name is Aiko Yoshida.  _I'm_   from Japan. _My_    family is in Nagoya.
+    _My_    brother is a college student.   _his_   name is Haruki.
+
+2.  _My_ name is Matias. _I'm_ from Santiago. _It's_ a really nice city.
+    _My_ sister is a student here. _Our_ parents are in Chile right now.
+
+3.  _I'm_ Angelica, but everyone calls me Angie. _My_ last name is Newton.
+    _I'm_ a student at City College. _My_ parents are on vacation this week.
+    _They're_ in Las Vegas.  
 */
