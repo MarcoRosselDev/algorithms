a024391170a43a16be55f78d7b17c43dcd1253b3
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-18 17:46:03 -0400

    ingles 1 | unit 1 | 9 word power

---
+++ ingles-1-interchange/unit-1/9-word-power.js
@@ -0,0 +1,36 @@
+/* 9 word power | hello and good-bye
+
+A   Do you know these expressions? Which ones are "hellos"
+    and which ones are "good-bye"? Complete the chart.
+    And expressions of your own.
+
+
+    Bye.-
+    Good morning.-
+    Good night.
+    Have a good day.
+    Hey.
+    Hi.
+    How are you?
+    How's it going?
+    See you later.
+    See you tomorrow.
+    Talk yo you later.
+    What's up?
+
+    Hello---------------------------
+    Good morning.
+    Hey.
+    Hi.
+    How are you?
+    How's it going?
+    What's up?
+    Good-bye------------------------
+    Bye.
+    Good night.
+    Have a good day.
+    See you later.
+    See you tomorrow.
+    Talk yo you later.
+
+*/
