a7e704b9d8c9513c178b2205825f70f44067a581
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-18 18:29:20 -0400

    ingles 1 | unit 2 | 2 word power

---
+++ ingles-1-interchange/unit-1/12-reading.js
@@ -0,0 +1,21 @@
+/* 12 READING
+
+1.  Baby names like Mildred and Larry aren't so trendy now.
+    true
+
+2.  Many bebies are named after clothes.
+    false.
+
+3.  Alicia Keys has a son names Hazel.
+    false   , his name is Egypt.
+
+4.  There is a famous prince names George.
+    true
+
+5.  Some girls' names are the same as flower names.
+    true
+
+6.  Babies never have names that are the same as planets or stars.
+    false
+
+*/
---
+++ ingles-1-interchange/unit-2/2-word-power.js
@@ -0,0 +1,47 @@
+/* 2    WORD POWER 
+
+A Complete the word map with jobs from the list.
+
+✓  accountant
+✓  cashier
+    chef
+✓  dancer
+✓  flight attendant
+    musician
+    pilot
+    receptionist
+    server
+    singer
+    tour guide
+    web designer
+
+--------------------OFFICE WORK
+accountant
+receptionist
+web designer
+
+sales manager
+secretary
+--------------------FOOD SERVICE
+cashier
+chef
+server
+
+dishwasher
+host/hostess
+--------------------TRAVEL INDUSTRY
+flight attendant
+pilot
+tout guide
+
+font desk clerk = Empleado de escritorio de fuente
+travel agent
+--------------------ENTERTAINMENT BUSINESS
+dancer
+musician
+singer
+
+actor/acress
+disc jockey
+
+*/
