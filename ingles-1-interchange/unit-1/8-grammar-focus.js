6e300480aafd2c2a1bf226693c66e68b11016b53
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-18 03:28:00 -0400

    ingles 1 | unit 1 | 8 grammar focus A

---
+++ ingles-1-interchange/unit-1/8-grammar-focus.js
@@ -0,0 +1,16 @@
+/* 8    Grammar focus
+A   Complete the conversations. Then practice with a partner
+
+1.  A:  -Is- Mr. Jones from the United Sates ?
+    B:  Yes, he -is- . -he's- from Baltimore.
+    .
+2.  A:  -Is- English class at 2:00 ?
+    B:  No, it -is not/isn't-. -it's- at 3:00.
+    .
+3.  A:  -Are- you and Giovanna from Italy ?
+    B:  Yes, we -are- . -We're- from Milan.
+    .
+4.  A:  -Are- Mr. and Mrs. Flores Brazilian ?
+    B:  No, they -aren't- . -They're- Peruvian.
+
+*/
