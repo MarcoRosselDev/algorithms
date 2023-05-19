f6ded78542f17b7da1ec742b93e08cab0b157b24
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-19 06:01:48 -0400

    ingles 1 | unit 2 | 10  listening

---
+++ ingles-1-interchange/unit-2/10-listening.js
@@ -0,0 +1,25 @@
+/* 10   LISTENING
+
+A   Listen to Aaron, Madison, and Kayla talk about their daily schedules.
+    Complete the chart.
+
+    Aaron---------------------------------------- 
+Job                     carpenter
+Gets up at . . .        6 Am
+Gets home at . . .      4 pm
+Goes to bed at . . .    10 pm
+
+    Madison--------------------------------------
+Job                     accountant
+Gets up at . . .        7:00 a.m.
+Gets home at . . .      6 pm
+Goes to bed at . . .    midnight on week-days
+
+    Kayla----------------------------------------
+
+Job                     nurse
+Gets up at . . .        4 pm   
+Gets home at . . .      8 am
+Goes to bed at . . .    8:30 am
+
+*/
