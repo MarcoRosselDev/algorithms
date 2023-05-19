24d104970aa6ea9da3989146f80577fdc8baa464
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-19 05:47:12 -0400

    ingles 1 | unit 2 | 9 grammar focus  A

---
+++ ingles-1-interchange/unit-2/9-grammar-focus.js
@@ -0,0 +1,18 @@
+/* 9    GRAMMAR FOCUS
+
+A   Choose the correct word.
+
+1. I get up -at- / until six at / -on- weekdays. 
+2. I have lunch -at- / early 11:30 in / -on- Mondays. 
+3. I have a snack in / -around- 10:00 in / -at- night. 
+4. In / -On- Fridays, I leave school -early- / before. 
+5. I stay up before / -until- 1:00 A.M. in / -on- weekends. 
+6. I sleep around / -until- noon in / -on- Sundays. dinner.
+7. I have dinner -at- / in 7:00 at / -on- weeknights.
+8. I read a book after / -before- I go to sleep.
+9. In / -On- weekends, I go to bed in / -at- 1:00 A.M.
+10. In / -On- Thursdays, I leave work -at- / in 9:00 P.M.
+11. I work -late- / until on Wednesdays.
+12. I study around / -until- 11:00 -after- / early
+
+*/
