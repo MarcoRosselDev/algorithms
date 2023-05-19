03a3e2329dcbd5cdfddcc1adf65baf6537a4c6aa
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-18 21:13:04 -0400

    ingles 1 | unit 2 | 5 grammar focus

---
+++ ingles-1-interchange/unit-2/5-grammar-focus.js
@@ -0,0 +1,31 @@
+/* 5    GRAMMAR FOCUS
+
+A   Complete these conversations. Then practice with a partner.
+
+1.  A:  What do you do?
+    B:  I'm a full-time student. I study the piano.
+    A:  And Where do you go to school?
+    B:  I Go to the Brooklyn School of Music.
+    A:  Wow! How do you like your classes?
+    B:  I like them a lot.
+
+2.  A:  What -does- Tanya do?
+    B:  She's teacher. She -teaches- an art class at a school in Denver.
+    A:  And what about Ryan? Where -does- he work?
+    B:  He -works- for a big computer company in San Francisco.
+    A:  -What- does he do, exactly?
+    B:  He's a web designer. He -designs- fantastic websites.
+
+3.  A:  What -doe- Bruce and Ivy do ?
+    B:  They -work- at an Italian restaurant. It's really good.
+    A:  That's nice. -What- is Ivy's job?
+    B:  Well, she manages the finances and Bruce -works- in the kitchen.
+
+4.  A:  Where -does- Ali work?
+    B:  He -works- at the university. He -has- a part-time job.
+    A:  Really? What -does- he do?
+    B:  He -does- office work.
+    A:  How -does- he like it?
+    B:  Not much, but he -has- some extra money to spend!
+
+*/
