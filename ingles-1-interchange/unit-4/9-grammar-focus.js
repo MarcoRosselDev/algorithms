1a07fd91ce76e05c5a7849b36d876f0d8d185f62
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-20 07:03:31 -0400

    ingles 1 | unit 4 | 9 grammar focus

---
+++ ingles-1-interchange/unit-4/9-grammar-focus.js
@@ -0,0 +1,38 @@
+/* 9    GRAMMAR FOCUS
+
+Would; verb + to + verb
+haria; verbo + a + verb
+
+Would you like to go out on Friday ? 
+Would you like to go to a concert ?
+    Contraction-------------------------I'd = I would---|me gustaria|
+Yes, I would.
+Yes, I'd love to. Thanks.
+
+I'd like to, but I have to work late.
+I'd like to, but I need to save money.
+I'd like to, but I want to visit my parents.
+
+
+A   Respond to three invitations. Then write three invitations for the given responses.
+
+
+1.  A:  I have tickets to the soccer game on Sunday.
+        Would you like to go?
+    B:  Yes, I'd love to, thanks.
+
+2.  A:  Would you like to come over for dinner tomorrow night?
+    B:  Yes, I'd love to go over for dinner tomorrow night, thanks.
+
+3.  A:  Would you like to go to a hip-hop dance class with me this weekend?
+    B:  I'd like to, but I have to go to work this weekend.
+
+4.  A:  Would you like to go out on Friday ?
+    B:  Yes, I'd like to. Thanks you!
+
+5.  A:  Would you like to go to a concert later ?
+    B:  Well, I'd like to, but I have to study.
+
+6.  A:  Would you like to go to a electronic party this Freeday ?
+    B:  Yes, I would. I really like electronic music.
+*/
