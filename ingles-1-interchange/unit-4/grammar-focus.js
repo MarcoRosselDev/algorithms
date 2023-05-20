f743ab2d5034831e47905eeed3c31dbddb8801b1
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-20 03:11:36 -0400

    ingles 1 | unit 4 | 4 grammar focus

---
+++ ingles-1-interchange/unit-4/grammar-focus.js
@@ -0,0 +1,17 @@
+/* 4    GRAMMAR FOCUS
+
+Complete these conversations.   Then practice with a partner.
+
+1.  A:  I like Alabama Shakes a lot. Do you know Them.
+    B:  Yes, I do, and I love this song. Let's download it.
+
+2   A:  Do you like science fiction movies?
+    B:  Yes, I do. I like them very much.
+
+3.  A:  Do Vinnie and Midori like soap operas?
+    B:  Vinnie does, but Midori doesn't. She hates them. 
+
+4.  A:  What kind of music Does Maya like?
+    B:  Classical music. She loves Yo-Yo Ma.
+    A:  Yeah, he's amazing. I like him a lot.
+*/
