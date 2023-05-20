6e83bc7dfc7d7da6f40a4aac69f20634e4468366
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-19 22:37:10 -0400

    ingles 1 | unit 3 | 3 grammar focus

---
+++ ingles-1-interchange/unit-3/3-grammar-focus.js
@@ -0,0 +1,19 @@
+/* 3    GRAMMAR FOCUS 
+
+A   Complete these conversations. Then practice with a partner.
+
+1.  ---------------------------------------------
+A:  Escuse me. How much are those jeans?
+B:  Which one ? Do you mean these ?
+A:  No, the light blue ones.
+B:  Oh, those are $59.95.
+A:  Wow! That's expensive!
+
+2.  ---------------------------------------------
+A:  How much is that backpack?
+B:  Which one ?
+A:  The orange one.
+B:  It's $36.99. But This green one is only $22.25.
+A:  That's not bad. Can i see it, please ?
+
+*/
