0039097d49654db3b2d2b9f7302a254453f30e85
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-20 03:44:03 -0400

    ingles 1 | unit 4 | 7 Listening

---
+++ ingles-1-interchange/unit-4/7-listening.js
@@ -0,0 +1,24 @@
+/* 7    LISTENING
+
+A Listen to a host and four people on a TV game show. Three men want to invite Alexis on a date.
+What kinds of things do they like ? Complete the chart.
+
+----Jacob-------------------------------------------------
+Music       Classical
+Movies      Action muvies
+TV shows    Reality shows
+----Tyler-------------------------------------------------
+Music       Jazz
+Movies      Musicals
+TV shows    Talk shows
+----Andrew------------------------------------------------
+Music       Rock
+Movies      Horror movies
+TV shows    Game shows
+----Alexis------------------------------------------------
+Music       Pop Music
+Movies      Horror movies
+TV shows    Took shows and Game shows
+
+
+*/
