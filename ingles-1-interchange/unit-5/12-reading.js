722d4488dcb73191bd191d4a1fd105ada4a0cc10
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-21 06:07:35 -0400

    ingles 1 | unit 5 | 12 reading

---
+++ ingles-1-interchange/unit-5/12-reading.js
@@ -0,0 +1,12 @@
+/* 12   READING
+
+B   Read the invertviews. Then check the correct names.
+
+1.  watches a lot of TV                         Billy
+2.  sees their grandparents twice a week        Carla
+3.  spends time outdoors                        Jane
+4.  stays at home with the kids                 Nick
+5.  doesn't spend much time with family         Billy
+6.  does housework during the day               Nick
+
+*/
