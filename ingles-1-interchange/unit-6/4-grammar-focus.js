1b82a14ad3e6ab911ddb639557555f85ea17ecc4
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-21 06:32:11 -0400

    ingles 1 | unit 6 | 4 grammar focus

---
+++ ingles-1-interchange/unit-6/4-grammar-focus.js
@@ -0,0 +1,33 @@
+/* 4    GRAMMAR FOCUS
+
+adverb of frequency
+
+How often do you exercise?
+    I run on the treadmill every day
+    I go jogging once a week
+    I play soccer twice a month
+    I swim about three timer a year.
+    I don't exercise very often/much
+    Usually I exercise before class.-------Usually and sometimes can begin a sentence.
+
+Do you ever watch TV in the evening?
+    Yes, I oftern watch TV after dinner.
+    I sometimes watch TV before bed.
+    Sometimes I watch TV before bed.*
+    I hardly ever watch TV.
+    No, I never watch TV.
+
+*/
+
+/* 100 % to 0 % 
+
+always
+almost always
+usually
+often
+sometimes
+hardly ever
+almost never
+never
+
+*/
