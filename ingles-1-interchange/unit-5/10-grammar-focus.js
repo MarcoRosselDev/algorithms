222b5b442b2aba33221ff23829fb521e140528f3
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-21 05:53:28 -0400

    ingles 1 | unit 5 | 20  grammar focus

---
+++ ingles-1-interchange/unit-5/10-grammar-focus.js
@@ -0,0 +1,21 @@
+/* 10   GRAMMAR FOCUS
+
+A   Rewrite these sentences using quantifiers.
+    Then compare with a partner.
+
+1.  In the U.S., 69% of high school students go to collage.
+    In the U.S., most high school students go to collage.
+
+2.  Seven percent of the people in Brazil are age 65 or older.
+    Few people in Brazil are age 65 or older.
+
+3.  In India, 0% of the people vote before the age of 18.
+    In India, No one vote before the age of 18.
+
+4.  Forty percent of the people in Sweden live alone.
+    Some people in Sweden live alone.
+
+5.  In Canada. 22% of the people speak French at home.
+    In Canada. Not many people speak French at home.
+
+*/
