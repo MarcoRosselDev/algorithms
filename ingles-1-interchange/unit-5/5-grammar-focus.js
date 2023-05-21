9079fc5720d9450033bde6a01626262b250bcdd6
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-21 02:52:01 -0400

    ingles 1 | unit 5 | 5 grammar focus

---
+++ ingles-1-interchange/unit-5/5-grammar-focus.js
@@ -0,0 +1,19 @@
+/* 5    GRAMMAR FOCUS
+
+A   Complete these phone converstions using the present continuos.
+
+    1.----------------------------------------------------------------
+A:  Hi, Brittany. What are you doing?
+B:  Hey, Zach. I'm eating a sandwich at O'Connor's.
+A:  Mmmm! Is it good?
+B:  Yeah. It's delicious. Wait, they're gringing my dessert now.
+It's chocolate cake with ice cream.
+Call you later! Bye!
+
+    2.----------------------------------------------------------------
+A:  So, Madison, how are you and your sister doing in collage?
+B:  We're having a lot of fun, Mom!
+A:  Fun? OK, but is your sister going to class avery morning?
+B:  Yes, Mom. She's workeing hard and I am, too. I'm serious!
+
+*/
