e035af1896f46f3ff37f70069b7c3c857bcf056a
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-20 23:35:25 -0400

    ingles 1 | unit 5 | 1 word power

---
+++ ingles-1-interchange/unit-5/1.word-power.js
@@ -0,0 +1,16 @@
+/* 1    WORD POWER
+
+A   Look at Joseph's family tree.
+    How are these people relatied to him?
+    Add the words to the family tree.
+
+    cousin = prima
+    niece = sobrina
+    daughter = hija
+    sister-in-law = cuñada
+    father = padre
+    uncle = tío
+    grandmother = abuela
+    wife = esposa
+
+*/
