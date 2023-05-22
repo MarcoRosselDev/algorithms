82e6cd47c791982d03de7622fd2d53db910bb146
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-22 13:43:36 -0400

    ingles 1 | unit 7 | 3 grammar focus

---
+++ ingles-1-interchange/unit-7/3-grammar-focus.js
@@ -0,0 +1,21 @@
+/* 3    GRAMMAR FOCUS
+
+A   Complete these conversations. Then practice with a partner.
+
+1.  A:  -Did- you -stay- home on Sunday?
+    B:  No, I -called- my friend Anna. We -drove- to a nice little restaurant for lunch.
+
+2.  A:  How did you spend your last birthday?
+    B:  I had a party. Everyone enjoyed it, but the neighbors next door  didn't like the noise.
+
+3.  A:  What did you do last night?
+    B:  I saw a sci-fi movie at the Cineplex. I loved It! Amazing special effects!
+
+4.  A:  Did you do anything special over the weekend?
+    B:  Yes, I did. I went shopping. Unfortunately. I spent all my money. Now I'm broke!
+
+5.  A:  Did you go out on Friday night?
+    B:  No, I didn't. I invited friend over, and I cooked spaghetti for them.
+
+
+*/
