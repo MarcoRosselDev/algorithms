a16e6f20e18654964b8ca991786b4a7672f834ab
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-22 13:05:06 -0400

    ingles 1 | unit 6 | 11 grammar how

---
+++ ingles-1-interchange/unit-6/ques-how.js
@@ -0,0 +1,22 @@
+/* 11   GRAMMAR FOCUS
+
+question with how: short answers
+
+How often do you work out?
+    Every day.
+    Twice a week.
+    Not very often.
+How long do you spend at the gym?
+    Thirty minutes a day.
+    Two hours a week.
+    About an hour on weekends.
+How well do you play tennis?
+    Pretty well.
+    About average.
+    Not very well.
+How good are you at sports?
+    Pretty good.
+    OK.
+    Not so good.
+
+*/
