a125a7414e2cec7311eac2885c8d892631682848
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-22 13:01:16 -0400

    ingles 1 | unit 6 | 11 grammar focus

---
+++ ingles-1-interchange/unit-6/11-grammar-focus.js
@@ -0,0 +1,17 @@
+/* 11   GRAMMAR FOCUS
+
+A   Complete these questions. Then practice with a partner.
+
+1.  A:  - How good are you - at sports?
+    B:  I guess I'm pretty good. I play a lot of different sports.
+
+2.  A:  -How long do you- spend online?
+    B:  About an hour after dinner. I like to chat with my friends.
+
+3.  A:  -How often do you- go to the beach?
+    B:  Once or twice a month. It's a good way to relax.
+
+4.  A:  -How well can you- swim?
+    B:  Not very well. I need to take swimming lessons.
+
+*/
