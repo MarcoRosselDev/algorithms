f5b22d1f3401fe567e245eced47401f18620e314
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-23 04:31:04 -0400

    ingles 1 | unit 7 | iregular verbs

---
+++ ingles-1-interchange/unit-7/verbs-iregular-iguales.js
@@ -0,0 +1,23 @@
+/* Verbos irregulares:  infinitivo, pasado y participio iguales
+
+Ejemplo:    read = leer
+
+I read every day            = Leo cada dia (presente)
+I read that book last year  = Lei ese libro el agnio pasado (pasado)
+Have you read this book ?   = Has leido este libro ? (participio)
+
+Present     Pasado simple   Participio  Traduccion
+
+Cut         Cut             Cut         Cortar
+Cost        Cost            Cost        Costo
+Get         Get             Get         Obtener
+Hit         Hit             Hit         Golpear
+Hurt        Hurt            Hurt        Herir
+Let         Let             Let         Dejar
+Put         Put             Put         Dejar
+Read        Read            Read        Leer
+Set         Set             Set         Fijar
+Split       Split           Split       Separar
+Shut        Shut            Shut        Cerrar
+
+*/
