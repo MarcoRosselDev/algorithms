ee62b46fbf56edbef5dcd3776db14587c67506e3
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-23 16:28:21 -0400

    ingles 1 | unit 7 | 10 grammar focus B

---
+++ ingles-1-interchange/unit-7/12-reading.js
@@ -0,0 +1,9 @@
+/* A    look at the picures. What do you think each perosn did on his or her vacations?
+
+B   Read the online posts. Then write the number of the post where each sentence could go.
+
+-3 Kelly - It was pretty tiring. but I enjoyed every minute of it.
+-1 Marco - The ride was scary because we were so high up.
+-2 Leticia - I hope to meet the pople who made it
+
+*/
