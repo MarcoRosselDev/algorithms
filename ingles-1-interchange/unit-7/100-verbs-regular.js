d848f413c74ed5421f8888077839a618b9fa3119
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-23 03:15:41 -0400

    ingles 1 | unit 7 | 100 regular verbs

---
+++ ingles-1-interchange/unit-7/100-verbs-regular.js
@@ -0,0 +1,102 @@
+/* Los  100 verbos regulares en ingles mas usados
+
+Accept      aceptar
+Add         a;adir
+Admit       admitir
+Allow       permitir
+Agree       estar de acuerdo
+Answer      Contestar/Responder
+Arrive      llegar
+Appear      aparecer
+Ask         preguntar
+Believe     creer
+Belong      pertenecer
+Brush       cepillar
+Call        llamar
+Cancel      cancelar
+Change      cambiar
+Clean       limpiar
+Close       cerrar
+Complain    quejar
+Complete    completar
+Cook        cocinar
+Copy        copiar
+Count       contar
+Cry         llorar
+Dance       bailar
+Decide      decidir
+Decorate    decorar
+Destroy     destruir
+Drop        dejar caer
+Dry         secar
+Enjoy       disfrutar
+Escape      escapar
+Explain     explicar
+Fill        llenar
+Finish      acabar
+Fix         areglar/reparar
+Follow      seguir
+Guess       adivinar
+Happen      suceder
+Hate        odiar
+Help        ayudar
+Imagine     imaginar
+Improve     mejorar
+Increase    aumentar
+Invent      ivitar
+Join        unir
+Jump        saltar
+Kill        matar
+Kiss        besar
+Laugh       aprender
+Like        gustar
+Listen      escuchar
+Live        vivir
+Look        mirar
+Love        amar
+Measure     medir
+Mention     mencionar
+Need        necesitar
+Notice      advertir(en el sentido de notar, darse cuenta de algo)
+Offer       ofrecer
+Open        abrir
+Order       ordenar/pedir
+Organize    organizar
+Paint       pintar
+Place       colocar
+Play        jugar
+Please      agradar/satisfacer
+Practice    practicar
+Prepare     preparar
+Qualify     calificar
+Rain        llover
+Receive     recibir
+Remember    acordarse de...
+Remind      recordar
+Repeat      repetir
+Report      informar
+Require     requerir
+Return      volver
+Search      buscar
+Sign        firmar
+Sit         sentarse
+Smile       sonreir
+Start       empezar/comenzar
+Stop        parar/detenerse
+Study       estudiar
+Talk        hablar
+Touch       tocar
+Translate   traducir
+Travel      viajar
+Try         intentar/probar
+Use         usar
+Visit       visitar
+Wait        esperar
+Walk        caminar
+Want        querer
+Watch       ver/observar/mirar/prestar atencion ("Watch" se refiere a algo que vemos con interes, como una pelicula, mientras que "See" es algo que se ve sin poder evirarse, como encontrar a alguien por la calle.)
+Wish        desear
+Work        trabajar
+Worry       preocuparse
+
+*/
