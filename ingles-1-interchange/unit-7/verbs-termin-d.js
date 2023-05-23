d4f74b72716dbd2bcafd214dbc1bc90f3e6657c7
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-23 04:38:30 -0400

    ingles 1 | unit 7 | verbs pasado y participio iguales

---
+++ ingles-1-interchange/unit-7/verbs-termin-d.js
@@ -0,0 +1,10 @@
+/* Verbos que terminan en -d y el pasado y el participio se conjugan con -t
+
+Present     Pasado simple   Participio  Traduccion
+
+Build       Built           Built       Contruir
+Lend        Lent            Lent        Prestar
+Send        Sent            Sent        Mandar
+Spend       Spent           Spent       Gastar
+
+*/
