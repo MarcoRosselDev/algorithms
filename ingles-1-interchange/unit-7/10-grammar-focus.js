2937635ca0ed6cec0596e10b59fee6cf843f7e32
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-23 16:13:19 -0400

    ingles 1 | unit 7 | 10 grammar focus A

--- ingles-1-interchange/unit-7/10-grammar-focus.js
+++ ingles-1-interchange/unit-7/10-grammar-focus.js
@@ -15,3 +15,25 @@ wasn't = was not
 weren't = were not
 
 */
+
+/* A    Complete these conversations.Then practice with a partner.
+
+1.  A:  -were- you in New York last weekend?
+    B:  No, I -wasn't-. I -was- in Chicago.
+    A:  How -was- it?
+    B:  It -was- great!. But it -was- cold and windy as usual.
+
+2.  A:  How long -were- your parents in Chile?
+    B:  They -were- there for two weeks.
+    A:  -were- they in Santiago the whole time?
+    B:  No, they -weren't-. They also went to Valparaiso.
+
+3.  A:  -were- you away last week?
+    B:  Yes, I -was- in Madrid.
+    A:  Really? How long -were- you there?
+    B:  For almost a week. I -was- there on business.
+
+were = eran
+was = era
+
+*/
