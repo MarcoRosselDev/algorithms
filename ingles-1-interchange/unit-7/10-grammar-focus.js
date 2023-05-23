933f84516a63ae1602f605baac20b3f91a18fe4b
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-23 16:00:10 -0400

    ingles 1 | unit 7 | 10 grammar focus

---
+++ ingles-1-interchange/unit-7/10-grammar-focus.js
@@ -0,0 +1,17 @@
+/* 10   GRAMMAR FOCUS
+
+Past of be
+
+Were you in california?                 Yes, I was.
+Was the weather OK?                     No, It wasn't.
+Were you and your cousin on vacation?   Yes, we were.
+Were your parents there?                No, they weren't
+How long were you away?                 I was away for a week.
+How was your vacation?                  It was excellent!
+
+Contraction
+
+wasn't = was not
+weren't = were not
+
+*/
