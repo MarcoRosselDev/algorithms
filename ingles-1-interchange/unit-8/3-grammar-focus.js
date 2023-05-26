f70ccf5bba2eb3186231d6e8e1f3355efacf9ebe
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-26 08:29:06 -0400

    ingles 1 | unit 8 | 3 grammar focus

---
+++ ingles-1-interchange/unit-8/3-grammar-focus.js
@@ -0,0 +1,25 @@
+/* 3    GRAMMAR FOCUS
+
+There is, there are;    one, any, some
+
+Is there a laundromat near here?
+    Yes, there is. There's one across from the shopping center.
+    No, there isn't, but there's one next to the library.
+
+Are there any grocery store around here?
+    Yes, there are. There are some nice stores on Pine Street.
+    No, there aren't, but there are some on Third Avenue.
+    No, there aren't any around here.
+
+Prepositions
+in
+on
+next to
+near / close to
+across from / opposite
+in front of
+in back of / behind
+between
+en the corner of
+
+*/
