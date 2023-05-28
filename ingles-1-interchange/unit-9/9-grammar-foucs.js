c63eece0b06f439ab0fa06dce6104fbd60c8149f
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-28 12:13:44 -0400

    ingles 1 | unit 9 | 9 grammar focus A

--- ingles-1-interchange/unit-9/9-grammar-foucs.js
+++ ingles-1-interchange/unit-9/9-grammar-foucs.js
@@ -13,5 +13,23 @@ Which one is Paula?             She's the tall one  in jeans.
 Who are the Harrisons?          They're the people  Next to the window.
 Wich ones are the Harrisons?    They're the ones    on the couch.
 
+*/
+
+/* A    Rewrite these statements using modifiers with participles or prepositions.
+
+1.  Kyle is the tall guy. He's wearing a yellow shirt and brown pants.
+    Kyle is the tall guy wearing a yellow shirt and brown pants.
+
+2.  Mark and Eva are the middle-aged couple. They're talking to Michael.
+    Mark and Eva are the middle-age couple talking to Michael.
+
+3.  Alexis is the young girl. She's in a white T-shirt and blue jeans.
+    Alexis is the young girl in white T-shirt and blue jeans.
+
+4.  Britany is the woman in the green dress. She's stting to the left of Javier.
+    Britany si the woman in the green dress stting to the left of Javier.
+
+5.  J.P. is the serious-looking boy. He's playing a video game.
+    J.P is the serious-looking boy playing a video game.
 
 */
