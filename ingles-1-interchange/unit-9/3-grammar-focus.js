f270ea6af9c7d8f9a4a4f4c479a4bb3fb126b62c
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-27 20:57:13 -0400

    ingles 1 | unit 9 | 3 grammar focus

---
+++ ingles-1-interchange/unit-9/3-grammar-focus.js
@@ -0,0 +1,40 @@
+/* 3    GRAMMAR FOCUS
+
+Describing people
+
+General appearance
+    What does she look like?
+        She's tell, with brown hair.
+        She's pretty.
+
+    Does he wear glasses?
+        No, he wears contacts?
+
+---------------------------------------
+Height
+    How tall is she?
+        She's 1 meter 78.
+        She's 5 fooy 10.
+
+    How tall is he?
+        He's medium height.
+
+---------------------------------------
+Hair
+    How long is her hair?
+        It's pretty short.
+
+    What color is his hair?
+        It's dark/light brown.
+
+---------------------------------------
+Age
+    How old is she?
+        She's about 32.
+        She's in her thirties.
+
+    How old is he?
+        He's in his twenties.
+
+
+*/
