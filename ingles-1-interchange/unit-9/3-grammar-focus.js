6e9fa25688b2b51b00762768ce74ccf81e66646d
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-27 21:06:24 -0400

    ingles 1 | unit 9 | 3 grammar focus A

--- ingles-1-interchange/unit-9/3-grammar-focus.js
+++ ingles-1-interchange/unit-9/3-grammar-focus.js
@@ -38,3 +38,29 @@ Age
 
 
 */
+
+/* A    Write questions to match these statements.
+Then compare with a partner.
+
+1.  How old is your father?
+    My father is 52.
+
+2.  How tall are you ?
+    I'm 167 cm (5 foot 6)
+
+3.  What color is your cousin's hair?
+    My cousin has red hair.
+
+4.  Does he wear glasses?
+    No, he wears contact lenses.
+    
+5.  What does he look like?
+    He's tall and very good-looking
+
+6.  How long is your sister's hair?
+    My sister's hair is medium lenght.
+
+7.  what color eyes do you have?
+    I have dark brown eyes.
+
+*/
