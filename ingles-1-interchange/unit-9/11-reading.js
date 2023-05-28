7c31499e0abc3af17b20b94f679539c95b8f9e49
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-28 12:36:50 -0400

    ingles 1 | unit 9 | 11 reading

---
+++ ingles-1-interchange/unit-9/11-reading.js
@@ -0,0 +1,10 @@
+/* B    Read the blog. Math each questions with correct answer.
+
+1.  What is Poppy Dinsey famous for?                        e.  a fashion blog
+2.  Where did Aki Hoshida take a selfie?                    a.  at the International Space Station
+3.  Who says selfies are a way of undestanding ourselves?   d.  psychologists
+4.  Who took the first selfies?                             f.  a man from Philadelphia.
+5.  Where do many people post selfies?                      c.  on social media.
+6.  What is Hoshida's job?                                  b.  astronaut
+
+*/
