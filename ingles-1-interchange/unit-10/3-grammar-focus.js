477cebadc769005982cc75c0d51c1d0aceadeea3
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-28 16:26:29 -0400

    ingles 1 | unit 10 | 3 grammar focus

---
+++ ingles-1-interchange/unit-10/3-grammar-focus.js
@@ -0,0 +1,36 @@
+/* 3    GRAMMAR FOCUS
+
+Present perfect;    already, yet
+
+The present perfect si formed with the verb have + the past particile.
+
+Have you been to a jazz club?
+    Has estado en un club de jazz?
+
+    Yes, I've been to several.
+        si, he estado en varios.
+
+    No, I haven't been to one.
+        No, no he estado en uno.
+
+-----------------------------------------
+Has Carlos visited the theme parks?
+    Ha visitado carlos los parques tematicos?
+
+    Yes, he's visited three or four.
+        Si, ha visitado tres o cuatro.
+
+    No, he hasn't visited any parks
+        No, no ha visitado ningun parque.
+
+-----------------------------------------
+Have they eaten dinner yet?
+    Ya han cenado?
+
+    Yes, they've already eaten
+        Si, ellas/ellos ya han comido.
+
+    No, they haven't eaten yet
+        No, ellas/ellos no han comido todavia.
+
+*/
