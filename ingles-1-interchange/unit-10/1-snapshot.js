557227a452aa64f4bf5200a3908bca795f1c6ad5
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-28 15:26:10 -0400

    ingles 1 | unit 10 | 1 snapshot

---
+++ ingles-1-interchange/unit-10/1-snapshot.js
@@ -0,0 +1,36 @@
+/* 1    SNAPSHOT
+
+Fun for everyone around Orlando!
+    diversion para todos en Orlando!
+
+go to a theme park
+    Ir a un parque tematico.
+
+go dancing
+    Ir a bailar.
+
+visit a space center
+    Visitar un centro espacial.
+
+eat Cuban food
+    comer comida cubana
+
+See an alligator
+    ver un caiman
+
+---------------------------------------------------------
+Which activities have you done? 
+    que actividades has hecho?
+
+    I have not done any of those activities.
+        no he hecho ninguna de esas actividades.
+
+Check the activities you would like to try.
+    marca las actividades que te gustaria probar.
+
+Where can you do these or similar activities in your country?
+    Donde puedes hacer estas actividades u otras similares en tu pais?
+
+    I can do those activities on the street avenue.
+
+*/
