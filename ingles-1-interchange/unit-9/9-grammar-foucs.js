eeaf295271919aa1ff064660b2d5744a78db9408
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-28 11:49:35 -0400

    ingles 1 | unit 9 | 9 grammar focus

---
+++ ingles-1-interchange/unit-9/9-grammar-foucs.js
@@ -0,0 +1,17 @@
+/* 9    GRAMMAR FOCUS
+
+Modifiers with present participles and prepositions
+
+                                        Participio
+Who's Diego?        He's the man        Wearing a blue shirt
+Which one is Diego  He's the one        talking to Brooke.
+
+------------------------------------------------------------------------
+                                                    Prepositions
+Who's Brooke?                   She's the woman     with long black hair.
+Which one is Paula?             She's the tall one  in jeans.
+Who are the Harrisons?          They're the people  Next to the window.
+Wich ones are the Harrisons?    They're the ones    on the couch.
+
+
+*/
