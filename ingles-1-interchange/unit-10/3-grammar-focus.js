2f2f026d1c812f03bed6b7e8a141cdfb3e24a94f
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-28 21:37:45 -0400

    ingles 1 | unit 10 | 3 grammar focus A

--- ingles-1-interchange/unit-10/3-grammar-focus.js
+++ ingles-1-interchange/unit-10/3-grammar-focus.js
@@ -34,3 +34,41 @@ Have they eaten dinner yet?
         No, ellas/ellos no han comido todavia.
 
 */
+
+
+/* A    How many times have you done these things in the past week?
+Write your answers. Then compare with a partner.
+
+1.  cook dinner
+
+*/
+
+/*  constractions
+
+I've = I have
+you've = you have
+he's = he has
+she's = she has
+it's = it has
+we've = we have
+they've = they have
+hasn't = has not
+haven't = have not
+
+*/
+
+/* B    Complete these conversations using the present perfect.
+Then practice with a partner.
+
+1.  A   Have you done much exercise this week?
+    B   Yes, I've already been to pilates class.
+
+2.  A   Have you played any sports this month?
+    B   No, I haven't had the time.
+
+3.  A   How many movies have you been to month
+    B   Actually, I haven't seen any yet.
+
+4.  A   
+
+*/
