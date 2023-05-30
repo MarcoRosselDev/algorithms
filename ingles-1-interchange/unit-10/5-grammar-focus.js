e46d32aae32aa545121f4d17a117841e484d8267
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-29 20:48:12 -0400

    ingles 1 | unit 10 | 5 grammar focus

---
+++ ingles-1-interchange/unit-10/5-grammar-focus.js
@@ -0,0 +1,19 @@
+/* 5    GRAMMAR FOCUS
+
+Have you ever eaten Cuban food?
+    Alguna vez has comido comida Cubana?
+
+----Use the present perfect for an indefinite time in the past.
+
+    Yes, I have. I've had it many times.
+        Si, lo he hecho muchas veces.
+    No, I haven't. I haven't tried it yet.
+        No, no lo he hecho, todavia no lo he probado.
+
+----Use the simple past for a specific event in the past
+
+    I ate a lot of Cuban food when I lived in Miami.
+        Comi mucha comida cubana cuando vivia en Miami.
+    No, I never tried it when I lived in Miami.
+        No, nunca lo probe cuando vivia en miami.
+*/
