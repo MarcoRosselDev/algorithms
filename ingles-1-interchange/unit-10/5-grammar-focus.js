da269e9113911870bba5bdbbf1bc2bdfcad197d2
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-31 13:04:04 -0400

    ingles 1 | unit 10 | 5 grammar focus 2

--- ingles-1-interchange/unit-10/5-grammar-focus.js
+++ ingles-1-interchange/unit-10/5-grammar-focus.js
@@ -16,4 +16,23 @@ Have you ever eaten Cuban food?
         Comi mucha comida cubana cuando vivia en Miami.
     No, I never tried it when I lived in Miami.
         No, nunca lo probe cuando vivia en miami.
+
+Have you ever seen an alligator?
+    Alguna vez has visto un caiman?
+
+----Use the present perfect for an indefinite time in the past.
+
+    Yes, I have. I've seen a few alligators in my life.
+        Si, lo e visto. He visto algunos caimanes en mi vida.
+
+    No, I haven't. I've never seen one.
+        No, no lo he hecho. Nunca he visto uno.
+
+----Use the simple past for a specific event in the past
+
+    I saw a big alligator at the new park last week.
+        Vi un caiman grande en el nuevo parque la semana pasada.
+
+    I didn't go to the alligator park last week, so I didn't see any.
+        No fui al parque de caimanes la semana pasada, asi que no vi nunguna.
 */
