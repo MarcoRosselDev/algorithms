c5199460b5df443601fe8c01b7a9733af3b1be5f
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-01 17:09:55 -0400

    ingles 1 | unit 11 | 3 grammar focus

---
+++ ingles-1-interchange/unit-11/3-grammar-focus.js
@@ -0,0 +1,11 @@
+/* 3    GRAMMAR FOCUS
+
+Adverbs before adjectives
+
+Punta Cana is really nice.      It's a really nice place.
+It's fairly expensive.          It's a fairly expensive destination.
+It's not very big.              It's not a very big city.
+
+New York is too noisy, and it's too crowded for me.
+
+*/
