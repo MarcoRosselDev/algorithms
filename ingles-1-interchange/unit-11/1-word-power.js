be89bb7c63f741f0e4183b39a5b9e47aecc6e9d2
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-01 15:54:06 -0400

    ingles 1 | unit 11 | 1 word power

---
+++ ingles-1-interchange/unit-11/1-word-power.js
@@ -0,0 +1,30 @@
+/* 1    WORD POWER
+
+A   Match each word in column A with its opposite in column B.
+Then add two more pairs of adjectives to the list.
+
+A
+1.  beautiful       hermoso/sa
+2.  cheap           barato/ta
+3.  clean           limpio/pia
+4.  interesting     interesante
+5.  quiet           tranquilo/la
+6.  relaxing        relajante
+7.  safe            segura/ro
+8.  spacious        espaciosa/so
+9.
+10.
+
+B
+a.  boring          aburrida/do
+b.  crowded         atestada/do
+c.  dangerous       peligrosa/so
+d.  expensive       caro/ra
+e.  noisy           ruidoso/sa
+f.  polluted        contaminado/da
+g.  stressful       estresante
+h.  ugly            feo/a
+i.
+j.
+
+*/
