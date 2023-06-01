192f0512985a0bd00366c2b034a50ef2edd55bdc
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-31 22:37:06 -0400

    ingles 1 | unit 10 | 5 grammar focus C 1

---
+++ ingles-1-interchange/unit-10/5-2-grammar-focus.js
@@ -0,0 +1,16 @@
+/* For and since
+
+How long did you live in Miami?
+    Cuanto tiempo viviste en Miami?
+
+    I lived there for four years. It was a great experience.
+        Vivi alli durante cuatro agnios. Fue una gran experiencia.
+
+How long have you lived in Orlando?
+    Cuanto tiempo ha vivido en Orlando?             -----------------------> lo sigue haciendo!
+
+    I've lived here for three years. I'm very happy here.
+        He vivido aqui durante tres agnios. Estoy muy feliz aqui.
+
+    I've worked at the hotel since last year. I love it there.
+*/
