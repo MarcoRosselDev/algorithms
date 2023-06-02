68b3133a91d190bddd7250a62c8d5b31acd73d97
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-02 18:41:35 -0400

    ingles 1 | unit 12 | 9 grammar focus A

--- ingles-1-interchange/unit-12/9-grammar-focus.js
+++ ingles-1-interchange/unit-12/9-grammar-focus.js
@@ -13,3 +13,21 @@ What do you suggest/have for a backache?
     Why don't you try these pills?
 
 */
+
+/* Choose the correct words. Then compare and practice with a partner.
+
+1.  A   Can I help you?
+    B   What do you suggest for dry skin?
+    A   Why don't you try this lotion? It's excellent.
+    B   OK. I'll take it.
+
+2.  A   May I have something for itchy eyes?
+    B   Sure. You could try a bottle of eyedrops.
+
+3.  A   Could I have a box of bandages, please?
+    B   Here  you are.
+    A   And what do you suggest for insomnia?
+    B   You should try this herbal tea. It's very relaxing.
+    A   OK. Thanks.
+
+*/
