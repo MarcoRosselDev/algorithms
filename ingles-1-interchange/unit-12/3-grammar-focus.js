b20e2c2d3f7aed5c41811770c9a29df21526cfc5
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-02 11:02:59 -0400

    ingles 1 | unit 12 | 3 grammar focus

---
+++ ingles-1-interchange/unit-12/3-grammar-focus.js
@@ -0,0 +1,11 @@
+/* 3    GRAMMAR FOCUS
+
+Adjective + infinitive; noun + infinitive
+
+What should you do for a could?
+
+    It's important              to get some rest.
+    It's sometimes helpful      to drink garlic tea.
+    It's a good idea            to take some vitamin C.
+
+*/
