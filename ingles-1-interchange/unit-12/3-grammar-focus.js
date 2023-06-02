4175ddf773ec607afa21adb80d156817e0567704
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-02 11:34:21 -0400

    ingles 1 | unit 12 | 3 grammar focus A

--- ingles-1-interchange/unit-12/3-grammar-focus.js
+++ ingles-1-interchange/unit-12/3-grammar-focus.js
@@ -9,3 +9,31 @@ What should you do for a could?
     It's a good idea            to take some vitamin C.
 
 */
+
+/* A    Look at these health problems. Choose several pieces of good advice for each problem.
+
+Problems
+
+1.  a backache      dolor de espalda
+2.  a bad headache  un mal dolor de cabeza
+3.  a burn          una quemadura
+4.  a cough         tos
+5.  a fever         fiebre
+6.  the flu         la gripe
+7.  a sore throat   un dolor de garganta
+8.  a toothache     un dolor de muelas
+
+advice
+
+a.  drink lots of liquids
+b.  get some medicine
+c.  go to bed and rest
+d.  put it under cold water
+e.  put a heating pad on it
+f.  put some cream on it
+g.  see a dentist
+h.  see a doctor
+i.  take some pain medicine.
+j.  take some vitamin C
+
+*/
