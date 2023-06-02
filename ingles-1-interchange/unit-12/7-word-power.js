d42c8d39678c757cc60dc029d86ca86a1dcc4fba
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-02 18:22:15 -0400

    ingles 1 | unit 12 | 7 word power

---
+++ ingles-1-interchange/unit-12/7-word-power.js
@@ -0,0 +1,25 @@
+/* 7    WORD POWER
+
+A   Use the words in the list to complete these expressions.
+Then compare with a partner. Sometimes more than one answer is correct.
+
+
+bag     ---------> bolsa
+bottle  ---------> botella
+box     ---------> caja
+can     ---------> lata
+jar     ---------> tarro
+pack    ---------> paquete
+stick   ---------> palo
+tube    ---------> tubo
+
+1.  a bottle of pain medicine.
+2.  a box/pack of bandages.
+3.  a bag of cough drops.
+4.  a stick of deodorant.
+5.  a jar of face cream.
+6.  a can of shaving cream.
+7.  a box/pack of tissues.
+8.  a tube of toothpaste.
+
+*/
