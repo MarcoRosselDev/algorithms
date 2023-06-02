2c3900f5f8ab2afcc6022cae162cd827af6fca76
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-01 21:36:35 -0400

    ingles 1 | unit 11 | 3 grammar focus  A

--- ingles-1-interchange/unit-11/3-grammar-focus.js
+++ ingles-1-interchange/unit-11/3-grammar-focus.js
@@ -9,3 +9,18 @@ It's not very big.              It's not a very big city.
 New York is too noisy, and it's too crowded for me.
 
 */
+
+/* A    Match the questions with the answers. Then practice.
+
+1.  What's Seoul like? Is it an interesting place?
+        e.  Yes, It has amazing shopping, and the people are pretty friendly.
+2.  Do you like your hometown? Why or why not?
+        d.  Not really. It's too small, and it's really boring. That's why I moved awayl.
+3.  What's Sydney like? I've nover been there.
+        a.  Oh, really? It's beautiful and very clean. It has a great harbor and beautiful beaches.
+4.  Have you ever been to Sao Paulo?
+        b. Yes, I have. It's an extremely large and crowded place, but I love it. It has excellent restauants.
+5.  What's the weather like in Chicago?
+        c.  It's really nice in the summer, but it's too cold for me in the winter.
+
+*/
