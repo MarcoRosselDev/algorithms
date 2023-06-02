f0157a8eb8416a3069effba62db13fcdb2d94402
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-02 02:44:37 -0400

    ingles 1 | unit 11 | 8 grammar focus

---
+++ ingles-1-interchange/unit-11/8-grammar-focus.js
@@ -0,0 +1,19 @@
+/* 8    GRAMMAR FOCUS
+
+Modal verbs can and should
+
+What can I do in Mexico City?
+    You can walk aroiund the historic center.
+    You can't miss the street food.
+
+What should I see there?
+    You should visit the National Museum of Anthropology.
+    You shouldn't miss the Diego Rivera murals.
+
+*/
+
+/* A    Complete these conversations using can, can't, chould, or shouldn't.
+Then practice with a partner.
+
+
+*/
