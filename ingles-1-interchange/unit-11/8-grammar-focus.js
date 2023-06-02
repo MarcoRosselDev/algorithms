1c64851a8da587249fd0388d1d4c7ccf331bc082
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-02 02:55:30 -0400

    ingles 1 | unit 11 | 8 grammar focus  A

--- ingles-1-interchange/unit-11/8-grammar-focus.js
+++ ingles-1-interchange/unit-11/8-grammar-focus.js
@@ -12,8 +12,21 @@ What should I see there?
 
 */
 
-/* A    Complete these conversations using can, can't, chould, or shouldn't.
+/* A    Complete these conversations using can, can't, should, or shouldn't.
 Then practice with a partner.
 
+1.  A   I can't decide where to go on my vacation.
+    B   You should go to Morocco. It's my favorite place to visit.
 
+2.  A   I'm planning to go to Puerto Rico next year. When do you think I should go?
+    B   You can go anytime. The weather is nice almost all year.
+
+3.  A   Should I rent a car when I arrive in New York? What do you recommend?
+    B   No, you should definitely use the subway. It's fast and not too expensive.
+    
+4.  A   Where can I get some nice jewelry in Istanbul?
+    B   You shouldn't  miss the Grand Bazaar. It's the best place for bargains.
+
+5.  A   What can I see from the Eiffel Tower?
+    B   You can see all of Paris, but in bad weather, you can't see anything. 
 */
