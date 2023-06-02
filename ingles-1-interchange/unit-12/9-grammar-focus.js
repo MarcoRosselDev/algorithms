d35b2b6d09370aea8fa8470af803c100aa106932
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-02 18:34:15 -0400

    ingles 1 | unit 12 | 9 grammar focus

---
+++ ingles-1-interchange/unit-12/9-grammar-focus.js
@@ -0,0 +1,15 @@
+/* 9    GRAMMAR FOCUS
+
+Modal verbs can, could and may for requests, suggestions.
+
+Can/May I help you?
+    Can I have a bag of cough drops?
+    Could I have something for a cough?
+    May I have a bottle of pain medicine?
+
+What do you suggest/have for a backache?
+    You could try this new cream.
+    You should get a heating pad.
+    Why don't you try these pills?
+
+*/
