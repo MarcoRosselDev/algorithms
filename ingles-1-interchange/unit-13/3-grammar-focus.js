2d162886dc2a904145136e78df17b81c06467d55
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-02 20:13:09 -0400

    ingles 1 | unit 13 | 3 grammar focus

---
+++ ingles-1-interchange/unit-13/3-grammar-focus.js
@@ -0,0 +1,24 @@
+/* 3    GRAMMAR FOCUS
+
+So, too, neither(+), either(-)
+Asi, tambien, tampoco(construccion negativo), tampoco(construccion positivo).
+
+I'm crazy about Italian food.
+    Estoy loco por la comida Italiana.
+
+I can eat really spicy food.
+    Puedo comer comida muy picante.
+
+I like Japanese food a lot.
+    Me gusta mucho la comida Japonesa.
+
+I'm not in the mood for Indian food.
+    No estoy de humor para la comida India.
+
+I can't stand fast food.
+    No soporto la comida rapida.
+
+I don't like slty food.
+    No me gusta la comida salada.
+
+*/
