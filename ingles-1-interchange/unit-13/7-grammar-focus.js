8a9a4b336d1e944ab31bd59f3dac8eb50c246e2a
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-07 04:52:00 -0400

    ingles 1 | unit 13 | 7 grammar focus

---
+++ ingles-1-interchange/unit-13/7-grammar-focus.js
@@ -0,0 +1,29 @@
+/* 7    GRAMMAR FOCUS
+
+Modal verbs would and will for requests
+
+---------------------------------------------
+Contractions
+---------------------------------------------
+I'll    =   I will
+I'd     =   I would
+---------------------------------------------
+
+what would you like?                que te gustaria?
+    I'd like the veggie burger.     Me gustaria la hamburgesa vegana.
+    I'll have a mango salad.        Tomare una ensalada de mango.
+
+What kind of soup would you like?   que tipo de sopa te gustaria?
+    I'd like onions soup, please.   quisiera sopa de cebolla, por favor.
+    I'll have the soup of the day.  Tomare la sopa del dia.
+
+What would you like to drink?           que te gustaria beber?
+    I'd like a lemonade.                 Quisiera una limonada.
+    I'll have a large orange juice.     Tomare un jugo de naranja grande.
+
+Would you like anything else?           quieres algo mas?
+    Yes, please. I'd like some coffee.  Si, por favor. Quisiera un poco de cafe.
+    That's all, thanks.                 Eso es todo, gracias.
+
+
+*/
