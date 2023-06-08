cd4bd4a256d1cec014495f70c443064e9083812c
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-07 23:13:50 -0400

    ingles 1 | unit 14 | 10 grammar focus | A

--- ingles-1-interchange/unit-14/10-grammar-focus.js
+++ ingles-1-interchange/unit-14/10-grammar-focus.js
@@ -24,3 +24,23 @@ How deep is the Grand Canyon?
     It's about 1282 meters deep.
 
 */
+
+
+/* A    Write the questions to these answers.
+Then practice with a partner.
+
+1.  A:  How high is Niagara Falls?
+    B:  Niagara Falls is 52 meters high.
+
+2.  A:  How big is California?
+    B:  California is about 423 970 square kilometers.
+
+3.  A:  How long is the Nile?
+    B:  The Nile is 6 670 kilometers long.
+
+4.  A:  How far is Osaka from Tokyo.
+    B:  Osaka is about 400 kilometers from Tokyo.
+
+5.  A:  How hot is Mexico City in the summer?
+    B:  Mexico City gets up to about 28° Celsius in the summer.
+*/
