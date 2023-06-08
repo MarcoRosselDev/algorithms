529b7a8c26979313878bc92edb4f8491675c1394
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-07 23:07:39 -0400

    ingles 1 | unit 14 | 10 grammar focus

---
+++ ingles-1-interchange/unit-14/10-grammar-focus.js
@@ -0,0 +1,26 @@
+/* 10   GRAMMAR FOCUS
+
+Questions with how
+
+How cold is Toronto in the winter?
+    It gets down to minus 25° Celsius.
+
+How hot is Fairbanks in the summer?
+    It gets up to about 20° Celsius.
+
+How far is Toronto from Fairbanks?
+    It's about 4800 kilometers.
+
+How big is Seoul?
+    It's 605 square kilometers.
+
+How high is Mount Everest?
+    It's  8848 meters high.
+
+How long is the Mississippi River?
+    It's about 3700 kilometers long.
+
+How deep is the Grand Canyon?
+    It's about 1282 meters deep.
+
+*/
