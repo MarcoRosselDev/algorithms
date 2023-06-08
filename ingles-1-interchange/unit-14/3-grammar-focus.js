28086d9ec709a412f6801fb5849d07da91d5441b
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-07 21:27:32 -0400

    ingles 1 | unit 14 | 3 grammar focus

---
+++ ingles-1-interchange/unit-14/3-grammar-focus.js
@@ -0,0 +1,15 @@
+/* 3    GRAMMAR FOCUS
+
+Comparisons with adjectives
+
+Which country is larger, Australia or Mexico?
+    Australia is larger than Mexico.
+
+Which country is the largest in the world?
+    Russia is the largest country.
+
+Which is more crowded? Malta or England?
+    Malta is more crowded than England.
+    Malta is the most crowded country in Europe.
+
+*/
