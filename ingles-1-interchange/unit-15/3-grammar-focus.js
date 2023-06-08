2dc639cd84c65d8dadfedaaeeee59a4b517f7d42
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-08 09:10:40 -0400

    ingles 1 | unit 15 | 3 grammar focus

---
+++ ingles-1-interchange/unit-15/3-grammar-focus.js
@@ -0,0 +1,29 @@
+/* 3    GRAMMAR FOCUS
+
+Future with present continuous and be going to
+
+With present continuous----------------------------------
+
+What are you doing tonight?
+    I'm going to a party.
+
+Are you doing anything tomorrow?
+    No, I'm not(doing anything).
+
+With be going to + verb-----------------------------------
+
+What is she going to do tomorrow?
+    She's going to see a play.
+
+Are they going to see the photo exhibit?
+    Yes, they are (going to see it).
+
+Time expressions------------------------------------------
+
+tonight
+tomorrow
+on Friday
+this weekend
+next week
+
+*/
