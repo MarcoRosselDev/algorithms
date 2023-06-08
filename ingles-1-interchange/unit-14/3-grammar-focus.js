43bddecb53319ffbc616c778032b1982f75aacfc
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-07 22:17:38 -0400

    ingles 1 | unit 14 | A

--- ingles-1-interchange/unit-14/3-grammar-focus.js
+++ ingles-1-interchange/unit-14/3-grammar-focus.js
@@ -27,3 +27,18 @@ good                better          the best
 bad                 worse           the worst
 
 */
+
+/*A    Complete questions 1 to 4   with comparatives and questions 5 to 8 with superlatives.
+Then ask and answer the questions. 
+
+1.  Which country is smaller, Monaco or Vatican City?
+2.  Which waterfall is higher, Niagara Falls or Victoria Falls?
+3.  Which city is more crowded, Honk Kong or Cairo?
+4.  Which lake is larger, Lake Michigan or Lake Baikel?
+5.  Which is the highest:   Mount Aconcagua, Mount Everest, or Mount Fuji?
+6.  What is the longest river int Americas, the Mississippi, the Colorado, or the Amazon?
+7.  Which city is the most expensive: London, Tokyo, or Moscow?
+8.  What is the deeppest ocean in the world, the Pacific, the Atlantic, or the Artic?
+
+
+*/
