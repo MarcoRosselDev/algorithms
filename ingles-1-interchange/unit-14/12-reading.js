752d03a34e8d8ef02c371e6048c8cb83246530b8
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-07 23:24:43 -0400

    ingles 1 | unit 14 | 12 reading

---
+++ ingles-1-interchange/unit-14/12-reading.js
@@ -0,0 +1,11 @@
+/* C    Read the article and answer the questions.
+
+1.  When did people discover Lake Vostok?
+    1956
+2.  How long has Lake Vostok been hidden?
+3.  What two things is Cape Grim famous for?
+4.  What's the main reason that Cape Grim is so clean?
+5.  About how many people live in Singapore?
+6.  What happens when people break the rules in Singapore?
+
+*/
