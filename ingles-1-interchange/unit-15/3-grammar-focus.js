7641cc0ee9a8e6b7b9bd47790c416606e8c3cd94
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-09 01:38:42 -0400

    ingles 1 | unit 15 | 3 grammar focus  | B

--- ingles-1-interchange/unit-15/3-grammar-focus.js
+++ ingles-1-interchange/unit-15/3-grammar-focus.js
@@ -31,7 +31,7 @@ next week
 /* A    Complete the inventions in column A with the present continuous used as future.
 Complete the responses inclomun B with be going to.
 
-A
+A----------------------------------------------------------------------
 
 1.  What Are you doing tonight? Would you like to go out?
 
@@ -40,5 +40,16 @@ A
 3.  We're having friends over for a barbacue on Sunday. Would you and your parents like to come?
 
 4.  Are you staying in town next weekend? Do you want to go for a hike?
+
+B----------------------------------------------------------------------
+
+a.  I'm going to be here on Saturday, but not Sunday. Let's try to go on Saturday.
+
+b.  Well, my father is going to visit my brother at collage. Butt my mother and I are going to be home. We'd love to come!
+
+c.  Sorry, I can't. I'm going to work late tonight. How about tomorrow night?
+
+d.  Can we go to a late show? I'm going to stay at the office till 7:00.
+
 
 */
