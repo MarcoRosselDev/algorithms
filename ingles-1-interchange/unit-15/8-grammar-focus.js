d4cf70f6d24e73b2ff9e3d09b2fc1e3c92b25b52
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-09 07:03:33 -0400

    ingles 1 | unit 15 | 8 grammar focus

---
+++ ingles-1-interchange/unit-15/8-grammar-focus.js
@@ -0,0 +1,20 @@
+/* 8    GRAMMAR FOCUS
+
+Formal and informal messages with tell and ask.
+
+Statements-------------------------------------------------
+I'm going to miss class tonight.
+
+Messages with a statement: tell----------------------------
+(Please) Tell him (that) I'm going to miss class.   ----> informal
+Could you tell him (that) I'm going to miss class?
+Would you tell him (that) I'm going to miss class?  ----> more formal
+
+Requests---------------------------------------------------
+Could she take a picture of the board?
+
+Messages with a request: ask-------------------------------
+(Please) Ask her to take a picutre of the board.     ----> informal
+Could you ask her to take a picutre of the board.
+Would you ask her to take a picutre of the board.    ----> more formal
+*/
