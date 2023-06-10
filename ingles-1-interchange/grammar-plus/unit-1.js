da6cee2edb04cacf858b70f19b7f21232e24bf1c
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-10 03:25:31 -0400

    ingles 1 | grammar plus | unit 1

---
+++ ingles-1-interchange/grammar-plus/unit-1.js
@@ -0,0 +1,11 @@
+/* UNIT 1       GRAMMAR PLUS
+
+1   Statements whit be: possessive adjectives
+
+Don't confuse contractions of be with possessive adjectives:
+    You're a student.
+    Your class is English 1. (NOT: You're class is English 1)
+    He's my classmate.
+    His name is Ricardo. (NOT: He's name is Ricardo.)
+
+*/
