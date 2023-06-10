d9cdfea0348f8a60794c8deb15b9b6e22ad28d1c
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-10 07:21:41 -0400

    ingles 1 | grammar plus | unit 2 | a

--- ingles-1-interchange/grammar-plus/unit-2.js
+++ ingles-1-interchange/grammar-plus/unit-2.js
@@ -16,3 +16,22 @@ he/she/it live? Where do I/you/we/they live?
 Don't add -s to the verb: Where does she live? (NOT: Where does she lives?)
 
 */
+
+/* Complete the conversations with the correct form of the verbs is parentheses.
+
+1. 
+A: I have (have) good news! Mona has (have) a new job.
+B: How does she like (like) it?
+A: She loves (love) it. The hours are great.
+B: What time she  (start)?
+A: She  (start) at ten and  (inish) at four.
+
+2. 
+A: What you  (do)?
+B: I’m a teacher.
+A: What you  (teach)?
+B: I  (teach) Spanish and English.
+A: Really? My sister  (teach) English, too.
+2
+
+*/
