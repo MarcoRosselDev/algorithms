d72d1b689e7857d66dd12e5669f4083bdf2736f9
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-10 03:30:01 -0400

    ingles 1 | grammar plus | unit 1 | a

--- ingles-1-interchange/grammar-plus/unit-1.js
+++ ingles-1-interchange/grammar-plus/unit-1.js
@@ -9,3 +9,22 @@ Don't confuse contractions of be with possessive adjectives:
     His name is Ricardo. (NOT: He's name is Ricardo.)
 
 */
+
+/* Choose the correct words.
+
+1. This is / are Dulce Castelo. She’s / Her a new student from Santo Domingo.
+    This is Dulce Castelo. She's a new students from Santo Domingo.
+
+2. My name am / is Sergio. I’m / He’s from Brazil.
+    My name is Sergio. I'm from Brazil.
+
+3. My brother and I is / are students here. Our / We’re names are Nate and Chad.
+    My brother and I are students here. Our names are Nate and Chad.
+
+4. He’s / His Kento. He’s / His 19 years old.
+    He's Kento. He's 19 years old.
+
+5. They’re / Their in my English class. It’s / Its a big class.
+    They're in my English class. It's a big class.
+
+*/
