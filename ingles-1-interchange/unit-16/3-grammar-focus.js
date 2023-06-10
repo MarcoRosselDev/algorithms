0825974249d765858a1267a80cf10ca257a4e0fb
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-10 01:20:00 -0400

    ingles 1 | unit 16 | 3 grammar focus | A

--- ingles-1-interchange/unit-16/3-grammar-focus.js
+++ ingles-1-interchange/unit-16/3-grammar-focus.js
@@ -19,3 +19,19 @@ With the comparative.
     My hair is longer now.
 
 */
+
+/* A
+
+1.  I dress differently now.
+2.  I've changed my hairstyle.
+3.  I've made some new frends.
+4.  I got a pet.
+5.  I've joined a gym.
+6.  I moved into my own apartment.
+7.  I'm more aoutgoing than before.
+8.  I'm not in high school anymore.
+9.  My life is easier now.
+10. I got married.
+
+
+*/
