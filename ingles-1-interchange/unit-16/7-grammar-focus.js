0352622a99811c8055c34339f94dbf2ee77f6e41
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-10 02:05:54 -0400

    ingles 1 | unit 16 | 7 grammar focus

---
+++ ingles-1-interchange/unit-16/7-grammar-focus.js
@@ -0,0 +1,13 @@
+/* 7    GRAMMAR FOCUS
+
+Verb + infinitive
+
+What are you going to do this year?
+    I'm (not) going to take a trip to the Caribbean.
+    I (don't) plan to take guitar lessons.
+    I (don't) want to learn to dance.
+
+    I hope to get a new job.
+    I'd like to travel around the United States.
+    I'd love to play the guitar.
+*/
