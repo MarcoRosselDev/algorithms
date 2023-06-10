6c62f5ca8c92d133be597b20d6b8b91f1099b438
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-10 06:26:14 -0400

    ingles 1 | grammar plus | unit 2

---
+++ ingles-1-interchange/grammar-plus/unit-2.js
@@ -0,0 +1,18 @@
+/* UNIT 2
+
+1.  Simple present Wh-questions and statements
+
+Statements-----------------------------------------------------
+Verbs with he/she/it end in -s: He/She walks to school, BUT I/You/We/They walk 
+to school.
+
+Have, go, and do are irregular with he/she/it: She has a class at 1:00.
+He goes to school at night. She does her homework before school.
+
+Wh-questions---------------------------------------------------
+Use does in questions with he/she/it and do with all the others: Where does
+he/she/it live? Where do I/you/we/they live?
+
+Don't add -s to the verb: Where does she live? (NOT: Where does she lives?)
+
+*/
