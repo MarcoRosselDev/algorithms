01bbea7aa130b31c7f1802cd41fb7c115e2029bc
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-10 01:42:52 -0400

    ingles 1 | unit 16 | 5 word power

---
+++ ingles-1-interchange/unit-16/5-word-power.js
@@ -0,0 +1,33 @@
+/* 5    WORD POWER
+
+A   Complete the word map with pharases from the list. 
+Then add two more examples to each category.
+
+APPEARANCE
+
+dye my hair
+grow a beard
+pierce my ears
+wear contact lenses
+get a haircut
+dress better
+
+MONEY
+
+get a bank loan
+get a credit card
+get a pay raise
+open a savings account
+get a mortgage
+support a charity
+
+SKILLS
+
+improve my English vocabulary
+learn a new sport
+learn how to dance
+start a new online course
+learn how to paint
+take an art class
+
+*/
