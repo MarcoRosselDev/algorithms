e17be2e3e71d15b537b62ada579230bdb2eb05f5
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-09 23:18:57 -0400

    ingles 1 | unit 16 | 3 grammar focus

---
+++ ingles-1-interchange/unit-16/3-grammar-focus.js
@@ -0,0 +1,21 @@
+/* 3    GRAMMAR FOCUS
+
+Describeing changes
+
+With the present tense
+    I'm not in school anymore.
+    I wear contacts now.
+
+With the past tense
+    I majored in business administration.
+    I got engaged.
+
+With the present perfect
+    I've just started a new job.
+    I've bought a new apartment.
+
+With the comparative.
+    It's less noisy than downtown.
+    My hair is longer now.
+
+*/
