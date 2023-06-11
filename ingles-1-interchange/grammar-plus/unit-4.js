fc42e2acb7a745c652c6fb43037f08d89dd505b6
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-11 11:11:23 -0400

    ingles 1 | grammar plus | unit 4

---
+++ ingles-1-interchange/grammar-plus/unit-4.js
@@ -0,0 +1,27 @@
+/* UNIT 4   
+
+1   Simple present questions;   short answers
+
+Use do + base form for yes/no questions and short answers with I/you/we/they:
+    Do I/you/we/they like rock?
+    Yes, I/you/we/they do.
+    No, I/you/we/they don't.
+
+Use does  in yes/no questions and short answers with he/she/it:
+    Does he/she like rock?
+    Yes, he/she does.
+    No, he/she doesn't.
+
+Use don't and doesn't + base form for negative statements:
+    I don't like horror movies.
+    He doesn't like action movies.
+
+Remember: Don't add -s to the base form:
+    Does she like rock?
+    NOT = Does she likes rock?
+Subject pronouns (I, you , he, she, it, we, they) usually come before a verb.
+Object pronouns (me, him, her, it, us, them) usually come after a verb:
+    He likes her, but she doesn't like him.
+
+
+*/
