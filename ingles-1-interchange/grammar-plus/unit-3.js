fe1be22b51cf3f95e7ed407a144b4e5ffc835de2
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-11 08:16:33 -0400

    ingles 1 | grammar plus | unit 3 | 1

---
+++ ingles-1-interchange/grammar-plus/unit-3.js
@@ -0,0 +1,17 @@
+/* 1    Demonstratives: one, ones
+
+With singular nouns, use this for a thing that is nearby and that for
+a thing that is not nearby:
+    How much is this hat here?
+    How much is that hat over there?
+
+With plural nouns, use these for things that are nearby and those for 
+things that are not nearby:
+    How much are these earrings here?
+    How much are those earrigs over there?
+
+Use one to replace a singular noun: 
+    I like the green begs. 
+    I like the green ones. 
+
+*/
