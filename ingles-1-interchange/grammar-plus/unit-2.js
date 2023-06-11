8be1d1e05e28b72243d83f0f6ed5431c50a06374
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-11 07:46:13 -0400

    ingles 1 | grammar plus | unit 2 | B

--- ingles-1-interchange/grammar-plus/unit-2.js
+++ ingles-1-interchange/grammar-plus/unit-2.js
@@ -23,15 +23,36 @@ Don't add -s to the verb: Where does she live? (NOT: Where does she lives?)
 A: I have (have) good news! Mona has (have) a new job.
 B: How does she like (like) it?
 A: She loves (love) it. The hours are great.
-B: What time she  (start)?
-A: She  (start) at ten and  (inish) at four.
+B: What time she start (start)?
+A: She starts (start) at ten and finishes (finish) at four.
 
 2. 
-A: What you  (do)?
+A: What do you do (do)?
 B: I’m a teacher.
-A: What you  (teach)?
-B: I  (teach) Spanish and English.
-A: Really? My sister  (teach) English, too.
+A: What do you teach (teach)?
+B: I teach (teach) Spanish and English.
+A: Really? My sister teaches (teach) English, too.
 2
 
 */
+
+const x = 'she/he/it';
+const y = 'I/you/they/we';
+
+// Statements
+
+// verbs of x add -s -------> She works at night.
+// verbs of y don't --------> You work at night
+
+// if x then verbs go, have, and do are irregular.
+// like -------> She has a new job.
+// like -------> He does home work.
+// like -------> It goes to the north.
+
+
+// Wh-questions
+
+// verbs with x use does.
+// verbs with y use do.
+
+// don't add -s at the verb.
