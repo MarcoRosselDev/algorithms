47ea55b504be01cff5a95c512f3a4ebe8a09ca37
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-12 03:00:27 -0400

    ingles 1 | grammar plus | unit 5 | present continuous

---
+++ ingles-1-interchange/unit-5/present-contnuous.js
@@ -0,0 +1,24 @@
+/* 5    GRAMMAR FOCUS
+
+Present continuous
+
+Are you living at home now?
+    Yes, I am.
+    No, I'm not.
+
+Is your sister working in another city?
+    Yes, she is.
+    No, she's not./No, she isn't.
+
+Are your parents studying English this year?
+    Yes, they are.
+    No, they're not./No, they aren't.
+
+------------------------------------------------
+
+Where are you working now?
+    I'm not working. I need a job.
+    He's traveling in Thailand.
+    They're studying for their exams.
+
+*/
