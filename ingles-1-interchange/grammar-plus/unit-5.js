58c077c5c0847e6ab0d6770cb80f60fd413201a5
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-11 23:50:35 -0400

    ingles 1 | grammar plus | unit 5

---
+++ ingles-1-interchange/grammar-plus/unit-5.js
@@ -0,0 +1,18 @@
+/* UNIT 5
+
+1   Present continuous
+
+Use the present continuous to talk about actions that are happening
+now:
+    What are you doing (these day)? 
+    I'm studying English.
+
+The present continuous is present of be + -ing.
+For verbs endign in e, drop the e and add -ing:
+    have ---->  having
+    live ---->  living
+
+For verbs ending in vowel + consonant, double the consonant and add -ing:
+    sit  ---->  sitting.
+
+*/
