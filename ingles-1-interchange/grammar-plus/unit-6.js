0650dd2abc9bc53447eb8016df0015a585ae2af1
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-12 05:23:38 -0400

    ingles 1 | grammar plus | unit 6

---
+++ ingles-1-interchange/grammar-plus/unit-6.js
@@ -0,0 +1,23 @@
+/*  UNIT 6
+
+1.  Adverbs of frequency
+
+Adverbs of frequency (always, almost always, usually, often, 
+sometimes, hardly ever, almost never, never) usually come before
+the main verb:
+    She never plays tennis.
+    I almost always eat breakfast.
+
+    BUT. Adverbs of frequency usually come after the verb be:
+    I'm always late.
+
+Usually and sometimes can begin a sentence:
+    Usually I walk to work.
+    Sometimes I exercise in the morning.
+
+Some frequency expressions usually come at the end of a sentence:
+    every day, once a week, twice a month, three times a year:
+        Do you exercise every day?
+        I exercise three times a week.
+
+*/
