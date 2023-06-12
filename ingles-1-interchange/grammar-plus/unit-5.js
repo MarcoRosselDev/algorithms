82b7b29a9fc8e73cc9b0d468f27921bfc74aa1a0
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-12 00:16:57 -0400

    ingles 1 | grammar plus | unit 5 | 1

--- ingles-1-interchange/grammar-plus/unit-5.js
+++ ingles-1-interchange/grammar-plus/unit-5.js
@@ -16,3 +16,33 @@ For verbs ending in vowel + consonant, double the consonant and add -ing:
     sit  ---->  sitting.
 
 */
+
+/* Write questions with the words in parentheses and the present continuous.
+Then complete the responses with short answers or the verbs in the box.
+
+[live, study, take, teach, work]
+
+1.  A:  (what / your sister / do / these days) 
+        What’s your sister doing these days?
+
+    B:  She’s teaching English.
+
+    A:  Really? (she / live / abroad)
+        Really? Is she living abroad?
+
+    B:  Yes, she is . She's living in South Korea.
+
+2.  A:  (how / you / spend / your summer)
+        How are you spending you summer?
+
+    B:  I part-time. I two classes also.
+        I'm working part-time. I'm taking two classes also.
+
+    A:  (what / you / take)
+        What are you takin?
+
+    B:  My friend and I photography and Japanese. We like our classes a lot.
+        My friend and I are stydying photography and Japanes.
+        We like our classes a lot.
+
+*/
