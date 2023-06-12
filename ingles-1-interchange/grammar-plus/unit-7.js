6b4fe1112edc765109f1be37d545096d08c31b29
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-12 13:12:31 -0400

    ingles 1 | grammar plus | unit 7

---
+++ ingles-1-interchange/grammar-plus/unit-7.js
@@ -0,0 +1,13 @@
+/* unit 7   
+
+1   Simple past
+
+Use did with the base form -not the past form -of the main verb in questions:
+    How did you spend the weekend?
+    NOT: How did you spent ...?
+
+Use didn't with the base form in negative statements:
+    We didn't go shopping.
+    NOT: We didn't went shopping
+
+*/
