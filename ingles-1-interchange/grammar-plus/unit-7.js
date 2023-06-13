69b49bca9e0f831dddd1d05618a4686c73bdfb34
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-13 08:50:05 -0400

    ingles 1 | grammar plus | unit 7 | 1 a

--- ingles-1-interchange/grammar-plus/unit-7.js
+++ ingles-1-interchange/grammar-plus/unit-7.js
@@ -13,7 +13,17 @@ Use didn't with the base form in negative statements:
 Complete the conversation.
 
 A:  Did you have a good weekend?
-B:  Yes, I did. I have a great time. My sister and I was whopping on Saturday.
-    We spend all day at the mall.
+B:  Yes, I did. I had a great time. My sister and I went shopping on Saturday.
+    We spent all day at the mall.
+A:  Did you buy anything special?
+B:  I bought a new laptop. And I got some new clothes, too.
+A:  Lucky you! What clothes did you buy?
+B:  Well, I needed some new boots. 
+    I got some great ones at Great Times Department Store.
+    What about you? What did you do on Saturday?
+A:  I didn't do anything special. I stayed home and worked around the house.
+    Oh, but I saw a really good movie on TV.
+    And then I made dinner with my mother.
+    I actually anjoyed the day.
 
 */
