1aa9e368bb4a2e48af390c01bc0ff8003f0c22f1
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-13 15:00:38 -0400

    ingles 1 | grammar plus | unit 9 | 1

--- ingles-1-interchange/grammar-plus/unit-9.js
+++ ingles-1-interchange/grammar-plus/unit-9.js
@@ -20,4 +20,34 @@ Then write answers using the phrases in the box.
 [blond, brown eyes, contact lenses,
 tall and good-looking, 6 foot 2, 26-two years older than me]
 
+a.  brother | like | look | what | your | does
+    What does your brother look like?
+
+b.  He's tall and good-looking.
+
+a.  tall | is | how | he
+    How tall he is?
+
+b.  he's 6 foot 2
+
+a.  he | does | glasses | wear
+    Does he wear glasses?
+
+b.  No, he doesn't. He wears contact lenses.
+
+a.  What | hair | color | his | is
+    what color is his hair?
+
+b.  He has blond hair.
+
+a.  He | does | blue | have | eyes
+    Does he have blue eyes?
+
+b.  No, he doesn't. He has brown eyes.
+
+a.  old | he | how | is
+    How old is he?
+
+b.  He's 26 years hold, two years older than me.
+
 */
