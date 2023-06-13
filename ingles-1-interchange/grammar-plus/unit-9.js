0776619638b822ed0cf72a5cf863ecb9920b8053
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-13 14:43:06 -0400

    ingles 1 | grammar plus | unit 9

---
+++ ingles-1-interchange/grammar-plus/unit-9.js
@@ -0,0 +1,23 @@
+/* UNIT 9
+
+1   Describing people
+
+Use have or is to describe eye and hair color:
+    I have brown hair.
+    My hair is brown.
+    He has blue eyes.
+    His eyes are blue.
+
+Don't confuse How and What in questions:
+    How tall are you?
+        NOT----> What tall are you?
+    What color is your hair?
+        NOT----> How color is your hair?
+
+Unscremble the questions.
+Then write answers using the phrases in the box.
+
+[blond, brown eyes, contact lenses,
+tall and good-looking, 6 foot 2, 26-two years older than me]
+
+*/
