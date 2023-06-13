b35687fd0f1d713b36af5117b721085067c3c93c
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-13 13:49:04 -0400

    ingles 1 | grammar plus | unit 8

---
+++ ingles-1-interchange/grammar-plus/unit-8.js
@@ -0,0 +1,17 @@
+/* UNIT 8
+
+1.  There is, there are: one, any, some
+
+Don't use a contraction in a short answer with Yes:
+    Is there a hotel near here?
+        Yes, there is
+            NOT:-----> Yes, there's.
+
+Use some in affirmative statements and any in negative statements:
+    There are some grocery stores in my neighborhood, 
+    but there aren't any restaurans.
+    
+Use any in most questions:
+    Are there any nice stores around here?
+
+*/
