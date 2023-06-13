69bef3db897b0ba3d0ef5089459db9f2196efb06
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-13 14:00:18 -0400

    ingles 1 | grammar plus | unit 8 | 1

--- ingles-1-interchange/grammar-plus/unit-8.js
+++ ingles-1-interchange/grammar-plus/unit-8.js
@@ -15,3 +15,18 @@ Use any in most questions:
     Are there any nice stores around here?
 
 */
+
+/* Complete the conversation.
+Choose the correct words.
+
+1.  a.  Are there any supermarkets in this neighborhood?
+    b.  No, there aren't, but there are some on Main Street.
+    a.  And is there a post office near here?
+    b.  Yes, there is. It's across from the bank.
+
+2.  a.  Is there a gas station around here?
+    b.  Yes, there's one behind the shopping center.
+    a.  Great! And are there any coffee shops nearby?
+    b.  Yes, there's a good one in the shopping center.
+
+*/
