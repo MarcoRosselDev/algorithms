39dba71b76669ef269ec25ec5c70984bf7641540
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-13 08:04:17 -0400

    ingles 1 | grammar plus | unit 7 | simple past

---
+++ ingles-1-interchange/unit-7/simple-past.js
@@ -0,0 +1,33 @@
+/* Simple past
+
+Did you work on Saturday?   ---------> did + verb in regular from to questions.
+    Yes, I did. I worked all day    ------> verb in past simple to answers or talk about past events.
+    No, I didn't work at all.       ------> verb in regular form to negative sentences
+
+Did you go anywhere last weekend?
+    Yes, I did. I went to the movies.
+    No, I didn't. I didn't go anywhere.
+
+What did Neil you do on Saturday?
+    He stayed home and studied for a test.
+
+How did Cara spend her weekend?
+    She went to a club and danced with some friends.
+
+regular verbs
+work    worked
+invite  invited
+study   studied
+stop    stopped
+
+irregular verbs
+buy     bought
+do      did
+drive   drove
+have    had
+go      went
+sing    sang
+see     saw
+spend   spent
+
+*/
