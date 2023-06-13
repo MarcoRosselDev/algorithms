9d7acc8bf0a620d4b3dc91d83be15c0486b464a9
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-13 13:39:48 -0400

    ingles 1 | grammar plus | unit 7 | 2

--- ingles-1-interchange/grammar-plus/unit-7.js
+++ ingles-1-interchange/grammar-plus/unit-7.js
@@ -25,5 +25,40 @@ A:  I didn't do anything special. I stayed home and worked around the house.
     Oh, but I saw a really good movie on TV.
     And then I made dinner with my mother.
     I actually anjoyed the day.
+*/
+
+/* 2    Past of be
+
+Present     past
+
+am/is       was
+are         were
+
+Rewrite the sentences. Find another way to each sentence using was,
+wasn't, were, or weren't and the words in parentheses.
+
+1.  Bruno didn't come to class yesterday. (in class)
+    Bruno wasn't in class yesterday.
+
+2.  He worked all day. (at work)
+    He was at work all day.
+
+3.  Bruno and his co-workers worked on Saturday, too (at work)
+    Bruno and his co-workers were at work on Saturday.
+
+4.  They didn't go to work on Sunday. (at work)
+    They weren't at work on Sunday.
+
+5.  Did Bruno stay home on Sunday?(at home)
+    Was Bruno stay at home on Sunday?
+
+6.  Where did Bruno go on Sunday? (on Sunday)
+    Where was Bruno go on Sunday?
+
+7.  He and his brother went to a baseball game. (at a baseball game)
+    He and his brother were at a baseball game.
+
+8.  They stayed at the park untill 7:00. (at the park)
+    They were at the park untill 7:00.
 
 */
