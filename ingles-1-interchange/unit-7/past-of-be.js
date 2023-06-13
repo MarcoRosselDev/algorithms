0c69d549efd3bf52269ac0f91de8f39fa8e7b555
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-13 08:32:54 -0400

    ingles 1 | grammar plus | unit 7 | past of be

---
+++ ingles-1-interchange/unit-7/past-of-be.js
@@ -0,0 +1,40 @@
+/* Past of be
+
+Were you in California?
+    Yes, I was
+
+Was the weather OK?
+    No, it wasn't
+
+Were you and your cousing on vacation?
+    Yes, we were.
+
+Were your parents there?
+    No, they away for a week.
+
+How long were you away?
+    I was away for a week.
+
+How was your vacation?
+    It was excellent!
+
+--------------------------------------------------
+Complete these conversations.
+Then practice with a partner.
+
+1.  a.  Were you in New York last weekend?
+    b.  No, I wasn't. I was in Chicago.
+    a.  How was it?
+    b.  It was great! But it was cold and windy as usual.
+
+2.  a.  How long were your parents in Chile?
+    b.  They were there for two weeks.
+    a.  Were they in Santiago the whole time?
+    b.  No, they weren't. They also went to Valparaiso.
+
+3.  a.  Were you away last week?
+    b.  Yes, I was in Madrid.
+    a.  Really? How long were you there?
+    b.  For almost a week. I was there on business.
+
+*/
