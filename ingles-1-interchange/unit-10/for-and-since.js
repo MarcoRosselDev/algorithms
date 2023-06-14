9ed44ed17d2d2f663fbe34c4aca8c5c2582b53d0
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-14 19:42:33 -0400

    ingles 1 | grammar plus | unit 10 | for and since

---
+++ ingles-1-interchange/unit-10/for-and-since.js
@@ -0,0 +1,24 @@
+/* For and since
+
+How long did you live in Miami?
+    I lived there for four years. It was a great experience.
+
+How long have you lived in Orlando?
+    I've lived here for three years. I'm very happy here.
+    I've worked at the hotel since last year. I love it there.
+
+*/
+
+/* C    Complete these sentences with for o since.
+Then compare with a partner.
+
+1.  Maura was in Central America for a mont last year.
+2.  I've been a collage student for almost four years.
+3.  Hiroshi has been at work since 6am,
+4.  I haven't gone to a party for a long time.
+5.  Sean lived in Bolivia for two year as a kid.
+6.  My parents have been on vacation since Monday.
+7.  Jennifer was engaged to Theo for six months.
+8.  Alex and Brianna have been best friends since high school.
+
+*/
