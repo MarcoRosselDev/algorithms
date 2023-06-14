81d3b04929368f83f862c66a7e412e320f21c12e
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-14 07:22:47 -0400

    ingles 1 | grammar plus | unit 10

---
+++ ingles-1-interchange/grammar-plus/unit-10.js
@@ -0,0 +1,23 @@
+/* UNIT 10
+
+1   Present perfect:    already, yet
+
+Use the present perfect for actions that happend some time in the past.
+
+Use yet in questions and negative statements:
+    Have you checked your email yet?
+    No, I haven't turned on my computer yet.
+
+Use already in affirmative statements:
+    I've already checked my email.
+
+*/
+
+/* A    Complete the conversations with the present perfect
+of the verbs in parentheses and short answers.
+
+1.  a.  Has Leslie called (call) you lately?
+    b.  No, she don't called (not call) me, but I've gotten/(have/has) got some
+        emails from her.
+
+*/
