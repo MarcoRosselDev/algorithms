11317724e271d5c8fc3a696cfb8ee5f0f004b5bb
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-14 08:56:50 -0400

    ingles 1 | grammar plus | unit 10 | present perfect

---
+++ ingles-1-interchange/unit-10/present-perfect.js
@@ -0,0 +1,40 @@
+/* Present
+
+Regular past participles
+
+visit       visited
+like        liked
+stop        stoped
+try         tried
+
+Irregular past participles
+
+be          been
+do          did
+eat         eaten
+go          gone
+have        had
+hear        heard
+make        made
+ride        ridden
+see         seen
+
+*/
+
+/* B    Complete these conversations using the present perfect.
+Then practice with a partenr.
+
+1.  a.  Have you done much exercise this week?
+    b.  Yes, I've already been to Pilates class four times.
+
+2.  a.  Have you played any sport this month?
+    b.  No, I haven't had the time .
+
+3.  a.  How many movies have you been to this month?
+    b.  Actually, I haven't seen any yet.
+
+4.  a.  Have you been to any interesting parties recently?
+    b.  No, I haven't gone to any paties for quite a while.
+
+
+*/
