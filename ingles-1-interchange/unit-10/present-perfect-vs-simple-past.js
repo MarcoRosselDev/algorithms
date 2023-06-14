4d6cf4c15d1999ff343444adc9b7d118618a3440
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-14 14:03:27 -0400

    ingles 1 | grammar plus | unit 10 | present perfect vs simple past

---
+++ ingles-1-interchange/unit-10/present-perfect-vs-simple-past.js
@@ -0,0 +1,20 @@
+/* Present perfect vs simple past
+
+Have you ever eaten Cuban food?
+---Use the present perfect for an indefinite time in the past.-----
+Yes, I have. I've had it many times.
+No, I haven't. I haven't tried it yet.
+---Use the simpel past for a specific event in the past.-----------
+    I ate a lot of Cuban food when I lived in Miami.
+    No, I never tried it when I lived in Miami.
+
+Have you ever seen an alligator?
+---Use the present perfect for an indefinite time in the past.-----
+    Yes, I have. I've seen a few alligator in my life.
+    No, I haven't. I've never seen one.
+---Use the simpel past for a specific event in the past.-----------
+    I saw a big alligator at the new park last week.
+    I didn't go to the alligator park last week, so
+    I didn't see any.
+
+*/
