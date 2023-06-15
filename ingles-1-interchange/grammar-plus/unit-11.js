80758db028323df73ea6fbf0675c2acdcd1a8589
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-15 10:53:54 -0400

    ingles 1 | grammar plus | unit 11

---
+++ ingles-1-interchange/grammar-plus/unit-11.js
@@ -0,0 +1,22 @@
+/* UNIT 11
+
+1   Adverbs before adjective
+
+Use a/an with (adverb) + adjective + singular noun:
+    It's a very modern city.
+    It's an expensive city.
+
+Don't use a/an with (adverb) + adjective:
+    It's really interesting
+    NOT:    It's a rally interesting.
+
+Read the sentences.
+Add a or an where it's necessary to complete the sentences.
+
+1.  Brasilia is an extremely modern city.
+2.  Seoul is a very interestin place.
+3.  Santiago is a pretty exciting city to visit.
+4.  Montreal is beautiful city, and it's fairly old.
+5.  London has a really busy airport.
+
+*/
