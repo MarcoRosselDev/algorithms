7b548a3b6f6622e3d2e24faccda355470dc4db5a
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-15 11:28:24 -0400

    ingles 1 | grammar plus | unit 12

---
+++ ingles-1-interchange/grammar-plus/unit-12.js
@@ -0,0 +1,9 @@
+/* UNIT 12
+
+1.  Adjective + infinitive: noun + infinitive
+
+In negative statements, not comes before the infinitive:
+    With a cold, it's important not to exercise too hard.
+
+
+*/
