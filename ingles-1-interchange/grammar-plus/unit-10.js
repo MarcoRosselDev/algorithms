2657975eaccd0be0e45f9b62b4e7b03ad7c7411c
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-15 10:25:15 -0400

    ingles 1 | grammar plus | unit 10 | 1 B

--- ingles-1-interchange/grammar-plus/unit-10.js
+++ ingles-1-interchange/grammar-plus/unit-10.js
@@ -22,8 +22,22 @@ of the verbs in parentheses and short answers.
 2.  a.  Have you and Jan been lunch yet?
     b.  No, we haven't. We're thinking of going to Tony's. have you tried it yet?
         Come with us.
-    a.  Thanks. I didn't eaten  there yet, but I heard it's very good.
+    a.  Thanks. I didn't eaten  there yet, but I heard it's pretty good.
     b.  Oh, yes -it's excellent!
 
+B   Look at things Matt said.
+    Put the edverb in the correct place in the second sentence.
+
+1.  I'm very hungry. I haven't eaten yet.
+2.  I don't need any groceries. I've already gone shopping.(already)
+3.  What have you done? Have you been to the zoo yet? (yet)
+4.  I called my parents before dinner. I've already talked to them. (already)
+
+
+*/
+
+/* 2    Present perfect vs. simple past
+
+Don't
 
 */
