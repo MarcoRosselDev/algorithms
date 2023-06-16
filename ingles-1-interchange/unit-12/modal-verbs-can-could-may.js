1a7e33754e12e9a43433cae6565354d27c8f7753
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-16 11:11:23 -0400

    ingles 1 | grammar plus | unit 12 | modal verbs can, could and may

---
+++ ingles-1-interchange/unit-12/modal-verbs-can-could-may.js
@@ -0,0 +1,13 @@
+/* Modal verbs can, could, and may for requests;    suggestions
+
+Can/May I help you?
+    Can  I  have bag of cough drops?
+    Could I have something for a cough?
+    May I have a bottle of pain medicine?
+
+What do you suggest/have for a backache?
+    You could try this new cream.
+    You should get a heating pad.
+    Why don't you try these pills?
+
+*/
