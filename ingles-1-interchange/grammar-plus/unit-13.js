b3f0697dc4e1f2f3bde0a700b91043b4c52bfe81
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-16 14:20:52 -0400

    ingles 1 | grammar plus | unit 13

---
+++ ingles-1-interchange/grammar-plus/unit-13.js
@@ -0,0 +1,19 @@
+/* UNIT 13
+
+1.  So, too, neigther, either
+
+Use so or too after an affirmative statement:
+    I'm crazy about suchi. So am I.
+    I'm crazy about suchi. I am, too.
+
+Use neither or not either after a negative statement:
+    I don't like fast food.
+        Neither do I.
+        I don't either.
+
+With so and neither, the verb comes before the subject:
+    So am I.
+    Neither do I.
+    NOT:    Neither I do.
+
+*/
