15fb3eb116a21ed2f80a76355a06238fe967078d
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-16 09:59:01 -0400

    ingles 1 | grammar plus | unit 12 | adjective infinitive

---
+++ ingles-1-interchange/unit-12/adjective-infinitive.js
@@ -0,0 +1,8 @@
+/* Adjective + infinitive;  noun + infinitive
+
+What should you do for a cold?
+    It's important                  to get some rest.
+    It's sometimes helpful          to drink garlic tea.
+    It's good idea                  to take some vitamin C.
+
+*/
