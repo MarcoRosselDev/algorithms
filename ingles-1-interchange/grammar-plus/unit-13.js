0f3eb3262d8be76887635a889c1b3116e0f07fcb
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-16 14:28:36 -0400

    ingles 1 | grammar plus | unit 13 | 1

--- ingles-1-interchange/grammar-plus/unit-13.js
+++ ingles-1-interchange/grammar-plus/unit-13.js
@@ -17,3 +17,22 @@ With so and neither, the verb comes before the subject:
     NOT:    Neither I do.
 
 */
+
+/* A    Choose the correct response to show that B agrees with A
+
+1.  a.  I'm in the mood for something salty.
+    b.  I am, too.
+
+2.  a.  I can't stand fast food.
+    b.  I can't eihter.
+
+3.  a.  I really like Korean food.
+    b.  So do I.
+
+4.  a.  I don't eat Frenche food very often.
+    b.  I don't either.
+
+5.  a.  I'm not crazy about chocolate.
+    b.  Neither am I.
+
+*/
