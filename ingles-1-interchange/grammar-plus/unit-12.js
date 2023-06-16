1c41f836c8041d83a091db44bec2ca2d200efbc8
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-16 11:48:06 -0400

    ingles 1 | grammar plus | unit 12 | 1

--- ingles-1-interchange/grammar-plus/unit-12.js
+++ ingles-1-interchange/grammar-plus/unit-12.js
@@ -6,4 +6,25 @@ In negative statements, not comes before the infinitive:
     With a cold, it's important not to exercise too hard.
 
 
+Rewrite the sentences using the words in parentheses.
+Add not when necessary.
+
+1.  For a bad headache, you should relax and close your eyes. (a good idea)
+    For a dab headache, it's a good idea to relax and close your eyes.
+
+2.  You should put some cold tea on that sunburn. (sometimes helpful)
+    For a sunburn, it's sometimes helpful to put some cold tea on it.
+
+3.  For a backache, you should take some pain medicine. (important)
+    For a backache, it's important to take some pain medicine.
+
+4.  For a cough, you shouldn't drink milk. (important)
+    For a cough, it's important not to drink milk.
+
+5.  For a cold, you should take a hot bath. (sometimes helpful)
+    For a cold, it's sometimes helful to take a hot bath.
+
+6.  When you feel stressed, you shouldn't drink a lot of coffee. (a good idea)
+    When you feel stressed, it's a good idea not to drink a lot of coffee.
+
 */
