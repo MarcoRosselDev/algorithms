a54706486ebf3f099aed057b945e8ae8d1f4fb71
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-17 12:18:42 -0400

    ingles 1 | grammar plus | unit 13 | modal verbs  2

--- ingles-1-interchange/unit-13/modal-verbs.js
+++ ingles-1-interchange/unit-13/modal-verbs.js
@@ -17,3 +17,25 @@ Would you like anything else?
     That's all, thanks
 
 */
+
+/* Complete the conversation.
+
+Server:     What would you like to order?
+Customer:   I'll have the spicy fish.
+Server:     would you like salad or potatos?
+Customer:   I'd like potatoes, please.
+Server:     OK. And would you like anything to drink?
+Customer:   I'll just have a glass of water.
+Server:     Would you like anything else?
+Customer:   No, that's all for now, thanks.
+
+Later
+
+Server:     Would you like dessert?     
+Customer:   Yes, I'd like ice cream.
+Server:     What flavor would you like?
+Customer:   Hmm. I'll have mint chocolate chip, please.
+Server: 
+Customer:
+
+*/
