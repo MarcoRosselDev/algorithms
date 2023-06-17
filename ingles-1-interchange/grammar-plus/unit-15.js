06cb7c6fa433450845b0bcc5e3636f2d335988ca
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-17 17:24:36 -0400

    ingles 1 | grammar plus | unit 15

---
+++ ingles-1-interchange/grammar-plus/unit-15.js
@@ -0,0 +1,16 @@
+/* UNIT 15
+
+1   Future with present continuous and be going to
+
+Use the present continuous to talk about something that is happening now:
+    What are you doing?
+        I'm stydying.
+You can also use the present continuous with time expressions to talk
+about the future:
+    What are you doing temorrow?
+        I'm working.
+
+Use be going to to talk about the future:
+    I'm going to see an old school friend tomorrow.
+
+*/
