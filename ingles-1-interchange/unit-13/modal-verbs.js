3132955af622816e73e26799fc153ed9a8cbdf8d
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-17 12:12:03 -0400

    ingles 1 | grammar plus | unit 13 | modal verbs

---
+++ ingles-1-interchange/unit-13/modal-verbs.js
@@ -0,0 +1,19 @@
+/* modal verbs would and will for requests
+
+What would you like?
+    I'd like the veggie burger.
+    I'll have a mango salad.
+
+What kind of soup would you like?
+    I'd like onion soup, please.
+    I'll have the soup of the day.
+
+What would you like to drink?
+    I'd like a lemonade.
+    I'll have a large orage juice.
+
+Would you like anything else?
+    Yes, please, I'd like some coffee.
+    That's all, thanks
+
+*/
