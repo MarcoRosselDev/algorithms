58f683dd547054ba404ec2c53ae2a11cdc0f4215
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-17 16:44:40 -0400

    ingles 1 | grammar plus | unit 14

---
+++ ingles-1-interchange/grammar-plus/unit-14.js
@@ -0,0 +1,21 @@
+/* UNIT 14
+
+1   Comparisons with adjectives
+
+Use the comparative form (adjective + -er or more + adjective) to compare two
+people, places, or things:
+    Which river is longer, The Nile or the Amazon?
+    The Nile is longer than the Amazon.
+
+Use the superlative form ( the + adjective + -est or the most + adjective)
+to compare three or more people, places, or things:
+    Which river is the longest: the Nile, the Amazon, or the Mississippi?
+    The Nile is the longest river in the world.
+
+You can use a comparative or superlative without repeating the noun:
+    Which country is larger, Canada or China?
+    Canada is lager.
+    What's the highest waterfall in the world?
+    Angel Falls is the highest.
+
+*/
