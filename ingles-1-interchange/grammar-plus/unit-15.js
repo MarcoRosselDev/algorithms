107e421f93ff33860b1ba325c8ffb5277cf64189
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-18 13:06:56 -0400

    ingles 1 | grammar plus | unit 15 | 1 a

--- ingles-1-interchange/grammar-plus/unit-15.js
+++ ingles-1-interchange/grammar-plus/unit-15.js
@@ -14,3 +14,15 @@ Use be going to to talk about the future:
     I'm going to see an old school friend tomorrow.
 
 */
+
+/* A    Read the sentences.
+        Are they present or future?
+        Write P of F.
+
+1.  Why are you wearing shorts? It's cold.  P
+2.  What are you wearing to the party on Friday? F
+3.  What are you doing this weekend? F
+4.  What are you doing? Can you please see who's at the door? P
+5.  Are you going to see a movie tonight? F  
+
+*/
