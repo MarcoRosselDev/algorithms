f31b19425612a3859164c74ae98ef20bd6759230
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-18 16:25:32 -0400

    ingles 1 | grammar plus | unit 15 | present continuous 2

--- ingles-1-interchange/unit-15/present-continuous.js
+++ ingles-1-interchange/unit-15/present-continuous.js
@@ -14,4 +14,13 @@ A
 4.  Are you staying in town next weekend?
     Do you want to go for a hike?
 
+B
+a.  I'm going to be here on Saturday, but not Sunday. Let's try to go on Saturday.
+b.  Well, my father is going to visit my brother att college.
+    But my mother and I are going to be home.
+    We'd love to come!
+c.  Sorry, I can't. I'm going to work late tonight.
+    How about tomorrow night?
+d.  Can we go to a late show?
+    I'm going to stay at the office till 7:00.
 */
