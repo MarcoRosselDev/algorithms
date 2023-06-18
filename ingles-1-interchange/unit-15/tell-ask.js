9799204555c1b060206f9d5c14c6b57e1e5b1b1c
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-18 19:08:59 -0400

    ingles 1 | grammar plus | unit 15 | tell and ask

---
+++ ingles-1-interchange/unit-15/tell-ask.js
@@ -0,0 +1,20 @@
+/* Formal and informal with tell and ask
+
+Statements
+
+I'm going to miss class tonight.
+
+Messages with a statement: tell
+    (Please) Tell him(that) I'm going to miss class.
+    Could you tell him (that) I'm going to miss class?
+    Would you tell him (that) I'm going to miss class?
+
+Requests
+    Could she take a picture of the board?
+
+Messages with a request: ask
+    (Please) Ask her to take a picture of the board.
+    Could you ask her to take a picture of the board?
+    Would you ask her to take a picture of the board?
+
+*/
