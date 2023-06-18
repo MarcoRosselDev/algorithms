30770f33ece92b21c5d1ea1131ad7d1201aa3a52
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-18 16:20:41 -0400

    ingles 1 | grammar plus | unit 15 | present continuous

---
+++ ingles-1-interchange/unit-15/present-continuous.js
@@ -0,0 +1,17 @@
+/* Complete the invitations in comlumn A with the present continuous used as future.
+Complete the responses in column B with be going to.
+
+A
+1.  What are you doing tonight?
+    Would you like to go out?
+
+2.  Are you doing anything on Friday night?
+    Do you want to see a movie?
+
+3.  We are having friends over for a barbecue on Sunday.
+    Would you and your parents like to come?
+
+4.  Are you staying in town next weekend?
+    Do you want to go for a hike?
+
+*/
