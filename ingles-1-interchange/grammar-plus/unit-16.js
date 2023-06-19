31a29961ffbd59c126a080064df742a503cfbdf0
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-19 19:19:00 -0400

    ingles 1 | grammar plus | unit 16 | 2

--- ingles-1-interchange/grammar-plus/unit-16.js
+++ ingles-1-interchange/grammar-plus/unit-16.js
@@ -25,3 +25,26 @@ buy a house | change her hairstyle | join a gym | start looking a new job
 4.  I don't eat greasy food anymore. I eat healthier food.(healthier food)
 
 */
+
+/* 2    Verb + infinitive
+
+Use the infinitive after a verb to describe future plans or things you
+want to happen:
+    I want to learn Spanish.
+
+Complete the conversation with the verbs in parentheses in the correct form.
+
+a.  Hey, Zach. What are you going to do after graduation?
+b.  Well, I plan to stay here in the city for a few months.
+a.  Really? I want to go home. I'm ready for my mom's cooking.
+b.  I undestand that, but my boss says I can keep my job for the summer.
+    So I want to work a lot of hours because I hope to make enought money
+    for a new car.
+a.  But you don't need a car in the city.
+b.  I don't plan to be here for very long.
+    In the fall, I'm going to drive across the country.
+    I really want to live in California.
+a.  California? Where in California would you like to live?
+b.  In Hollywood, of course. I'm going to be a movie star!
+
+*/
