b6457e601de10d852bf0774d813fbf6db9598fbc
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-19 17:48:46 -0400

    ingles 1 | grammar plus | unit 16 | 1

---
+++ ingles-1-interchange/grammar-plus/unit-16.js
@@ -0,0 +1,18 @@
+/* UNIT 16
+
+1   Describing changes
+
+You can several tenses to describe change - present tense, past tense, and
+present perfect
+
+A   Complete the sentences with the information in the box.
+Use the present perfect of the verbs given.
+
+buy a house | change her hairstyle | join a gym | start looking a new job
+
+1.  Chris and Brittany have bought a house. Their apartment was too small.
+2.  Josh has started looking for a new job. The one he has now is too stressful.
+3.  Shawna has changed her hairstyle. Everyone says it's more stylish.
+4.  Max has joined a gym. He feels healthier now.
+
+*/
