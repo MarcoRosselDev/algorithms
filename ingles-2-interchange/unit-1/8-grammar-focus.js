a832e01291a6609697beb10159c19add351e6bc6
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-19 20:51:29 -0400

    ingles 2 | unit 1 | 7 GRAMMAR FOCUS | a

--- ingles-2-interchange/unit-1/8-grammar-focus.js
+++ ingles-2-interchange/unit-1/8-grammar-focus.js
@@ -15,3 +15,20 @@ What sport did you use to play?
     but now I play tennis.
 
 */
+
+/* A    Complete these questions and answers.
+Then compare with a partner. 
+
+1.  a.  Did you use to have any pets when you were a kid?
+    b.  Yes, I used to have a white cat named Snowball.
+
+2.  a.  Did you and you classmates use to play together after school?
+    b.  No, we didn't use to play duting the week. We used to study a lot.
+
+3.  a.  What music did you use to listen to?
+    b.  I used to listen to rock a lot. Actually, I still do.
+
+4.  a.  What hobbies did you use to have when you were little?
+    b.  I didn't use to have any hobbies, but now I play chess every week.
+
+*/
