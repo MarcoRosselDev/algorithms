81e64d8673dc2801fa79e259ec0769dbc003b1d3
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-19 20:41:53 -0400

    ingles 2 | unit 1 | 7 GRAMMAR FOCUS

---
+++ ingles-2-interchange/unit-1/8-grammar-focus.js
@@ -0,0 +1,17 @@
+/* 8    GRAMMAR FOCUS
+
+Used to
+
+Used to refers to something that you regularly did in the past but don't do anymore.
+
+Did you use to collect things?
+    Yes, I used to collect comic books.
+    No, I didn't use to collect anything.
+    but now I collect old recors.
+
+What sport did you use to play?
+    I used to play baseball and volleyball.
+    I never used to play sports.
+    but now I play tennis.
+
+*/
