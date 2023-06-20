c6d746f9fe3eab1dffd024eacd4533dff247374d
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-19 20:04:16 -0400

    ingles 2 | unit 1 | 2 conversation

---
+++ ingles-2-interchange/unit-1/2-conversation.js
@@ -0,0 +1,20 @@
+/* 2    CONVERSATION
+
+    Where did you learn to skateboard?
+
+Isabel: Oh, I'm really sorry. Are you OK?
+Nico:   I'm fine. But I'm not very good at this.
+Isabel: Neither am I. ...Hey, I like your shirt.
+        Are you from Argentina?
+Nico:   Yes, I am, originally. I was born there.
+Isabel: Did you grow up there?
+Nico:   Yes, I did, but my family moved here 10 yerars ago,
+        when I was in middle school.
+Isabel: And where did you learn to skateboeard?
+Nico:   Here in the park. I only started about a month ago.
+Isabel: Well, it's my first time. Can you give me some lessons?
+Nico:   Sure. Just follow me.
+Isabel: By the way, my name is Isabel.
+Nico:   And I'm Nico. Nice to meet you.
+
+*/
