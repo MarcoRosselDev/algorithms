348b831eb8c0d7899394730a809756b98a9bac7f
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-19 20:10:50 -0400

    ingles 2 | unit 1 | 3 grammar focus

---
+++ ingles-2-interchange/unit-1/3-grammar-focus.js
@@ -0,0 +1,19 @@
+/* 3    GRAMMAR FOCUS
+
+Past tense
+
+Where were you born?
+    I was born in Argentina.
+
+Were you born in Buenos Aires?
+    Yes, I was.
+    No, I wasn't. I was born in Cordoba.
+
+When did you move to Los Angeles?
+    I moved here 10 years ago. I didn't speak English.
+
+Did you take English classes in Argentina?
+    Yes, I did. I took classes for a year.
+    No, I didn't. My aunt taught me at home.
+
+*/
