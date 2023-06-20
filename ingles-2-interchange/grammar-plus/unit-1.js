5aab7b2e1d6be59cb8fcf674da216bdc81b566f7
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-20 00:32:26 -0400

    ingles 2 | grammar plus | unit 1

---
+++ ingles-2-interchange/grammar-plus/unit-1.js
@@ -0,0 +1,15 @@
+/* GRAMMAR PLUS
+
+UNIT 1
+
+1   Past tense
+
+Use a form of be with born:
+    I was born here.
+    NOT: I born here.
+
+Don't use a form of be with the verb die:
+    He died last year.
+    NOT: He was died last year.
+
+*/
