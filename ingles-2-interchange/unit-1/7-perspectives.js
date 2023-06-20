70881478bc3159210f609f78b2f307e25d9b9580
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-20 00:26:50 -0400

    ingles 2 | unit 1 | 7 perspectives

---
+++ ingles-2-interchange/unit-1/7-perspectives.js
@@ -0,0 +1,19 @@
+/* 7    PERSPECTIVES
+
+A   Listen to these statements about changes.
+
+1.  When I was a kid, I never used to play sports, but now I like to keep fit.
+
+2.  I used to go out with friends a lot, but now I don't have any free time.
+
+3.   When I was younger, I didn't use to collect anything, but now I do.
+
+4.  I didn't use to be a good student, but now I love to study and learn new things.
+
+5.  I never used to follow politics, but now I read the news online every morning.
+
+6.  I used to be really neat and organized, but now I'm very messy.
+
+7.  I used to care a lot about my appearence. Now, I'm too busy to care about how I look.
+
+*/
