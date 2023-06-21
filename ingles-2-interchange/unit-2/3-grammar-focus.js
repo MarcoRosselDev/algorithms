bb81df30f81f8526e9959a817b120afc7abec030
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-20 23:17:56 -0400

    ingles 2 | unit 2 | 3 grammar focus

---
+++ ingles-2-interchange/unit-2/3-grammar-focus.js
@@ -0,0 +1,17 @@
+/* 3    GRAMMAR FOCUS
+
+Expressions of quantity
+
+With count nouns
+    There are too many cars.
+    There should be fewer cars.
+    We need more streetlights.
+    There aren't enought police officers.
+
+With noncount nouns
+    There is too much pollution.
+    There should be less pollution.
+    We need more public transportation.
+    There isn't enough parking.
+
+*/
