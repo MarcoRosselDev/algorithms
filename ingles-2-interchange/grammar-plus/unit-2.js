4028ffad0baf55201792c1d1696dc1ff4e33b2e2
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-21 22:08:02 -0400

    ingles 2 | grammar plus | unit 2

---
+++ ingles-2-interchange/grammar-plus/unit-2.js
@@ -0,0 +1,10 @@
+/* UNIT 2
+
+Count nouns have a plural form that usually ends in -s.
+Nouncount nouns don't have a plural form because you can't separate end coun
+them:
+    Are there any parking garages around here?
+    But: Is there any parking around here?
+    NOT: Are there any parkings around here?
+
+*/
