aae833440383bf971ddbce82acf1ff770cf37a04
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-21 22:31:31 -0400

    ingles 2 | unit 2 | 9 grammar focus

---
+++ ingles-2-interchange/unit-2/9-grammar-focus.js
@@ -0,0 +1,28 @@
+/* 9    GRAMMAR FOCUS
+
+Indirec questions from wh-questions
+
+Wh-questions with be----------------------------------------
+    Where is the nearest ATM?
+    Where are the estrooms?
+
+Wh-questions with do
+    How often do the buses run?
+    What time does the bookstore open?
+
+Wh-questions with can
+    Where can I catch the bus?
+
+Indirect questions-----------------------------------
+    Could you tell me where the nearest ATM is?
+    Do you know where the restrooms are?
+
+Indirect questions
+    Can you tell me how often the buses run?
+    Do you know what time the bookstore opens?
+
+Indirect questions
+    Do you know where I can catch the bus?
+
+
+*/
