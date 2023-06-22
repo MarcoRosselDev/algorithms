512c7709098c123a6df78df63258e7e714c374d4
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-21 23:04:41 -0400

    ingles 2 | unit 3 | 9 grammar focus

---
+++ ingles-2-interchange/unit-3/9-grammar-focus.js
@@ -0,0 +1,21 @@
+/* 9    GRAMMAR FOCUS
+
+Wish
+
+Use wish + past tense to refer to present wishes.
+
+I live with my parents.
+    I wish I didn't live with my parents.
+    I wish I had my own apartment.
+
+I can't move out.
+    I wish I could move out.
+
+Life is difficult.
+    I wish it were* easier.
+    I wish it weren't so difficult.
+
+My parents won't stop worrying about me.
+    I wish they would stop worriying about me.
+
+*/
