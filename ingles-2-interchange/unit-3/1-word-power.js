469eb97c3aaf4601ecedab26d7f1c4b27bfa8653
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-22 18:51:07 -0400

    ingles 2 | unit 3 | 1 word power

---
+++ ingles-2-interchange/unit-3/1-word-power.js
@@ -0,0 +1,25 @@
+/* 1    WORD POWER
+
+A   These words are used to describe houses and apartments.
+    Which are positive(P)? wich are negative(N)?
+
+bright      ---->   brillante
+comfortable ---->   Cómodo/da
+convenient  ---->   conveniente
+cramped     ---->   estrecha
+dangerous   ---->   peligroso/sa
+dark        ---->   oscuro/ra
+dingy       ---->   sucio/cia
+expensive   ---->   caro/ra
+huge        ---->   enorme
+inconvenient---->   inconveniente
+modern      ---->   moderno/na
+noisy       ---->   ruidoso/sa
+private     ---->   privado/da
+quiet       ---->   tranquilo/la
+run-down    ---->   atropellar
+safe        ---->   seguro/ra
+small       ---->   pequegnio/nia
+spacious    ---->   espacioso/sa
+
+*/
