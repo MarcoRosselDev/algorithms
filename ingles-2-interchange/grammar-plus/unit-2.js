86cf1d142f48a1d7714da0e8aebd90424ca8e9c8
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-21 22:13:15 -0400

    ingles 2 | grammar plus | unit 2 | a

--- ingles-2-interchange/grammar-plus/unit-2.js
+++ ingles-2-interchange/grammar-plus/unit-2.js
@@ -8,3 +8,22 @@ them:
     NOT: Are there any parkings around here?
 
 */
+
+/* Complete the conversations with the correct words in parentheses.
+
+1.  a.  There's too much traffic in this city.
+        There should be fewer cars downtown.
+    b.  The problem is there isn't enough public transportation.
+    a.  You're right.
+        We should have more buses.
+        There aren't enough of them during rush hour.
+    
+2.  a.  How do you like your new neighborhood?
+    b.  It's terrible, actually.
+        There's too much noise and too little parking.
+    a.  That's too bad.
+        There aren't enough parking spaces in my neghborhood aither.
+    
+
+
+*/
