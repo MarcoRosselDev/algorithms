46d5a64087ad386aadbd4958e7cfcd3002d9bf2d
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-22 19:07:33 -0400

    ingles 2 | unit 3 | 2 perspective

---
+++ ingles-2-interchange/unit-3/2-perspectives.js
@@ -0,0 +1,25 @@
+/* 2    PERSPECTIVES    How's your new apartment?
+
+A   Which opinions are about the building or the neighborhood?
+    Which area about the apartment?
+
+1.  I don't like living in an apartment.
+    We don't have as much privacy as we had in our old place.
+    -apartment
+
+2.  I just can't sleep at night.
+    The neighbors make too much noise.
+    The building isn't as quiet as our old one.
+    -building and neighborhood.
+
+3.  The new apartment is too dark and too hot.
+    There aren't enough windows.
+    -apartment
+
+4.  Our new apartment isn't big enough for our family.
+    We don't have a big kitchen anymore, so cooking is difficult.
+
+5.  The location is just as convenient as the old one,
+    but there aren't as many good restaurants around.
+
+*/
