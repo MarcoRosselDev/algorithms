e82e30fbd737c3ce862f3ae73f421bb99b73fd58
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-23 19:00:27 -0400

    ingles 2 | intro reminder | unit 1 | 9

---
+++ ingles-2-interchange/intro-reminder/unit-1/9-grammar-focus.js
@@ -0,0 +1,17 @@
+/* 9    GRAMMAR FOCUS
+
+The verb be
+
+I'm     Lena Garza.
+You're  in my class.
+She's   over there. (Andre is over there.)
+He's    in our class. (Ben is in our class.)
+It's    Garza. (My last name is Garza.)
+
+Wh-questions
+
+Are you Andrea ?
+    Yes, I am.  -------> Short answerst don't are contracted.
+    No, I'm not.
+
+*/
