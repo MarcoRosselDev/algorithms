e098e5c7209e5cd8731f71b92dd24c742f4493e8
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-23 19:40:05 -0400

    ingles 2 | intro reminder | unit 2 | 2

---
+++ ingles-2-interchange/intro-reminder/unit-2/2-articles.js
@@ -0,0 +1,13 @@
+/* 2    ARTICLES
+
+an + vowel sound    -------->   termina en vocal
+a + consonant sound -------->   termina en consonante (=/ vocal)
+
+1.  This is a book.
+2.  This is an English book.
+3.  This is an eraser.
+4.  This is a notebook.
+5.  This is a pen.
+6.  This is a clock.
+
+*/
