79653c9bf576537d61bb27895729e7577df2c7b9
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-23 18:06:40 -0400

    ingles 2 | intro reminder | unit 1

---
+++ ingles-2-interchange/intro-reminder/unit-1/8-conversation.js
@@ -0,0 +1,16 @@
+/* 8    CONVERSATION
+
+Daniel  Excuse me. Are you Andrea Clark?
+Sheila  No, I'm not. She's over there.
+Daniel  Oh, I'm sorry.
+
+Lena    Matt? This is your book?
+Matt    Oh, thank you. You're in my math class, right?
+Lena    Yes, I am. I'm Lena Garza.
+
+Jack    Hey, Christy, this is Ben.
+        He's in our history class.
+Christy Hi, Ben.
+Ben     Hi, Christy. Nice to meet you.
+
+*/
