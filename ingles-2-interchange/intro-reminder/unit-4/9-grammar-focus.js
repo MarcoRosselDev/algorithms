4ea220ddba62ff195d873c76a01a545dcbcce952
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-24 18:16:41 -0400

    ingles 2 | intro reminder | unit 4 | 8 A

---
+++ ingles-2-interchange/intro-reminder/unit-4/9-grammar-focus.js
@@ -0,0 +1,13 @@
+/* 9    GRAMMAR FOCUS
+
+Present continuous statements; conjunctions
+
+I'm wearing shoes.          I'm not.        
+You're  wearing shoes.      You're not.     You aren't.
+She's wearing shoes.        She's not.      She isn't.
+We're wearing shoes.        We're not.      We aren't.
+He's wearing shoes.         He's not.       He isn't.
+They're wearing shoes.      They're not.    They aren't.
+It's snowing.               It's not.       It isn't snowing.
+
+*/
