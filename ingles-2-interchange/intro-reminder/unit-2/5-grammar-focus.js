cbfbfac0c948e0fb535174881ff4a575ce166b2b
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-23 20:54:41 -0400

    ingles 2 | intro reminder | unit 2 | 5

---
+++ ingles-2-interchange/intro-reminder/unit-2/5-grammar-focus.js
@@ -0,0 +1,35 @@
+/* 5    GRAMMAR FOCUS
+
+This/These ----> Is/are -------> It/They; plurals
+
+This is a laptop.
+    What is that?
+    It's a laptop.
+
+These are laptops.
+    What are these?
+    They're laptops.
+
+*/
+
+/* Complete these conversations. 
+
+1.  a.  What are these?
+    b.  They're keys.
+
+2.  a.  What are these?
+    b.  They're backpacks/handbags.
+
+3.  a.  What's this?
+    b.  It's an umbrella.
+
+4.  a.  What are these?
+    b.  They're sunglasses.
+
+5.  a.  What's this?
+    b.  It's a wallet.
+
+6.  a.  What is this?
+    b.  It's a window.
+
+*/
