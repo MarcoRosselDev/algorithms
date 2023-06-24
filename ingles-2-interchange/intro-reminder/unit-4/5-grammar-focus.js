3b5deb6ec7c656c5fa693613fe3d198705d5c9a2
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-24 17:00:52 -0400

    ingles 2 | intro reminder | unit 4 | 5

---
+++ ingles-2-interchange/intro-reminder/unit-4/5-grammar-focus.js
@@ -0,0 +1,29 @@
+/* 5    GRAMMAR FOCUS
+
+Possessives
+
+Adjective
+    These are my shoes.
+    These are your shoes.
+    These are his shoes.
+    These are her shoes.
+    These are our shoes.
+    These are their shoes.
+
+Pronouns
+    These shoes are mine.
+    These shoes are yours.
+    These shoes are his.
+    These shoes are hers.
+    These shoes are ours.
+    These shoes are theirs.
+
+Names
+    Jack's tie.
+    Tylor's shoes.
+    Alex's coat.
+
+    Whose tie is this?      It's Greg's.
+    Whose shoes are these?  They're Taylor's.
+
+*/
