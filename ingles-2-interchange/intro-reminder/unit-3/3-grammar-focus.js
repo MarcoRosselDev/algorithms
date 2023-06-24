f8ce5eb3258c86b3e0d2e262b097d0998989b16d
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-23 22:38:54 -0400

    ingles 2 | intro reminder | unit 3 | 3

--- ingles-2-interchange/intro-reminder/unit-3/3-grammar-focus.js
+++ ingles-2-interchange/intro-reminder/unit-3/3-grammar-focus.js
@@ -22,3 +22,27 @@ Are we late?                    Yes, you are.   No, you're not.
 Are they in Egypt?              Yes, They are.  No, they're not.
 
 */
+
+/* A    complete these convarsations 
+
+1.  a.  Are Diana and Mario from Ecuador?
+    b.  No, they're not. They are from Mexico.
+    a.  Are you from Mexico, too?
+    b.  No, I'm not. I'm from Colombia.
+    a.  So, Is your first lenguage Spanish?
+    b.  Yes, It is.
+
+2.  a.  Is Meera from England?
+    b.  No, she's not. She's from Australia.
+    a.  Is she from Sydney?
+    b.  Yes, she is. But her parents are from India.
+        They're not from Australia originally.
+    a.  Is Meera's first language Hindi?
+    b.  No, it's not. It's English.
+
+3.  a.  Ji-hye, Are you and Kwang-ho from south Korea?
+    b.  Yes, we are.
+    a.  And are you from Seoul?
+    b.  No, we are not. We are from Busan.
+
+*/
