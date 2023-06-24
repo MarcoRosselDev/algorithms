0e31f581d02cd192c77823f3f8f746e460c27052
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-24 18:25:47 -0400

    ingles 2 | intro reminder | unit 4 | 8 B

--- ingles-2-interchange/intro-reminder/unit-4/9-grammar-focus.js
+++ ingles-2-interchange/intro-reminder/unit-4/9-grammar-focus.js
@@ -11,3 +11,26 @@ They're wearing shoes.      They're not.    They aren't.
 It's snowing.               It's not.       It isn't snowing.
 
 */
+
+/* A    Complete the conversations from a travel show on TV.
+
+1.  My name is Dylan Jones. I'm waring a new gray suit.
+    I'm wearing new black shoes, too. It's raining, but I'm
+    not wearing a raincoat.
+
+2.  It's very hot and sunny today. 
+    Michael is wearing light blue shorts and white sneakers.
+    He's wearing a white T-shirt, but he's not wearing a cap. 
+
+3.  Adriana Fuentes is from Mexico. She is wearing a pretty
+    yellow dress and brown belt.
+    She's high heels and a light beown jacket, but she
+    isn't wearing a coat. Wow, it's really windy!
+
+4.  Hee-sun and Kun-woo are here with me today.
+    They're 10 year old. It's really cold, so they're 
+    wearing winter clothes.
+    They're wearing boots, gloves, hats, and scarves.
+    And they're wearing heavy coats!
+
+*/
