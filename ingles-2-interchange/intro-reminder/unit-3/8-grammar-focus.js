d56f49701df14ba3f45e3c3d6406c3cc0d2e73c0
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-24 00:42:55 -0400

    ingles 2 | intro reminder | unit 3 | 8

---
+++ ingles-2-interchange/intro-reminder/unit-3/8-grammar-focus.js
@@ -0,0 +1,33 @@
+/* 8    GRAMMAR FOCUS
+
+A   Complete the conversations with Wh-questions.
+
+1.  a.  Look! Who's that?
+    b.  Oh, she's a new student.
+    a.  What's her name?
+    b.  I think her name is Yoo-jin.
+    a.  Yoo-jin? Where's she from.
+    b.  She's from South Korea.
+
+2.  a.  Hi, Brittany. How are you?
+    b.  I'm fine, thanks. My friend Leandro is here this week - from Argentina.
+    a.  Oh, cool. What's he like?
+    b.  He's really friendly.
+    a.  How old are he?
+    b.  He's twenty-five years old.
+
+3.  a.  Azra, where are you from?
+    b.  I'm from Turkey. From Ankara.
+    a.  What's Ankara like? / What's it like?
+    b.  Well, Ankara is the capital of Turkey. It's very old.
+    a.  What's your last name?
+    b.  My last name is Genim.
+
+4.  a.  Good morning, Luke. How are you doing?
+    b.  I'm grat, thanks.
+    a.  Cool, who are they?
+    b.  They're my friens from school.
+    a.  Where'er they from?
+    b.  They're from Miami, like me.
+
+*/
