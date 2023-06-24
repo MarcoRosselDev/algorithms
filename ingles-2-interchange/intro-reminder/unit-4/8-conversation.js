5138b1f52edd9f02888ec41e637b9f7c570b68d4
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-24 18:12:07 -0400

    ingles 2 | intro reminder | unit 4 | 8

---
+++ ingles-2-interchange/intro-reminder/unit-4/8-conversation.js
@@ -0,0 +1,16 @@
+/* 8    CONVERSATION
+
+Ashley      Oh, no!
+Jessica     What's the matter?
+Ashley      It's snowing! Wow, it's so cold and windy!
+Jessica     Are you wearing your gloves?
+Ashley      No, I'm not. They're at home.
+Jessica     What about your scarf?
+Ashley      It's at home, too.
+Jessica     Well, you're wearing your coat.
+Ashley      But my coat isn't very warm.
+            And I'm not wearing boots!
+Jessica     OK. Let's take a taxi.
+Ashley      Good idea!
+
+*/
