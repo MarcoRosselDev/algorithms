64ffb33712efbdb17b7e8ddbd97cf0c306ce6a13
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-23 21:09:29 -0400

    ingles 2 | intro reminder | unit 2 | 8

---
+++ ingles-2-interchange/intro-reminder/unit-2/8-grammar-focus.js
@@ -0,0 +1,43 @@
+/* 8    GRAMMAR FOCUS
+
+Yes/No and Where questions with be
+
+Is this your wallet?
+    Yes, it is/ No, it's not.
+
+Are these your keys?
+    Yes, they are./ No, they're not.
+
+Where's your wallet?
+    It's in my pocket.
+
+Where are my keys?
+    They're on the table.
+
+*/
+
+/* A    Complete these conversations.
+
+1.  a.  Is this your cell phone?
+    b.  No, it's not.
+    a.  Are these your car keys?
+    b.  Yes, they're. Thanks!
+
+2.  a.  Where are my glasses?
+    b.  Are these your glasses?
+    a.  No, they're not.
+    b.  Look! Are they in your pocket?
+    a.  Yes, They're. Thanks!
+
+3.  a.  Where are your headphones?
+    b.  They're on the table.
+    a.  No, They're not. They're my headphones!
+    b.  You're right. My headphones are in my backpack.
+
+4.  a.  Is this my umbrella?
+    b.  No, it's not. It's my umbrella.
+    a.  Sorry. where is my umbrella?
+    b.  It is/It's on your chair.
+    a.  Oh, you're right!
+
+*/
