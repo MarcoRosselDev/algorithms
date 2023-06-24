b09d53c5504f4312b7eea0c0ef5328f5d64dab7b
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-23 22:24:47 -0400

    ingles 2 | intro reminder | unit 3 | 3

---
+++ ingles-2-interchange/intro-reminder/unit-3/3-grammar-focus.js
@@ -0,0 +1,24 @@
+/* 3    GRAMMAR FOCUS
+
+Negative statements and yes/no questions with be
+
+I'm not from Rio.
+You're not late.
+She's not from Japan.
+He's not from Chile.
+It's not English.
+We're not from China.
+You're not early.
+They're not in India.
+We're = we are
+
+Are you from Sao Paulo?         Yes, I am.      No, I'm not.
+Am I early?                     Yes, you are.   No, you're not.
+Is she from U.S.?               Yes, she is.    No, she's not.
+Is he from Mexico?              Yes, he is.     No, he's not.
+Is it French?                   Yes, it is.     No, it's not.
+Are you from South Korea?       Yes, we are.    No, we're not.
+Are we late?                    Yes, you are.   No, you're not.
+Are they in Egypt?              Yes, They are.  No, they're not.
+
+*/
