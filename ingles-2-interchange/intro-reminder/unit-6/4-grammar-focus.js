dbcb8ce810589687b3db24981e508fac90c375a8
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-25 03:53:52 -0400

    ingles 2 | intro reminder | unit 6 | 4 A

--- ingles-2-interchange/intro-reminder/unit-6/4-grammar-focus.js
+++ ingles-2-interchange/intro-reminder/unit-6/4-grammar-focus.js
@@ -19,5 +19,20 @@ They    don't need      a car.
 don't = do not
 doesn't = does not
 
+*/
+
+/* A    Tom Mitchell is talking about his family. Complete the sentences
+white the correct verb forms. Then compare with a partner.
+
+1.  My family and I live in the suburbs. My family and I 
+    work near here, so we walk to work.
+    Our daughter Megan works downtown, so she drives to work.
+    Our son doesn't drive. He rides his bike to school.
+
+2.  My parents live in the city. My mother
+    takes the subway to work. My father is 
+    retired, so he doesn't work now. He
+    also uses public transportation, so they
+    don't need a car.
 
 */
