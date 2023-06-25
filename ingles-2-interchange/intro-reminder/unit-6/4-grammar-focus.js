0b8cca5d262c87b870a68ef1716577f337d2d651
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-25 03:46:36 -0400

    ingles 2 | intro reminder | unit 6 | 4

---
+++ ingles-2-interchange/intro-reminder/unit-6/4-grammar-focus.js
@@ -0,0 +1,23 @@
+/* 4    GRAMMAR FOCUS
+
+Simple present statements
+
+I       walk        to school.
+You     ride        your bike to school.
+He      works       near here.
+She     takes       the bus to work.
+We      live        with our parents.
+They    use         public transportation.
+
+I       don't live      far from here.
+You     don't live      near here.
+He      doesn't work    downtown.
+She     doesn't drive   to work.
+We      don't live      alone.
+They    don't need      a car.
+
+don't = do not
+doesn't = does not
+
+
+*/
