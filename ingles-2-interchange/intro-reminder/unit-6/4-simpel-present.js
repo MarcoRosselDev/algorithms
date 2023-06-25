a38771ebb5a4b39e6a28b17ea755f101240f69f5
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-25 03:57:28 -0400

    ingles 2 | intro reminder | unit 6 | 4 B

---
+++ ingles-2-interchange/intro-reminder/unit-6/4-simpel-present.js
@@ -0,0 +1,15 @@
+/* 4 B  GRAMMAR FOCUS
+
+Simple present statements with irregular verbs.
+
+I/you/we/they
+    I have a bike.
+    We do our homework every day.
+    My parents go to work by train.
+
+He/she/it
+    My mother has a car.
+    My father does a lot of work at home.
+    The train goes downtown.
+
+*/
