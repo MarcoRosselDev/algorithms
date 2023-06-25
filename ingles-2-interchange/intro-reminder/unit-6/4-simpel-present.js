fe29c4a4265e1e4413e8d526b914937b5a20d8ba
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-25 04:02:16 -0400

    ingles 2 | intro reminder | unit 6 | 4 B 2

--- ingles-2-interchange/intro-reminder/unit-6/4-simpel-present.js
+++ ingles-2-interchange/intro-reminder/unit-6/4-simpel-present.js
@@ -13,3 +13,17 @@ He/she/it
     The train goes downtown.
 
 */
+
+/* B    Yoto is talking about his family and his friend Austin.
+Complete the sentences.
+
+1.  My parents have a house in the suburbs. My mom and dad go downtown to work.
+    My parents are very busy, so I do a lot of work at home.
+
+2.  My brother doesn't live with us. He has an apartment in the city.
+    He goes to school all day, and he does his homework at night.
+
+3.  I have a new friend. His name is Austin.
+    We go to the same school, and sometimes we do our homework together.
+
+*/
