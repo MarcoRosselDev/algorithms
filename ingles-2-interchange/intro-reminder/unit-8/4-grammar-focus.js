e64d23996998ff6f6444dee005a7e4e66ab4da98
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-25 22:44:44 -0400

    ingles 2 | intro reminder | unit 8 | 4

---
+++ ingles-2-interchange/intro-reminder/unit-8/4-grammar-focus.js
@@ -0,0 +1,32 @@
+/* 4    GRAMMAR FOCUS
+
+Simple present Wh-questions.
+
+Where do you work?
+    In a hospital.
+
+What do you do?
+    I'm a doctor.
+
+How do you like it?
+    I really like it.
+
+Where does he work?
+    In a hotel.
+
+What does he do?
+    He's a manager.
+
+How does he like it?
+    It's OK.
+
+Where do they work?
+    In an office.
+
+What do they do?
+    They're accountants.
+
+How do they like it?
+    They hate it.
+
+*/
