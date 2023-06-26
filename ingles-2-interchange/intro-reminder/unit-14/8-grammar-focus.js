758086cd693345d819368bb97781ea6ab426ef2f
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-26 18:46:55 -0400

    ingles 2 | intro reminder | unit 14 | 8

---
+++ ingles-2-interchange/intro-reminder/unit-14/8-grammar-focus.js
@@ -0,0 +1,17 @@
+/* 8    GRAMMAR FOCUS
+
+Simple past yes/no questions
+
+Did you have a good summer?
+    Yes, I did. I had a great summer.
+
+Did you go surfing?
+    No, I didn't. I went swimming.
+
+Did Maritin like his vacation?
+    Yes, He did. He liked it a lot.
+
+Did Martin and his sister go to Montreal?
+    No, they didn't. They went to Sydney.
+
+*/
