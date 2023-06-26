574c4c898e2fdecf5622ea8384455f81a38d6046
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-26 18:34:22 -0400

    ingles 2 | intro reminder | unit 14 | 3

---
+++ ingles-2-interchange/intro-reminder/unit-14/3-grammar-focus.js
@@ -0,0 +1,27 @@
+/* 3    GRAMMAR FOCUS
+
+Simple past statements; regular verbs
+
+I       stayed      home.
+You     watched     a TV show.
+She     exercised   on Saturday.
+We      estudied    English.
+They    shopped     for groceries.
+
+I       didn't stay     home.
+You     didn't watch    a game.
+She     didn't exercise on Sunday.
+We      didn't study    math.
+They    didn't shop     for clothes.
+
+didn't = did not
+
+Spelling
+
+stay    --> stayed
+watch   --> watched
+exercise--> exercised
+study   --> studied
+shop    --> shopped
+
+*/
