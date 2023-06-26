3cca801eeaf10820dc6c27da30f46ea8b3df8443
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-25 21:19:59 -0400

    ingles 2 | intro reminder | unit 7 | 3 A

--- ingles-2-interchange/intro-reminder/unit-7/3-grammar-focus.js
+++ ingles-2-interchange/intro-reminder/unit-7/3-grammar-focus.js
@@ -15,3 +15,20 @@ Does the house have a yard?
     Yes, it does.   /   No, it doesn't.
 
 */
+
+/* A    Complete the conversation.
+
+Julia   Do you live in an apartment?
+Ethan   No, I don't. I live in a house.
+Julia   Does it have a yard?
+Ethan   Yes, it does.
+Julia   That sounds nice. Do you live alone?
+Ethan   No, I don't. I live with my family.
+Julia   Do you have any brothers or sisters?
+Ethan   Yes, I do. I have four sisters.
+Julia   Really? Does your house have many bedrooms?
+Ethan   Yes, It does. It has four.
+Julia   Do you have your own bedroom?
+Ethan   Yes, I do. I'm really lucky.
+
+*/
