da9e1a88cf01ed11717e776844535d77108382d3
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-25 22:51:20 -0400

    ingles 2 | intro reminder | unit 8 | 4 A

--- ingles-2-interchange/intro-reminder/unit-8/4-grammar-focus.js
+++ ingles-2-interchange/intro-reminder/unit-8/4-grammar-focus.js
@@ -30,3 +30,27 @@ How do they like it?
     They hate it.
 
 */
+
+/* A    Complete these conversations.
+
+1.  a.  What does your sister do?
+    b.  My sister? She's teacher.
+    a.  How does she like it?
+    b.  It's difficult, but she loves it.
+
+2.  a.  Where does your brother work?
+    b.  In an office. He's an accountant.
+    a.  Oh? How does he like it?
+    b.  He doesn't really like it.
+
+3.  a.  How do your parents like theri jobs?
+    b.  Oh, I guess they like  them.
+    a.  I don't remember. Where do they work?
+    b.  In a big hospital. They're doctors.
+
+4.  a.  What do you do?
+    b.  I'm a student.
+    a.  I see. How do you like your classes?
+    b.  They're great. I like them a lot.
+
+*/
