f4bb1250302c28a038225839138c6389d874881e
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-25 20:50:20 -0400

    ingles 2 | intro reminder | unit 6 | 7 A

--- ingles-2-interchange/intro-reminder/unit-6/7-grammar-focus.js
+++ ingles-2-interchange/intro-reminder/unit-6/7-grammar-focus.js
@@ -21,3 +21,20 @@ When do they take the subway?
     On Mondays and Wednesdays.
 
 */
+
+/* A    Complete the questions with do or does.
+
+1.  Do you get up late on Sundays?
+2.  Do you have lunch at home every day?
+3.  What time does your father leave work on Frindays?
+4.  Does your mother cook on weekdays?
+5.  Does your father shop on Saturdays?
+6.  Do you take a walk in the evening?
+7.  When do you listen to music?
+8.  What time do you check your email?
+9.  What time do your parents have dinner?
+10. When do you study English?
+11. Does your best friend ride a bike on weekends?
+12. Does your father drive to work every morning?
+
+*/
