7ee449dddce43ff6dd77cd603cd298066dfb01c0
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-26 04:50:15 -0400

    ingles 2 | intro reminder | unit 9 | 3

---
+++ ingles-2-interchange/intro-reminder/unit-9/3-grammar-focus.js
@@ -0,0 +1,34 @@
+/* 3    GRAMMAR FOCUS
+
+Count and noncount nouns; some and any.
+
+some ---->  algo de...  in question = afirmative questins.
+            unos/as
+            algunos/as
+
+any ----->  nada de...  in question = negative questions.
+            algo de
+            alunos/as
+
+examples:
+1.  We need some apples.
+2.  The coffe needs some sugar.
+3.  There isn't any juise.
+4.  I want some water.
+5.  There aren't any students in the class.
+6.  The pot doesn't have any salt.
+
+in questions
+
+1.  Quieres algunas manzanas?
+    Do you want some apples?
+
+2.  Hay algo de agua en la botella?
+    Is there any water in the bottle?
+
+3.  Te gustaria algo de leche?
+    Would you like some milk?
+
+4.  Hay algunos animales en tu casa?
+    Are there any animals in your house?
+*/
