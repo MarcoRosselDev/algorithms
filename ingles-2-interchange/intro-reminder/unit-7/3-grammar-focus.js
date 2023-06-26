cef7d271f856b7485d62601fe50c71c19a720df0
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-25 21:10:24 -0400

    ingles 2 | intro reminder | unit 7 | 3

---
+++ ingles-2-interchange/intro-reminder/unit-7/3-grammar-focus.js
@@ -0,0 +1,17 @@
+/* 3    GRAMMAR FOCUS
+
+Simple present short answers
+
+Do you live in an apartment?
+    Yes, I do.  /   No, I don't.
+
+Do the bedrooms have closets?
+    Yes, they do.   /   No, they don't.
+
+Does Ethan live in a house?
+    Yes, he does.   /   No, he doesn't.
+
+Does the house have a yard?
+    Yes, it does.   /   No, it doesn't.
+
+*/
