90d102dae68e158001ccb2e143bbdbe768f21eab
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-25 20:43:17 -0400

    ingles 2 | intro reminder | unit 6 | 7

---
+++ ingles-2-interchange/intro-reminder/unit-6/7-grammar-focus.js
@@ -0,0 +1,23 @@
+/* 7    GRAMMAR FOCUS 
+
+Simple present questions
+
+Do you get up early on Sundays?
+    No, I get up late.
+
+Does he eat breakfast at seven o'clock?
+    No, he eats breakfast at seven-thirty.
+
+Do they take a taxi to class?
+    No, they take the bus.
+
+What time do you get up?
+    At noon.
+
+What time does she have dinner?
+    At eight o'clock.
+
+When do they take the subway?
+    On Mondays and Wednesdays.
+
+*/
