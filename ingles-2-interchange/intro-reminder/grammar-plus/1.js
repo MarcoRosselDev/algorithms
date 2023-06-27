07a9e4dfefd424fb5d6f65117386315ea5b46e7b
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-27 02:54:35 -0400

    ingles 2 | intro reminder | grammar-plus | 1

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/1.js
@@ -0,0 +1,24 @@
+/* My, your, his, her
+
+Use his with males and her with females
+    His name is Travis
+    Her name is Nicole.
+
+Complete the conversations with my, your, his, or her.
+
+1.  a.  Hello, My name is Carlos.
+    b.  Hi, Carlos. What's your last name?
+    a.  It's Gonzales.
+    b.  How do you spell your last name? Is it ...?
+    a.  No, it's ... And what's your name?
+    b.  My name is Bill Powers. Nice to meet you.
+
+2.  a.  What's Ms. Robinson's first name?
+    b.  Her first name is Elizabeth. Her nickname is Liz.
+    a.  I'm sorry. What's her first name again?
+    b.  It's Elizabeth. And what's Mr. Weber's first name?
+    a.  His first name is Peter.
+    b.  That's right. And his nickname si Pete.
+    a.  That's right, too!
+
+*/
