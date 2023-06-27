368a51ee2e87b90ea24c19d092f6914e2a7a248e
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-26 21:02:11 -0400

    ingles 2 | intro reminder | unit 14 | 8  A

--- ingles-2-interchange/intro-reminder/unit-14/8-grammar-focus.js
+++ ingles-2-interchange/intro-reminder/unit-14/8-grammar-focus.js
@@ -15,3 +15,23 @@ Did Martin and his sister go to Montreal?
     No, they didn't. They went to Sydney.
 
 */
+
+/* A    Complete the conversations.
+
+1.  a.  Did you have a good summer?
+    b.  Yes I did. I had a great summer.
+        I went out with my friends a lot.
+
+2.  a.  Did you go anywhere last summer?
+    b.  No, I didn't. I stayed here.
+        I got a part-time job, so I made some extra money.
+
+3.  a.  Did you take any classes last summer?
+    b.  Yes, I did. I took tennis lessons, and
+        I played tennis every day!
+
+4.  a.  Did you speak English last summer?
+    b.  No, I didn't. But I read blogs
+        in English, and I watched English movies.
+
+*/
