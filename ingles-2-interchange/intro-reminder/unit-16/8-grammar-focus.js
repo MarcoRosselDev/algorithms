0c1db951a76adf6668d5bfe86864dcfcbcc37316
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-27 02:44:00 -0400

    ingles 2 | intro reminder | unit 16 | 8 A

--- ingles-2-interchange/intro-reminder/unit-16/8-grammar-focus.js
+++ ingles-2-interchange/intro-reminder/unit-16/8-grammar-focus.js
@@ -14,3 +14,27 @@ Would you like to go to an amusement park?
     I'd like to, but I need to study.
 
 */
+
+/* A    Complete the invitations
+
+Invitations
+
+1.  Would you like to go to an art festival this weekend?
+    I'd like to, but I can't. I'm going to go on a trip this weekend.
+
+2.  Do you want to go to a volleyball game tomorrow night?
+    I don't really like volleyball. Do you want to do something else?
+
+3.  Would you like to see a comedy tonight?
+    Tonight? I can't. I need to help my parents.
+
+4.  Do you want to go swimming on Satruday?
+    I'd like to, but I don't have a swimsuit!
+
+5.  Do you want to play soccer after school today?
+    I'm sorry, but I have to talk to the teacher after school.
+
+6.  would you like to go to a hip-hop concert on Saturday night?
+    Yes, I'd love to. It's my favorite type of music.
+
+*/
