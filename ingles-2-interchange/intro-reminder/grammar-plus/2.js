5cf3731913339c4bed5998924457bf1a058886d5
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-27 04:31:05 -0400

    ingles 2 | intro reminder | grammar-plus | 2 A

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/2.js
@@ -0,0 +1,23 @@
+/* This/these   it/they     plurals
+
+Don't use a contraction with what + are
+    What are these? =/= What're these?
+
+Use this with singular nouns
+    This is a laptop.
+
+Use these with plural nouns
+    These are flash drives.
+
+Choose the correct words.
+
+1.  a.  What are these?
+    b.  They're my flash drives.
+
+2.  a.  What are this?
+    b.  It's a cell phone.
+
+3.  a.  What's this?
+    b.  It's an English book.
+
+*/
