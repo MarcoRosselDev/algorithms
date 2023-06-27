0565af7b4f136f0f9d9e4ab05336d206a18f474b
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-27 05:20:15 -0400

    ingles 2 | intro reminder | grammar-plus | 3 A

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/3.js
@@ -0,0 +1,30 @@
+/* 1    Negative statements and yes/no questions with be
+
+Use be + not to form negative statements
+    Ana isn't a student.
+
+You is a singular and plural pronoun
+    Are you from Rio?
+        Yes, I am./Yes, we are.
+
+A   Unscremble the words to write negative statements.
+
+1.  is/of Canada/Toronto/the capital/not
+    Toronto is not the capital of Canada.
+
+2.  Buenos Aires/not/from/we're
+    we're not from Buenos aires.
+
+3.  not/you and Ashley/in my class/are
+    You and Ashle not are in my class.
+
+4.  is/my first language/Korean/not
+    Korean is not my frist lenguage.
+
+5.  from/my mother/not/is/Italy
+    My mother is not from Italy.
+
+6.  my parents/not/are/they
+    They are not my parents.
+
+*/
