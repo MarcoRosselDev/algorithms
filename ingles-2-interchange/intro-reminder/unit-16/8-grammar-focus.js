24da2e8186d89937633514af3829299d6cacdfa7
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-27 02:24:53 -0400

    ingles 2 | intro reminder | unit 16 | 8

---
+++ ingles-2-interchange/intro-reminder/unit-16/8-grammar-focus.js
@@ -0,0 +1,16 @@
+/* 8    GRAMMAR FOCUS
+
+Invitations- verb + to
+
+
+Do you want to see a play with me tonight?
+    Sure I'd really like to see a good play.
+    I'd like, but I have to work late.
+
+    I'd = I would
+
+Would you like to go to an amusement park?
+    Yes, I'd love to (go to an amusement park)!
+    I'd like to, but I need to study.
+
+*/
