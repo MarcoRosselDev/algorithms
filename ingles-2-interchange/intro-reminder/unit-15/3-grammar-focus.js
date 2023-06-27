9271bb7895d34f1e39b65cce7e757cf20a361ba8
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-26 21:22:25 -0400

    ingles 2 | intro reminder | unit 15 | 3 A

--- ingles-2-interchange/intro-reminder/unit-15/3-grammar-focus.js
+++ ingles-2-interchange/intro-reminder/unit-15/3-grammar-focus.js
@@ -11,3 +11,15 @@ They    were    born in Milan.
 weren't = were not
 wasn't = was not
 */
+
+/* A    Bianca is talking about her family. Choose the correct verb forms.
+
+My family and I were all born in Brazil -
+we weren't born in the U.S. I was 
+born in the city of Recife, and my brother was born 
+there, too. My parents weren't born in Refice.
+They were born in Rio de Janeiro. In Rio, my father
+was a teacher and my mother was an engineer.
+They have their own business in Recife now.
+
+*/
