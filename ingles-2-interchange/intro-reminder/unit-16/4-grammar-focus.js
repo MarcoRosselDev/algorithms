cd0fe6878dcb631c76aed73bce35d905480b76d2
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-26 21:43:31 -0400

    ingles 2 | intro reminder | unit 16 | 4

---
+++ ingles-2-interchange/intro-reminder/unit-16/4-grammar-focus.js
@@ -0,0 +1,32 @@
+/*  4   GRAMMAR FOCUS
+
+Subject and object pronouns.
+
+Subjects      Objects
+    I           me
+    You         you
+    He          him
+    She         her
+    We          us
+    They        them
+
+A   Complete the phone conversations with the correct pronouns.
+
+1.  a.  Can I speak with Ms. Murphy, pleace?
+    b.  She's not here. But maybe I can help you.
+    a.  Please give her my new cell number.
+        It's ...
+
+2   a.  Hi, this is Colin Shaw. Is Mr. Kerr there?
+    b.  I'm sorry, but he's not here right now.
+        Do you want to leave a message?
+    a.  Yes. Please tell him to call me at work.
+
+3.  a.  Hello, this is Carol's Cafe. Are Lauren and Matt in?
+    b.  No, they're not. I'm their son. Can I help you?
+    a.  I found their keys. They left them on the table.
+    b.  Just bring me the keys. I can give them to my parents.
+    a.  I'm sorry, but I can't. Can your mom or dad call me?
+    b.  OK. 
+
+*/
