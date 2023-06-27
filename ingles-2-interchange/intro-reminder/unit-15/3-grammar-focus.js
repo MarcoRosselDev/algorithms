71447adb25e344974d624acc9a731dbd2b5905c6
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-26 21:13:47 -0400

    ingles 2 | intro reminder | unit 15 | 3

---
+++ ingles-2-interchange/intro-reminder/unit-15/3-grammar-focus.js
@@ -0,0 +1,13 @@
+/* 3    GRAMMAR FOCUS
+
+Past of be.         ---->   I/she/he/it = was || You/we/they = were
+
+I       was     born here.
+You     were    pretty young.
+She     was     seven.
+We      were    at the hair salon.
+They    were    born in Milan.
+
+weren't = were not
+wasn't = was not
+*/
