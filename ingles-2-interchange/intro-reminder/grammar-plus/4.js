a01a81b0e4b2a7546f2a873fc4362c8cc8f2014a
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-29 10:06:03 -0400

    ingles 2 | intro reminder | grammar-plus | 4 A

--- ingles-2-interchange/intro-reminder/grammar-plus/4.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/4.js
@@ -16,7 +16,17 @@ There are two extra words in each box.
 
 his | mine | my | your | yours | whose
 
-1.  a.  Whose jacket is this? Is it your, Ethan?
-    b.  No, it's not mine. Ask Matt. I think it's 
+1.  a.  Whose jacket is this? Is it yours, Ethan?
+    b.  No, it's not mine. Ask Matt. I think it's his.
+    
+Her | my | mine | your | yours
+
+2.  a.  These aren't my gloves. Are they yours?
+    b.  No, they're not mine. Maybe they are Young-min's.
+
+Her | hers | their | theirs | whose
+
+3.  a.  Whose sweaters are these? Are they Rechel's?
+    b.  No, they're not her sweaters. But these short are hers.
 
 */
