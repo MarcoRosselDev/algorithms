16c7f8ec0640074a84c8149ace7c962766ca55b2
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-29 13:32:18 -0400

    ingles 2 | intro reminder | grammar-plus | 5 | 2

--- ingles-2-interchange/intro-reminder/grammar-plus/5.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/5.js
@@ -13,15 +13,38 @@ Write each sentence in a different way.
     It's seven a clock in the evening.
 
 3.  It's six-fifteen.
-    It's quarter past six.
+    It's quarter after six.
 
 4.  It's 10 o'clock at night.
     It's 10 P.M.
     
 5.  It's three-oh-five.
-    It's five past three.
+    It's five after three.
 
 6.  It's twenty-five to eleven.
-    It's 
+    It's ten thirty-five.
+
+7.  It's one o'clock in the morning.
+    It's one A.M.
+
+8.  It's midninght.
+    It's 12:00 A.M./It's twelve at night.
+
+*/
+
+/* 2    Present continuous Wh-questions
+
+Use the present continuous to talk about actions that are happening right now:
+    What are you doing?
+    I'm talking to you!
+
+In questions, the be verb comes after the question word:
+    What are you doing?
+
+To form the continuous of verbs ending in -e, drop the e and add -ing:
+    Have ---> having.
+
+For verbs ending in vowel + consonant, double the consonant and add -ing:
+    get ---> getting.
 
 */
