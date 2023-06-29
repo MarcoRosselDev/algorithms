7e0aa126c8fcaebbf5bf137cf7a8b7b33a8f88b9
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-29 10:46:31 -0400

    ingles 2 | intro reminder | grammar-plus | 5

--- ingles-2-interchange/intro-reminder/grammar-plus/5.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/5.js
@@ -1,1 +1,27 @@
-/* another change, but now is on another file.js */
+/* 1    What time is it?    /Is it A.M. or P.M.?
+
+Remember:
+    You can say times different ways:
+        1:15 = one-fifteen OR a quarter a after one.
+
+Write each sentence in a different way.
+
+1.  It's a quarter to four.
+    It's three forty-five.
+
+2.  It's 7:00 P.M. 
+    It's seven a clock in the evening.
+
+3.  It's six-fifteen.
+    It's quarter past six.
+
+4.  It's 10 o'clock at night.
+    It's 10 P.M.
+    
+5.  It's three-oh-five.
+    It's five past three.
+
+6.  It's twenty-five to eleven.
+    It's 
+
+*/
