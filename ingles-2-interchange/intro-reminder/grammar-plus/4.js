0cf73ce2bf998051dda8a76d9ca5b76251e09e09
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-29 10:18:53 -0400

    ingles 2 | intro reminder | grammar-plus | 4 | 2

--- ingles-2-interchange/intro-reminder/grammar-plus/4.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/4.js
@@ -30,3 +30,33 @@ Her | hers | their | theirs | whose
     b.  No, they're not her sweaters. But these short are hers.
 
 */
+
+/* 2    Present continuous statements;  conjunctions
+
+The present continuous is the  present of be + verb + -ing:
+    It's raining.
+    She's wearing a raincoat.
+    
+The two negative contractions mean the same:
+    He's not/He isn't wearing a coat.
+    We're not/We aren't wearing gloves.
+
+Change the affirmative sentences to negative sentences.
+Change the negative sentences to affirmative sentences.
+
+1.  Mr. and Mrs. Liu are wearing green caps.
+    Mr. and Mrs. Liu aren't wearing green caps.
+
+2.  It isn't snowing.
+    It's snowing.
+
+3.  I'm wearing a winter coat.
+    I'm not wearing a winter coat.
+
+4.  You're wearing David's sunglasses.
+    You aren't wearing David's sunglasses.
+
+5.  Ayumi isn't wearing a scarf.
+    Ayumi is wearing a scarf.
+
+*/
