e7e20806724f857292ef25eddd7e5da611934e7e
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-29 06:10:51 -0400

    ingles 2 | intro reminder | grammar-plus | 4

--- ingles-2-interchange/intro-reminder/grammar-plus/4.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/4.js
@@ -1,6 +1,22 @@
-/* 4    prectis from the git course to profecionals
--
--
--   more changes to add just on this file and dont add the 5.js file
--
+/*  1   Possessives
+
+The noun come after a possessive adjective:
+    This is my T-shirt.
+
+Don't include the noun after a possessive pronoun:
+    This T-shirt is mine.
+
+Whose can be used with singular and plural nouns:
+    Whose scarf is this?
+    Whose sneakers are these?
+
+Complete the conversations.
+Use the words in the boxes.
+There are two extra words in each box.
+
+his | mine | my | your | yours | whose
+
+1.  a.  Whose jacket is this? Is it your, Ethan?
+    b.  No, it's not mine. Ask Matt. I think it's 
+
 */
