6e64118663851fae967214e4c8c98f0d9267da39
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-30 10:57:18 -0400

    ingles 2 | intro reminder | grammar-plus | 7

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/7.js
@@ -0,0 +1,20 @@
+/* 1    Simple present short answers
+
+Remember:   I/you/we/they do/don't 
+            He/she/it does/doesn't.
+
+Choose the correct words.
+
+a.  Does your family live in an apartment?
+b.  No, we don't. We have a house.
+a.  That's nice. Does your house have two floors?
+b.  Yes, It does. It has four rooms on the first floor. And we have three bedrooms
+    and a bathroom on the second floor.
+a.  And do you and your family have a yard?
+b.  Yes, we do. And how about you, Tim?
+    Do you live in a house, too?
+a.  No, I don't. My wife and I have a small apartment in the city.
+b.  Oh. Do you like the city?
+a.  Yes, I do. But my wife doesn't.
+
+*/
