16c16cc90ec5e1f6e71e89c1f05709f6ebc206db
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-01 08:24:12 -0400

    ingles 2 | intro reminder | grammar-plus | 10

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/10.js
@@ -0,0 +1,33 @@
+/* 1    Simple present Wh-questions
+
+Remember:   Who = what person
+            Where = what place
+            How often = what frequency
+            When = what day
+            What time = what time of day
+
+RememberL   Use do or does after the question word.
+
+
+Complete the questions with the correct question word and do or does.
+Then match the questions with the answers.
+
+1.  What sport do you like?
+    c.  Soccer. I love to wath my team.
+
+2.  Who do you go to games with?
+    a.  My father and my two brothers.
+
+3.  How often does your team play?
+    d.  Once or twice a month.
+
+4.  When do they play?
+    e.  On Sunday afternoons.
+
+5.  Where do they play?
+    f.  At the Olympic Stadium
+
+6.  What time do the games start?
+    b.  Usually at four o'clock.
+
+*/
