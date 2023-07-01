11306a47d11a8871970e47a42e942cee3b0ba7bf
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-01 05:20:13 -0400

    ingles 2 | intro reminder | grammar-plus | 8-2

--- ingles-2-interchange/intro-reminder/grammar-plus/8.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/8.js
@@ -33,3 +33,43 @@ Complete the conversations.
     b.  They work very hard, but they love their jobs.
 
 */
+
+/* 2    Placement of adjactives
+
+Adjectives come after the verb be:
+    A doctor's job is stressful.
+
+Adjectives come before nouns:
+    A police office has a dangerous job.
+
+Adjectives have the same form with singular or plural nouns:
+    Firefightres and plice officers have stressful jobs.
+
+
+Use the information to write two sentences.
+
+1.  accountant/ job / boring
+    An accountant's job is boring.
+    An accountant has a boring job.
+
+2.  salesperson / job / stressful
+    A salesperson's job is stressful.
+    A salesperson has a stressful job.
+
+3.  security guard / job / dangerous
+    A security guard's job is dangerous.
+    A security guard has a dangerous job.
+
+4.  actor / job / exciting
+    An actor's job is exciting.
+    An actor has an exciting job.
+
+5.  Host / job / interesting
+    A Host's job is interesting.
+    A host has an interesting job.
+
+6.  nourse / job / difficult
+    A nourse's job is difficult.
+    A nourse has a difficult job.
+
+*/
