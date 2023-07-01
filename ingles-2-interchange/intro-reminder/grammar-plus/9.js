cceeab9d7fb53de09fea1ed7ca39d818fdc5fbe3
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-01 05:33:56 -0400

    ingles 2 | intro reminder | grammar-plus | 8-2 B

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/9.js
@@ -0,0 +1,40 @@
+/* 1    Count and nouncount nouns: some and any
+
+Count nouns name things you can nount:
+    Bananas, crackers, carrots.
+Count nouns have a singular and plural form:
+    1 orange, 2 oranges.
+Nouncount nouns name things you can't count:
+    milk, oil, rice.
+
+Use some in affirmative sentences:
+    We have some butter.
+Use any in negative sentences and questions:
+    We don't have any lettuce.
+    Do we have any tomatoes?
+
+
+Complete the conversations with some or any.
+
+1.  a.  What do you want for lunch?
+    b.  Let's make some sandwiches.
+    a.  Good idea! We have some bread. Do we have any cheese?
+    b.  Yes, I think there's some in the refrigerator. Let me see...
+        No, I don't see any.
+    a.  Well, let's go to the store. We need some milk, too.
+        And do we have any cheese?
+    b.  Yes, we do. There's some lettuce here, and there are some tomatoes, too.
+    a.  Do we have any mayonnaise? I love some mayonnaise on my sandwiches.
+    b.  Me, too. But there isn't any here. Let's buy some.
+
+2.  a.  Let's make a big breakfast tomorrow morning.
+    b.  Good idea! What do we need? Are there any eggs?
+    a.  There are some but I think we need to buy some more.
+    b.  Ok. And let's get some cereal, too. We don't have any.
+        and I love ceral for breakfast.
+    a.  Me, too. Do we have any blueberry yogurt?
+    b.  Yes, there's some in the refrigerator.
+    a.  Great! So we don't need to buy any at the store.
+    b.  That's right. Just eggs and cereal!
+
+*/
