2fd72709009eb4369f5e03c98ba04e757c311826
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-01 11:54:09 -0400

    ingles 2 | intro reminder | grammar-plus | 11 B

--- ingles-2-interchange/intro-reminder/grammar-plus/11.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/11.js
@@ -21,3 +21,28 @@ After the movie, we are going to go to our frind Philip's house.
 He is going to make his special homemade pizza for Rosa and me.
 
 */
+
+/* B    Write questions. Then look at part A and answer the questions.
+
+1.  Matthew / celebrate / with his family?
+    Q.  Is Matthe going to celebrate with his family?
+    A.  No, he's going to celebrate with his freinds.
+
+2.  Stephen and Matthew / ride their bikes / to the beach?
+    Q   Are Stephen adn Matthew going to ride their bikes to the beach?
+    A.  No, they aren't. They are going to drive to the beach.
+
+3.  the friends / have lunch / at a restaurant?
+    Q.  Are the friends going to have lunch at a restaurant?
+    A.  Yes, they are.
+
+4.  Rosa and Matthew / go to a museum?
+    Q.  Are Rosa and Matthew going to go to a museum?
+    A.  No, they aren't. They're going to see a movie.
+
+5.  Rosa and Matthew / have pizza / at a restaurant?
+    Are Rosa and Matthew going to have pizza at a restarant?
+    No, they aren't. They're going to go to Philip's house.
+
+
+*/
