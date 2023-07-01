016d7c71ed13e3cad7077e2c086ea2e725c520b0
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-01 08:13:04 -0400

    ingles 2 | intro reminder | grammar-plus | 9

--- ingles-2-interchange/intro-reminder/grammar-plus/9.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/9.js
@@ -38,3 +38,38 @@ Complete the conversations with some or any.
     b.  That's right. Just eggs and cereal!
 
 */
+
+/* 2    Adverbs of frequency
+
+Adverbs of frequency usually go before the main verb:
+    always, almost always, usually, often, sometimes, hardly ever, almost never, never:
+        She never eats breakfast.
+        I almost always have tea in the morning.
+
+        Sometimes can also begin a sentences:
+            Sometimes I eat broccoli.
+
+Rewrite the conversation. Add the adverbs in the correct places.
+
+A   Where do you have lunch? (usually)
+    Where do you usually have lunch?
+
+b   I go to a restaurant near work. (often)
+    I often go to a restaurant near work.
+
+a.  Do you eat at your desk? (ever)
+    Do you ever eat at your desk?
+
+b.  No, I stay in for lunch. (hardly ever)
+    No, I hardly ever stay in for luch.
+
+a.  And what do you have? (usually)
+    And what do you usually have?
+
+b.  I have soup or a salad.
+    I always have soup or a salad.
+
+a.  Me, too. I have a big lunch. (never)
+    Me, too. I never have a big luch.
+
+*/
