e7d475edd51d1909706a60cfd9afea84403989e2
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-01 11:33:17 -0400

    ingles 2 | intro reminder | grammar-plus | 11 A

--- ingles-2-interchange/intro-reminder/grammar-plus/11.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/11.js
@@ -14,5 +14,10 @@ Tomorrow is going to be a very exciting day.
 It's my birthday, and my friends and I are going to celebrate.
 In the morning, Stephen and I are going to drive to the beach.
 Our friend Rose is going to meet us there.
+We are going to stay at the beach for a few hours.
+Then we're going to have lunch at my favorite restaurant.
+After lunch, Stephen is going to go to work, and Rose and I are going to see a movie.
+After the movie, we are going to go to our frind Philip's house.
+He is going to make his special homemade pizza for Rosa and me.
 
 */
