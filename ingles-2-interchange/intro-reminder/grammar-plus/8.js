356916f0edfbe93ff88de36b179dbbc7aacd1402
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-01 05:00:10 -0400

    ingles 2 | intro reminder | grammar-plus | 8

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/8.js
@@ -0,0 +1,35 @@
+/* 8    Simple present Wh-questions.
+
+Use what to ask about things:
+    What do you do?
+
+Use where to ask about places:
+    Where do you work?
+
+use How do/does..like...? to ask for an opinion:
+    How does he like his job?
+
+
+Complete the conversations.
+
+1.  a.  What does you husband do?
+    b.  My husband? Oh, he's a nourse.
+    a.  Really? Where does he work?
+    b.  He works at mercy hospital.
+
+2.  a.  Where do you work?
+    b.  I work in a restaurant.
+    a.  Nice! What do you do?
+    b.  I'm a chef.
+
+3.  a.  How do you like your job?
+    b.  My job? I don't really like it very much.
+    a.  That's too bad. What do you do?
+    b.  I'm a cashier. I work at a clothing store.
+
+4.  a.  What do your brother ans sister do?
+    b.  My brother is a doctor, and my sister is a lawyer.
+    a.  How do they like their jobs?
+    b.  They work very hard, but they love their jobs.
+
+*/
