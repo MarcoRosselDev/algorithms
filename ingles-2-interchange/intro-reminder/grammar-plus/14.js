b969f86070841d0f70c6c99c2e6cdc51b244ee68
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-02 14:01:43 -0400

    ingles 2 | intro reminder | grammar-plus | 14  B

--- ingles-2-interchange/intro-reminder/grammar-plus/14.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/14.js
@@ -16,4 +16,27 @@ The form doesn't change:
     He didn't shop for groceries yesterday.
 
 
+Maya wrote an email to a friend.
+Complete the sentences with the simple past form of the verbs in parentheses.
+
+Hi!
+I didn't do anything special this weekend, but I
+had a lot of fun. I didn't go out on Friday night.
+I stayed home. I cleaned my room and I did the laundry. 
+I helpped my sister with her homework,
+and then we wathed our favorite series.
+On Saturday, my freind Lori come over. She needed some new shoes,
+so we taked the bus downtown to Todd's Shoes Store.
+We shopped for a long time, but Lori didn't like any of the shoes at Todd's.
+She buyed some puple socks, but she didn't buy any shoes.
+On our way back to my house, we stoped at the gym and exerciseed.
+We didn't exercise very hard. I invited Lori for dinner and my dad cooked hamburgers in the yard.
+After dinner, Lori and I talked and played video games.
+She didn't stay vary late -  my mother drived her home at around ten.
+On Sunday, my whole family visited my mother's best friend and her family.
+They have a swimming pool, so my sister and I went swimming all afternoon.
+Tell me about your weekend!
+Maya.
+
+
 */
