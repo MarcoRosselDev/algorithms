2bdeaceda124b3462fe99c41a7efd44aa84c6493
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-02 12:16:56 -0400

    ingles 2 | intro reminder | grammar-plus | 13

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/13.js
@@ -0,0 +1,26 @@
+/* 1    Prepositions of place
+
+Use on with the names of streets and avenues:
+    The bookstore is on Center Street.
+    The theater is on Park Avenue.
+
+Across from is another way of saying opposite:
+    The library is across from the theater.
+    The library is opposite the theater.
+
+
+Choose the correct words.
+
+a.  Excuse me. Is there a post office around here?
+b.  Yes, there is. It's on Maple Street.
+a.  where on Maple?
+b.  It's in the corner of Maple Street and Second Avenue.
+a.  Next to Charlie's Restaurant?
+b.  Yes, that's right. It's across the street to the Windsor Hotel.
+a.  Thanks. Oh, and where is the bank?
+b.  It's on Oak Street -between the  hospital and police station.
+a.  Great. Thanks very much.
+b.  You're welcome.
+
+
+*/
