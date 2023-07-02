823bea072de8d5c2969843bf90cfa1302b70e8a6
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-02 14:13:18 -0400

    ingles 2 | intro reminder | grammar-plus | 14  C

--- ingles-2-interchange/intro-reminder/grammar-plus/14.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/14.js
@@ -23,20 +23,43 @@ Hi!
 I didn't do anything special this weekend, but I
 had a lot of fun. I didn't go out on Friday night.
 I stayed home. I cleaned my room and I did the laundry. 
-I helpped my sister with her homework,
-and then we wathed our favorite series.
+I helped my sister with her homework,
+and then we watched our favorite series.
 On Saturday, my freind Lori come over. She needed some new shoes,
-so we taked the bus downtown to Todd's Shoes Store.
+so we took the bus downtown to Todd's Shoes Store.
 We shopped for a long time, but Lori didn't like any of the shoes at Todd's.
-She buyed some puple socks, but she didn't buy any shoes.
-On our way back to my house, we stoped at the gym and exerciseed.
+She bought some puple socks, but she didn't buy any shoes.
+On our way back to my house, we stopped at the gym and exercised.
 We didn't exercise very hard. I invited Lori for dinner and my dad cooked hamburgers in the yard.
 After dinner, Lori and I talked and played video games.
-She didn't stay vary late -  my mother drived her home at around ten.
+She didn't stay too late -  my mother drove her home at around ten.
 On Sunday, my whole family visited my mother's best friend and her family.
 They have a swimming pool, so my sister and I went swimming all afternoon.
 Tell me about your weekend!
 Maya.
+
+*/
+
+/* 2    Simple past yes/no questions
+
+Use did + base form in questions.
+The from doesn't change:
+    Did you have fun yesterday?
+
+
+
+Complete the conversation.
+Use the simple past form of the verbs in parentheses.
+
+a.  Did you enjoy you vacations?
+b.  Yes, I did. My brother and I has a great time.
+a.  Did you make any videos?
+b.  No, we didn't. But we took a lot of pictures.
+a.  That's good. Did you see a lot of interesting things?
+b.  Yes, we did. And we ate a lot of new foods.
+    How about you?
+a.  Well, I didn't go anywhere, but I read a lot of good books and
+    saw some grat movies.
 
 
 */
