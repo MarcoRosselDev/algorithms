978eea3dcd5b33d7f218615cf14db0c9278ac81f
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-02 07:35:30 -0400

    ingles 2 | intro reminder | grammar-plus | 12

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/12.js
@@ -0,0 +1,32 @@
+/* 1    Have + noun:    feel + adjective
+
+For most health problems, use a/an:
+    I have a cold.
+    I have an earche.
+With flu, use the ---------> I have the flu.
+
+
+Complete the conversation. Use the sentences in the box.
+
+I think I have a fever.
+Thanks.
+I feel awful, actually.
+Yes, I'm going to call my doctor in a few minutes.
+Yes, I do. And I have a stomachache, too.
+Hi, Vanessa. How are you?
+
+
+a.  Hi, Vanessa. How are you?
+b.  I'm terrific, thanks. How about you?
+a.  I feel awful, actually.
+b.  Oh, no! What's the matter?
+a.  I think I have a fever.
+b.  That's too bad. Do you have a headache?
+a.  Yes, I do. And I have a stomachache, too.
+b.  Are you going to see a doctor?
+a.  Yes, I'm going to call my doctor in a few minutes.
+b.  Well, feel better soon.
+a.  Thanks.
+
+
+*/
