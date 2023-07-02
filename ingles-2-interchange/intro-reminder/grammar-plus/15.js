1501370dcf2737543f3b156d0aec46f648607ae0
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-02 19:44:53 -0400

    ingles 2 | intro reminder | grammar-plus | 15 A

--- ingles-2-interchange/intro-reminder/grammar-plus/15.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/15.js
@@ -23,3 +23,25 @@ Complete the conversations with was, wasn't, were, or weren't.
     b.  Yes, It was. Jamil said it was a terrific trip!
 
 */
+
+/* 2    Wh-questions with did, was, and were
+
+Don't use did with the past of be:
+    Where were you last Tuesday?
+Use did in simple past questions with other verbs:
+    Where did you go last Tuesday?
+
+Because answers the question Why?
+
+
+Complete the questions. Use the words in the box.
+
+how     what    Where   Why
+How old         when    who
+
+1.  a.  How was your childhood?
+    b.  I had a fantastic childhood!
+
+2.  a.  
+
+*/
