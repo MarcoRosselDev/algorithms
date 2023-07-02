de4bd571e54feb5b10074895888a90ec7f299637
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-02 12:47:17 -0400

    ingles 2 | intro reminder | grammar-plus | 14

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/14.js
@@ -0,0 +1,19 @@
+/* 14   Simple past statements: regular verbs and irregular verbs
+
+Use simple past verbs to talk about the past.
+Regular verbs end in -ed:
+    I watched TV last night.
+
+For verbs end in -e, add -d:
+    live ----> lived.
+
+For verbs ending in vowel + consonant, double the consonant and add -ed:
+    shop ----> shopped.
+
+
+Use didn't + base form in negative statements.
+The form doesn't change:
+    He didn't shop for groceries yesterday.
+
+
+*/
