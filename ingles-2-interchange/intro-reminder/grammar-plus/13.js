605c8d948afcc081070f1fac16c7116483c97f1e
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-02 12:27:11 -0400

    ingles 2 | intro reminder | grammar-plus | 13 B

--- ingles-2-interchange/intro-reminder/grammar-plus/13.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/13.js
@@ -14,13 +14,39 @@ Choose the correct words.
 a.  Excuse me. Is there a post office around here?
 b.  Yes, there is. It's on Maple Street.
 a.  where on Maple?
-b.  It's in the corner of Maple Street and Second Avenue.
+b.  It's on the corner of Maple Street and Second Avenue.
 a.  Next to Charlie's Restaurant?
-b.  Yes, that's right. It's across the street to the Windsor Hotel.
+b.  Yes, that's right. It's across the street from the Windsor Hotel.
 a.  Thanks. Oh, and where is the bank?
 b.  It's on Oak Street -between the  hospital and police station.
 a.  Great. Thanks very much.
 b.  You're welcome.
 
+*/
+
+/* 2    Directions
+
+Wals up/Go up mean the same thing.
+Walk down/Go down also mean the same thing.
+
+
+Jenna doesn't know Manhattan at all. Correct Janna's directions.
+Write the opposite of what she says.
+
+1.  Cal     How do I get to Washington Squere Park?
+    Jenna   Walk up Fifth Avenus.
+    You     No, don't walk up Fifth Avenue. Walk down Fifth Avenue.
+
+2   Cal     How can I get to the Empire State Building?
+    Jenna   Turn right on 32nd Street.
+    You     No, don't turn right on 32nd Street. turn left on 32nd Street.
+
+3.  Cal     How do I get to Bryant Park from Rockefeller Center?
+    Jenna   Go down Sixth Avenue.
+    You     No, don't go down Sixth Avenue. Go up Sixth Avenue.
+
+4.  Cal     How do I get to Central Park?
+    Jenna   Walk down Eighth Avenue.
+    You     No, don't walk down Eighth Avenue. walk up Eighth Avenue.
 
 */
