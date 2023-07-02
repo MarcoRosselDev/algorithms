892950f54fb007e61f30e6e9675eeeefa0c0a164
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-02 11:17:31 -0400

    ingles 2 | intro reminder | grammar-plus | 12 B

--- ingles-2-interchange/intro-reminder/grammar-plus/12.js
+++ ingles-2-interchange/intro-reminder/grammar-plus/12.js
@@ -30,3 +30,42 @@ a.  Thanks.
 
 
 */
+
+/* 2    Imperatives
+
+Use the base form of the verb in affirmative imperatives:
+    Go home and rest, Ms. Lake.
+
+Use don't + base form of the verb in negative imperative. The form doesn't change:
+    Don't go to work today, Ms. Lake.
+
+
+Read the situations. Give the people advice.
+Use the phrases in the box.
+
+drink coffee in the evening
+eat any cold food
+exercise today or tomorrow
+take an antacid
+take two aspirins
+work too hard
+
+1.  Dave: "I can'r sleep at night."
+    Don't drink coffee in the evening.
+
+2.  Corey:  "I have a headache".
+    You can take two aspirins.
+
+3.  I work 23 hours a day.
+    You don't work to hard.
+
+4.  My legs hurt.
+    Don't exercise today or tomorrow.
+
+5.  I have a toothache.
+    Don't eat any cold food.
+
+6.  I have an awful stomachache.
+    You can take an antacid.
+
+*/
