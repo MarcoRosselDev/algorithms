ebe50b72450301e34bb20c83cc0a5954117f865d
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-03 06:57:19 -0400

    ingles 2 | intro reminder | grammar-plus | 16 A

---
+++ ingles-2-interchange/intro-reminder/grammar-plus/16.js
@@ -0,0 +1,24 @@
+/* 1    Subject and object pronouns
+
+Subject pronouns usually come before verbs, and object pronouns go after verbs:
+    I saw him, but he didn't see me.
+
+
+Complete the conversations.
+
+1.  a.  Hello. Is Mr. Chang there?
+    b.  No, He's not here right now. Can take a message?
+    a.  Yes, Please tell  Him to call Todd Harris.
+    b.  Does he have your number?
+    a.  No, but please give it to him. It's 555.
+
+2.  a.  Oh, hello, Kimberly!
+    b.  Hello, Mrs. Sanchez. Is Veronica home?
+    a.  No, She's at the mall with her brother. Their dad drove them there this morning.
+        Would you like to come in?
+    b.  Thank you, Mrs. Sanches, but I need to go home. Anyway, my sister and I are 
+        going to an amusement park tomorrow and maybe Veronica can go with us.
+    a.  Sure. I can give her your message, or you can text her.
+    b.  Oh, don't worry. Mrs. Sanchez. I'll text her. Thanks a lot. Bye!
+
+*/
