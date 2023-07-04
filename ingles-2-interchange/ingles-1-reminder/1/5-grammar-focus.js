e7a50d60d84079cf3aa0c1ebfefdbf86a221e2b3
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-04 11:40:09 -0400

    ingles 2 | intro reminder | ingles 1 | unit 1-5

---
+++ ingles-2-interchange/ingles-1-reminder/1/5-grammar-focus.js
@@ -0,0 +1,26 @@
+/* Statements with be: possessive adjectives
+
+Possessive adjectives
+
+I'm     my
+You're  your
+He's    his
+She's   her
+It's    its
+We're   our
+They're their
+
+A   Complete these sentences.
+
+1.  My name is Aiko Yoshida. I am from Japan. my family is in Nagoya.
+    My brother is a college student. His name is Haruki.
+
+2.  My name is Matias. I am from Santiago. It's a really nice city.
+    My sister is a student here. Our parents are in Chile right now.
+
+3.  I am Angelica, but everyone calls me Angie. My last name is Newton.
+    I am a student at City Collega. My parents are on vacation this week.
+    They're in Las Vegas.
+
+
+*/
