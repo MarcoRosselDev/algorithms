e7fdebfb33906534a6336b0098ef72d17820d792
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-07 19:35:17 -0400

    ingles 2 | intro reminder | ingles 1 | unit 1-8

---
+++ ingles-2-interchange/ingles-1-reminder/1/8.js
@@ -0,0 +1,26 @@
+/* 8    GRAMMAR FOCUS
+
+Yes/No questions and short answers with be
+
+Are you free?                           Yes, I am.          No, I'm not.
+Is Arturo from Mexico                   Yes, he is.         No, he's not./No, he isn't.
+Is alexa's class in the morning?        Yes, it is.         No, it's not./No, it isn't.
+Are you and Alexa in the same class?    Yes, we are.        No, we're not./No, we aren't.
+Are your classes interesting?           Yes, they are.      No, They're not./No, they aren't.
+
+
+A   Complete the conversations.
+
+1.  a.  Is Mr. Jones from the United States?
+    b.  Yes, he is. He's from Baltimore.
+
+2.  a.  Is English class at 2?
+    b.  No, it isn't. It's at 3.
+
+3.  a.  Are you and Giovanna from Italy?
+    b.  Yes, we are. We're from Milan.
+
+4.  a.  Are Mr. and Mrs. Flores Brazilian?
+    b.  No, they aren't. They're Peruvian.
+
+*/
