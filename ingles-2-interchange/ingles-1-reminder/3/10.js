71b7e96edd2fddbc0c87658447ececa59877996f
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-08 00:15:49 -0400

    ingles 2 | intro reminder | ingles 1 | unit 3 - 10

---
+++ ingles-2-interchange/ingles-1-reminder/3/10.js
@@ -0,0 +1,40 @@
+/* 10   GRAMMAR FOCUS
+
+Preferences;    Comparisons with adjectives
+
+Which dress do you prefer?
+    I prefer the blue one.          It's nicer than the black one.
+
+Which one do you like more?
+    I like the blue one more.       It's lighter than the black one.
+
+Which one do you like better?
+    I like the black one better.    It's more stylish tan the blue one.
+
+
+Spelling
+
+cheap       cheaper
+nice        nicer
+big         bigger
+pretty      Prettier
+
+
+A   Complete these conversations.
+
+1.  a.  Which of these jackets do you like more?
+    b.  I prefer the leather one. 
+        The design is nicer and it looks more expensive than the wool one.
+
+2.  a.  These sweaters are nice.
+        Which one do you prefer?
+    b.  I like the gray one better.
+        The color is prettier.
+        It's more attractive than the brown and yellow one.
+
+3.  a.  Which rings do you like better?
+    b.  I like the silver ones more.
+        They're smaller than the gold ones.
+        And they're cheaper.
+
+*/
