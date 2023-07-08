590203fa3b7aa43424e6abf62285ebbc4fccce52
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-07 20:01:30 -0400

    ingles 2 | intro reminder | ingles 1 | unit 2 - 5

---
+++ ingles-2-interchange/ingles-1-reminder/2/5.js
@@ -0,0 +1,55 @@
+/* 5    GRAMMAR FOCUS
+
+Simple present Wh-questions and statements
+
+What do you do?
+    I'm a student. I have a part-time job, too.
+
+Where do you work?
+    I work at a restaurant.
+
+Where do you go to school?
+    I go to the University of Texas.
+
+--------------------------------------the verb add an -s at the end
+What does Amy do?
+    She's a dancer.
+
+Where does she work?
+    She works at a dancer company.
+    She travels, too.
+
+How does she like it?
+    She loves it.
+
+
+
+A   Complete these conversations.
+
+1.  a.  What do you do?
+    b.  I'm a full-time student. I study the piano.
+    a.  And Where do you go to school?
+    b.  I go to the Brooklyn School of Music.
+    a.  Wow! How do you like your classes?
+    b.  I like them a lot.
+
+2.  a.  What does Tanya do?
+    b.  She's a teacher. She teaches an art class at a school in Denver.
+    a.  And what about Ryan? Where does he work?
+    b.  He works for a big computer company in San Fransisco.
+    a.  What does he do, exactly?
+    b.  He's a web designer. He designs fantastic websites.
+
+3.  a.  What do Bruce and Ivy do?
+    b.  They work at an Italian restaurant. It's really good.
+    a.  That's nice. What is Ivy's job?
+    b.  Well, she manages the finances and Bruce works in the kitchen.
+
+4.  a.  Where does Ali work?
+    b.  He works at the University. He has a part-time job.
+    a.  Really? What does he do?
+    b.  He does office work.
+    a.  How does he like it?
+    b.  Not much, but he gets some extra money to spend!
+
+*/
