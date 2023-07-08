c71ffede3f81ccc26d42ad6e8cdf915fc111df81
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-07 20:40:04 -0400

    ingles 2 | intro reminder | ingles 1 | unit 3 - 3

---
+++ ingles-2-interchange/ingles-1-reminder/3/3.js
@@ -0,0 +1,24 @@
+/* 3    GRAMMAR FOCUS
+
+Demostratives;  one, ones.
+
+How much is this T-shirt?
+How much is that T-shirt?
+    Which one?
+
+How much is this one?
+How much is that one?
+How much is the blue one?
+    It's $28.99             ===     twenty-eight ninety-nine.
+
+How much are these sneakers?
+How much are those sneakers?
+    Which ones?
+
+How much are these?
+How much are those?
+    The gray ones.
+        They're $40.        ===     Fourtyn dollars.
+
+
+*/
