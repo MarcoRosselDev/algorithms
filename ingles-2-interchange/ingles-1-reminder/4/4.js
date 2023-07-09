fbda0ea9488be78c47088db86515ec0864fb440a
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-08 21:36:58 -0400

    ingles 2 | intro reminder | ingles 1 | unit 4 - 4

---
+++ ingles-2-interchange/ingles-1-reminder/4/4.js
@@ -0,0 +1,38 @@
+/* 4    GRAMMAR FOCUS
+
+Do you like country music?
+    Yes, I do. I love it.
+    No, I don't. I don't like it very much.
+
+Does she play the piano?
+    Yes, she does. She play very well.
+    No, she doesn't. She doesn't play an instruments.
+
+    What does she play?
+        She play the guitar.
+
+Do they like imagine Dragons?
+    Yes, they do. They like them a lot.
+    No, they don't. They don't like them at all.
+
+    Who do they like?
+        They like maroon 5.
+
+
+
+Complete these conversations.
+
+1.  a.  I like Alabama Shakes a lot. Do you know them?
+    b.  Yes, I do, and I love this song. Let's download it.
+
+2.  a.  Do you like science fiction movies?
+    b.  Yes, I do. I like them very much.
+
+3.  a.  Do Vinnie and Midori like soap operas?
+    b.  vinnie does, but Midori doesn't. She hates them.
+
+4.  a.  What kind of music does Maya like?
+    b.  Classical music. She loves Yo-Yo Ma.
+    a.  Yeah, he's amazing. I like him a lot.
+
+*/
