dd588f182596832e3af7a065d21471ff0a9fb2ec
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-08 22:25:57 -0400

    ingles 2 | intro reminder | ingles 1 | unit 5 - 5 A

---
+++ ingles-2-interchange/ingles-1-reminder/5/5.js
@@ -0,0 +1,23 @@
+/* 5    GRAMMAR FOCUS
+
+Present continuous
+
+Are you living at home now?                     Yes, I am.      No, I'm not.
+I your sister working in another city?          Yes, she is.    No, she's not./No, she isn't.
+Are your parents studying English this year?    Yes, they are.  No, they're not./No, they aren't.
+
+Where are you working now?              I'm not working. I need a job.
+What is your brother doing?             He's traveling in Thailand.
+what are your friends doing these days? They're studying for their exams.
+
+
+
+A   Complete these phone conversations using the present continuous.
+
+a.  Hi, Brittany. What are you doing?
+b.  Hey, Zach. I'm eating a sandwich at O'Connor's.
+a.  Mmmmm! Is it good?
+b.  Yeah. It's delicious. Wait, they are bringing my dessert now.
+    Iis' chocolate cake with ice cream. Call you later! Bye!
+
+*/
