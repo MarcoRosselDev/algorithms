2a8ad2e6dd30a9fc8b83f70741f3470c5c96c399
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-08 22:14:36 -0400

    ingles 2 | intro reminder | ingles 1 | unit 4 - 9

---
+++ ingles-2-interchange/ingles-1-reminder/4/9.js
@@ -0,0 +1,24 @@
+/* 9    GRAMMAR FOCUS
+
+Would; verb + to + verb
+
+Would you like to go out on Friday?
+    Yes, I would.
+    Yes, I'd love to. Thanks.
+
+
+Would you like to go to a concert?
+    I'd like to, but I have to work late.
+    I'd like to, but I need to save money.
+    I'd like to, but I want to visit my parents.
+
+
+
+A   Respond to three invitations.
+    Then write three invitations for the given responses.
+
+
+1.  a.  I have tickets to the soccer game on Sunday. Would you like to go?
+    b.  Yes, I'd like to. Thanks.
+
+*/
