2bc7228b4e2862c8fe2a22e0ccaa6daf522dea1d
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-08 22:51:06 -0400

    ingles 2 | intro reminder | ingles 1 | unit 5 - 10

---
+++ ingles-2-interchange/ingles-1-reminder/5/10.js
@@ -0,0 +1,34 @@
+/* 10   GRAMMAR FOCUS
+
+Quantifiers
+
+All
+nearly all      families have only one child.
+most
+many
+a lot of        families are smaller these days.
+some
+not many        couples have more than one child.
+few
+no one          get married before the age of 18.
+
+
+
+A   Rewrite these sentences using quantifiers.
+
+1.  In the U.S., 69% of high school students go to collage.
+    In the U.S., most high school students go to collage.
+
+2.  Seven percent of the people in Brazil are age 65 or older.
+    Few people in Brazil are age 65 or older.
+
+3.  In India, 0% of the people vote before the age of 18.
+    In India, no one vote before the age of 18.
+
+4.  Forty percent of the people in Sweden live alone.
+    Some people in Sweden live alone.
+
+5.  In Canada, 22% of the people speak French at home.
+    In Canada, Not many of the people speak French at home.
+
+*/
