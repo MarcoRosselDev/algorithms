8480892246c82e653dc6ab564c8a2574866bb521
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-08 23:16:51 -0400

    ingles 2 | intro reminder | ingles 1 | unit 7 - 3

---
+++ ingles-2-interchange/ingles-1-reminder/7/3.js
@@ -0,0 +1,37 @@
+/* 3    GRAMMAR FOCUS
+
+Simple past
+
+Did you work on Saturday?
+    Yes, I did. I worked all day.
+    No, I didn't. I didn't work at all.
+
+Did you go anywhere last weekend?
+    Yes, I did. I went to the movies.
+    No, I didn't. I didn't go anywhere.
+
+What did Neil do on Saturday?
+    He stayed home and studied for a test.
+
+How did Cara spend her weekend?
+    She went to a club and danced with some friends.
+
+
+A   Complete these conversations.
+
+1.  a.  Did you stay home on Sunday?
+    b.  No, I called my friend Anna. We drove to a nice little restaurant for lunch.
+
+2.  a.  How did you spend your last birthday?
+    b.  I had a party. Everyone enjoyed it, but the neighbors next door didn't like the noise.
+
+3.  a.  What did you do last night?
+    b.  I saw a sci-fi movie at the Cineplex. I loved it! Amazing special effects!
+
+4.  a.  Did you do anything special over the weekend?
+    b.  Yes, I did. I went shopping. Unfortunately, I spent all my money. Now I'm broke!
+
+5.  a.  Did you go out on Friday night?
+    b.  No, I didn't. I invited friend over, and I cooked spaghetti for them.
+
+*/
