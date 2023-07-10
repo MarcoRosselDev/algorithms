e68e6f180da41f05e2ed299acb06a6cdcf8ade58
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-09 20:48:55 -0400

    ingles 2 | intro reminder | ingles 1 | unit 7 - 10

---
+++ ingles-2-interchange/ingles-1-reminder/7/10.js
@@ -0,0 +1,48 @@
+/* 10   GRAMMAR FOCUS
+
+Were you in California?
+    Yes, I was.
+
+Was the weather OK?
+    No, It wasn't
+
+Were you and your cousin on vacation?
+    Yes, we were.
+
+Were your parents there?
+    No, they weren't.
+
+How long were you away?
+    I was away for a week.
+
+How was your vacation?
+    It was excellent!
+
+
+Contractions
+
+wasn't = was not.
+weren't = were not.
+
+
+
+
+
+Complete these conversations.
+
+1.  a.  Were you in New York last weekend?
+    b.  No, I wasn't. I was in Chicago.
+    a.  How was it?
+    b.  It was great! But it was cold and windy as usual.
+
+2.  a.  How long were your parents in Chile?
+    b.  They were there for two weeks.
+    a.  were they in Santiago the whole time?
+    b.  No, they weren't. They also went to Valparaiso.
+
+3.  a.  Were you away last week?
+    b.  Yes, I was in Madrid.
+    a.  Really? How long were you there?
+    b.  For almos a week. I was there on business.
+
+*/
