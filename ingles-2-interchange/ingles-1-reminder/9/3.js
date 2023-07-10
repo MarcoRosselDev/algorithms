42bbda64963689806bb7ea7640e08cd740a3c080
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-09 20:57:27 -0400

    ingles 2 | intro reminder | ingles 1 | unit 9 - 3

---
+++ ingles-2-interchange/ingles-1-reminder/9/3.js
@@ -0,0 +1,14 @@
+/* 3    GRAMMAR FOCUS
+
+Describing people
+
+General appearence
+
+What does she look like?
+    She's tall, with brown hair.
+    She's pretty.
+
+Does he wear glasses?
+    No, he wears contacts.
+
+*/
