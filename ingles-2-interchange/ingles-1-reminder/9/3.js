b5a46da615ede2bd73a4c0a962b7792b2d3cd26f
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-10 22:53:33 -0400

    ingles 2 | intro reminder | ingles 1 | unit 9 - 3 A

--- ingles-2-interchange/ingles-1-reminder/9/3.js
+++ ingles-2-interchange/ingles-1-reminder/9/3.js
@@ -11,4 +11,31 @@ What does she look like?
 Does he wear glasses?
     No, he wears contacts.
 
+
+Height
+    How tall is she?
+        She's 1 meter 78.
+        She's 5 foot 10.
+
+    How tall is he?
+        He's medium height.
+
+
+Hair
+    How long is her hair?
+        It's pretty short.
+
+    what color is his hair?
+        It's dark/light brown.
+
+
+Age
+    How old is she?
+        She's about 32.
+        She's in her thirties.
+
+    How old is he?
+        He's in his twenties.
+
+
 */
