804db28db2a72d4bec8760523362bc31de28dc4d
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-10 23:25:08 -0400

    ingles 2 | intro reminder | ingles 1 | unit 9 - 9

---
+++ ingles-2-interchange/ingles-1-reminder/9/9.js
@@ -0,0 +1,13 @@
+/* 9    GRAMMAR FOCUS
+
+Modifiers with present participles and prepositions
+
+Who's Diego?            He's the man    wearing a blue shirt.
+Which one is Diego?     He's the one    talking to Brooke.
+
+Who's Brooke?                   She's the woman     with long black hair.
+Which one is Paula?             She's the tall one  in jeans.
+Who are the Harrisons?          They're the people  next to the window.
+Which ones are the Harrisons?   They're the ones    on the couch.
+
+*/
