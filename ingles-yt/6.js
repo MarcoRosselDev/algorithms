86cd0fd8d31d3d46da04c8d355ee9486f806825d
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-12 01:03:53 -0400

    ingles yt | 6 A

--- ingles-yt/6.js
+++ ingles-yt/6.js
@@ -13,5 +13,48 @@ I like the sky.
 Me gusta mirar el cielo.
 
 I like to see the sky.
+-----------------------
+No me gusta mirar el cielo
+
+I don't like to see the sky
+-----------------------
+
+Auxiliar hacer (do) para hacer preguntas
+
+do          negative        neg contraction
+I do        I do not        I don't
+You do      You do not      You don't
+he does     He does not     He doesn't
+she does    She does not    She doesn't
+it does     It does not     It doesn't
+we do       We do not       We don't
+they do     They do not     they don't
+
+---------------------------------------
+traducir
+
+No te gusta ver el cielo
+
+You don't like to see the sky.
+-----------------------
+A ella no le gusta ver el cielo y las estrellas
+
+she doesn't like to see the sky and the stars.
+-----------------------
+Mirar television
+To watch T.V.
+To watch television.
+-----------------------
+No me gusta ver television
+
+I don't like to watch television.
+-----------------------
+A Marco no le gusta mirar las estrellas
+
+Marco doesn't like to see the stars.
+-----------------------
+A Maria no le gusta ver television con mi papa.
+
+Maria doesn't like to watch television with mi dad.
 
 */
