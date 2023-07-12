579c5b66103e42127a867a5eefe41d4b2e914f01
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-12 00:35:58 -0400

    ingles yt | 6

---
+++ ingles-yt/6.js
@@ -0,0 +1,17 @@
+/* verbos
+ver = see       ----> algo grande como el cielo.
+mirar = watch   ----> una pantalla o algo concreto.
+-----------------------
+cielo = sky
+-----------------------
+traducir
+
+Me gusta el cielo.
+
+I like the sky.
+-----------------------
+Me gusta mirar el cielo.
+
+I like to see the sky.
+
+*/
