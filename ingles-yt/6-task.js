d86d926027135472ecc5f853bd96c5cd8835c513
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-12 05:06:57 -0400

    ingles yt | 6 task A

--- ingles-yt/6-task.js
+++ ingles-yt/6-task.js
@@ -36,5 +36,24 @@ She eats            She doesn't eat
 He eats             He doesn't eat
 we eat              We don't eat
 They eat            They don't eat
+------------------------------------------------------
+2.  Work, present continuous of work is working
+    then
+
+present continuous |     negative
+
+I'm working         I'm not working
+She's working       She's not working
+He's working        He's not working
+We're working       We're not working
+They're working     they're not working
+
+present simple      |   negative
+
+I work              I don't work
+She works           She doesn't work
+He works            He doesn't work
+We work             We don't work
+They work           they don't work
 
 */
