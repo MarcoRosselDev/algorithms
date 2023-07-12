3b13cf3726d54884aede3364d481c135d56c8b87
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-12 04:59:06 -0400

    ingles yt | 6 task

---
+++ ingles-yt/6-task.js
@@ -0,0 +1,40 @@
+/* conjugar
+
+1.  eat     comer
+2.  work    trabajar
+3.  sleep   dormir
+4.  sing    cantar
+5.  read    leer
+
+con los pronombres = 
+
+1.  I
+2.  She
+3.  he
+4.  we
+5.  They
+
+en presente continuo
+en presente simple
+
+------------------------------------------------------
+1.  Eat, present continuous of eat is eating
+    then
+
+present continuous |     negative
+
+I'm eating          I'm not eating
+She's eating        She's not eating
+He's eating         He's not eating
+we're eating        We're not eating
+they're eating      They're not eating
+
+present simple      |   negative
+
+I eat               I don't eat
+She eats            She doesn't eat
+He eats             He doesn't eat
+we eat              We don't eat
+They eat            They don't eat
+
+*/
