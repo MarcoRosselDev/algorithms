c196b8a7b1b7c682a2598954e9a52918595d69bf
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-11 23:46:30 -0400

    ingles yt | 5

---
+++ ingles-yt/5.js
@@ -0,0 +1,37 @@
+/* 
+el articulo [the] englova [el, la, los, las]
+
+the cats, the film, 
+----------------------------
+Me gusta comer
+I like to eat
+
+Me gusta la pelicula
+I like the movie.
+----------------------------
+Si lleva verbo add el auxiliar to
+Si no lleva accion add el article the
+----------------------------
+Traducir
+
+Me gusta caminar
+I like to walk
+
+Me gusta cocinar
+I like to cook
+
+Me gusta dormir
+I like to sleep
+
+-------------------------------
+Ir = go || con = with
+-------------------------------
+Me gusta comer con tigo
+
+I like to eat with you.
+-------------------------------
+Me gusta cocinar con mi hermano
+
+I like to cook with my brother.
+
+*/
