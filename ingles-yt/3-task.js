1d869eae516948f5f6ddaade56e6a985fe54d1ce
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-11 22:16:31 -0400

    ingles yt | 3 task

---
+++ ingles-yt/3-task.js
@@ -0,0 +1,23 @@
+/* conjugar be + prosent continuous of this 5 verbs
+
+1.  cocinar
+2.  comer
+3.  dormir
+4.  trabajar
+5.  caminar
+
+------------------------------
+1.  infinitivo de cocinar = cook;
+    present continuous of cook = cooking;
+    then --->
+
+I'm cooking     Estoy cocinando
+You're cooking  Estas cocinando
+He's cooking    El esta cocinando
+She's cooking   Ella esta cocinando
+It's cooking    Eso esta cocinando
+They're cooking Ellos estan cocinando
+You're cooking  Ustedes estan cocinando
+We're cooking   Nosotros estamos cocinando
+
+*/
