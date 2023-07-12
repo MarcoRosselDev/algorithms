15b0c2becaa48a79ecca8a6eb655b53c6c13c3d0
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-11 23:20:43 -0400

    ingles yt | 4

---
+++ ingles-yt/4.js
@@ -0,0 +1,28 @@
+/* 
+traducir
+
+Nosotros estamos aqui.
+
+We're here.
+----------------------------
+Estamos aqui?
+
+Are we here?
+----------------------------
+Tu eres mi amiga
+
+You're my friend
+----------------------------
+Eres mi amiga?
+
+Are you my friend?
+----------------------------
+Estas durmiendo
+
+You're sleeping
+----------------------------
+Estas durmiendo?
+
+Are you sleeping?
+
+*/
