b1e8d89cd283edda15493bb858f7f9b64eaea010
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-11 21:44:17 -0400

    ingles yt | 3

---
+++ ingles-yt/3.js
@@ -0,0 +1,31 @@
+/* 
+exercise 1
+
+Tus papas estan trabajando
+
+1.  You parents is working.
+2.  Your parents are working.
+3.  Youre parents are working.
+
+answer = 2.
+
+---------------------------------------------------
+exercise 2
+
+Ustedes estan trabajando
+
+1.  You're working.
+2.  They're working.
+3.  We're working.
+
+answer = 1.
+
+---------------------------------------------------
+traduce
+
+Maria esta comiendo una ensalada.
+
+Maria is eating a salad.
+
+
+*/
