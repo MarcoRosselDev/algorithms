bea6cf6882d7c83bb0f78b66a50993d4590a4ad9
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-14 05:42:05 -0400

    ingles yt | 9 task

---
+++ ingles-yt/unidad-2/9-task.js
@@ -0,0 +1,41 @@
+/* Task 
+
+Presente, 
+presente continuo, 
+pasado simple positivo.
+pasado simple negativo.
+pasado simple pregunta.
+
+Ella bailo con mi hermano
+ellos comieron conmigo
+nosotros compramos veinte libros
+tu llamaste a mi papa
+
+----------------------------------------
+Ella bailo con mi hermano
+
+Presente,
+    She dance with my brother
+presente continuo,
+    She's dancing with my brother
+pasado simple positivo.
+    She danced with my brother
+pasado simple negativo.
+    She didn't dance with my brother.
+pasado simple pregunta.
+    Did she dance with my brother?
+
+----------------------------------------
+ellos comieron conmigo
+
+Presente,
+    They eat with me.
+presente continuo,
+    They're eating with me.
+pasado simple positivo.
+    They ate with me.
+pasado simple negativo.
+    They didn't eat with me.
+pasado simple pregunta.
+    Did they eat with me
+*/
