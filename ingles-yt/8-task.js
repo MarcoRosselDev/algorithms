92d0cb61bf94970f6bcaab196d2f3618e056b426
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-14 00:15:43 -0400

    ingles yt | examen 1

--- ingles-yt/8-task.js
+++ ingles-yt/8-task.js
@@ -13,10 +13,10 @@ El no tiene 5 manzanas.
 Resolution:
 
 She has five apples.
-She doesn't has five apples.
+She doesn't have five apples. -----> solo has en afirmativos
 I have five apples.
 I don't have 5 apples.
 He has five apples.
-He doesn't has five apples.
+He doesn't have five apples.
 
 */
---
+++ ingles-yt/examen-1.js
@@ -0,0 +1,42 @@
+/* 
+1.  Mi amiga esta comiendo
+
+My friend is eating. = d;
+--------------------------------------------------
+2.  Mi hermano esta durmiendo.
+
+My brother is sleeping. = c;
+--------------------------------------------------
+3.  Tu eres mi amigo y el es tu amigo
+
+You're my friend and he's your friend. = c;
+--------------------------------------------------
+4.  Tu eres mi amigo y yo tengo tu libro azul.
+
+You're my friend and I have your blue book.
+--------------------------------------------------
+5.  Buenos dias, hoy es martes.
+
+Good morning, today is tuesday. = b;
+--------------------------------------------------
+6.  Marie camina con mi amigo
+
+Marie is walking with my friend. = a;
+--------------------------------------------------
+1.  22467001
+2.  79022892
+3.  13409976
+--------------------------------------------------
+Pablo and Juan
+
+red an blue
+Pablo ask to Juan about her favorite color?
+This combersation is maked on Tuesday
+Pablo's favorite colors are Red and Blue.
+Juan doesn't like Green color. 
+--------------------------------------------------
+--------------------------------------------------
+--------------------------------------------------
+--------------------------------------------------
+
+*/
