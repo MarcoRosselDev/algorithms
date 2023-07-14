c9d0ac61143c3b595ea795abeedb649d9f09b797
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-13 23:02:23 -0400

    ingles yt | 8 task

---
+++ ingles-yt/8-task.js
@@ -0,0 +1,22 @@
+/* Task
+
+Traducir:
+
+Ella tiene 5 manzanas.
+Ella no tiene 5 manzanas.
+Yo tengo 5 manzanas.
+Yo no tengo 5 manzanas.
+El tiene 5 manzanas.
+El no tiene 5 manzanas.
+
+----------------------------------
+Resolution:
+
+She has five apples.
+She doesn't has five apples.
+I have five apples.
+I don't have 5 apples.
+He has five apples.
+He doesn't has five apples.
+
+*/
