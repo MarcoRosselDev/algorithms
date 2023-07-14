8cded12b1b747be315295bf5f78503ced4d30808
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-13 22:58:12 -0400

    ingles yt | 8 A

--- ingles-yt/8.js
+++ ingles-yt/8.js
@@ -39,6 +39,28 @@ Tengo 20 manzanas y 22 platanos
 
 I have twenty apples and twenty-two bananas
 ----------------------------------
+Tengo 15 manzanas y 50 platanos
 
+I have fifteen apples and fifty bananas.
+----------------------------------
+Tengo 4 libros azules pero marco tiene 8 libros rojos.
+
+I have four blue books but Marco has eight red books.
+----------------------------------
+No tengo 5 manzanas
+
+I don't have five apples.
+----------------------------------
+No tengo un libro verde
+
+I don't have a green book.
+----------------------------------
+Mi libro rosa.
+
+My pink book.
+----------------------------------
+Alejandra tiene 3 platanos, pero no tiene 4 libros negros.
+
+Alejandra has three bananas, but she doesn't have four black books.
 
 */
