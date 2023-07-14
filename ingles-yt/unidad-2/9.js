94534c6205d4c842492f469c6c367c920d1d7054
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-14 01:28:09 -0400

    ingles yt | 9

---
+++ ingles-yt/unidad-2/9.js
@@ -0,0 +1,20 @@
+/* 
+
+Yo leo mi libro
+
+I read my book.
+---------------------------------
+Estoy leyendo mi libro
+
+I'm reading my book.
+---------------------------------
+de infinitivo a pasado simple add -ed
+
+trabajar = work = worked
+cocinar = cook = cooked 
+bailar = dance = danced
+---------------------------------
+---------------------------------
+---------------------------------
+---------------------------------
+*/
