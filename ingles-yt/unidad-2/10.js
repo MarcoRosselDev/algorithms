a1ac4275578db0ab87053b15263db8b11eda7f05
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-14 19:25:56 -0400

    ingles yt | 10

---
+++ ingles-yt/unidad-2/10.js
@@ -0,0 +1,45 @@
+/* class 10
+
+Where = donde?
+On = sobre/en
+under = bajo
+
+-------------------------------
+La manzana esta sobre la caja
+
+The apple is on the box.
+
+ti -vocal       ta -consonante 
+-------------------------------
+La manzana esta bajo la caja
+
+The apple is under the box
+
+ti                  ta
+-------------------------------
+between = entre
+in = dentro
+-------------------------------
+La manzana esta entre las cajas
+
+The apple is between the boxes
+-------------------------------
+La manzana esta dentro de la caja
+
+The apple is in the box
+-------------------------------
+behind = detras
+next to = al lado
+-------------------------------
+La manzana esta detras de la caja
+
+The apple is behind the box.
+-------------------------------
+La manzana esta al lado de la caja
+
+The apple is next to the box
+-------------------------------
+-------------------------------
+-------------------------------
+
+*/
