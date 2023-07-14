1a941dcb7aa1ffa70120565c311613db4e629997
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-14 04:54:39 -0400

    ingles yt | 9 A

--- ingles-yt/unidad-2/9.js
+++ ingles-yt/unidad-2/9.js
@@ -14,7 +14,45 @@ trabajar = work = worked
 cocinar = cook = cooked 
 bailar = dance = danced
 ---------------------------------
+verbos irregulares no terminan en -ed
+me los tengo que momorizar a fuerza
 ---------------------------------
+3 verbos irregulares
+
+eat = ate       comer
+buy = bought    comprar
+be = was/were   ser/estar
 ---------------------------------
+Ella comio pizza anoche.
+
+She ate pizza last night
+---------------------------------
+Ellos compraron un libro anoche.
+
+They bought a book last night.
+---------------------------------
+Was - Were
+
+I/he/she/it = was
+we/you/they = were
+---------------------------------
+Ella era mi amiga.
+
+She was my friend.
+---------------------------------
+Nosotros fuimos amigos
+
+We were friends
+---------------------------------
+yo compre un helado
+
+I bought an ice cream
+---------------------------------
+ellos compraron los libros con mi mama
+
+They bought the books with my mom.
+---------------------------------
+The -- then bocal = thi
+The -- then consonante = tha
 ---------------------------------
 */
