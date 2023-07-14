b46fe2ece263f6bb58784fc6625911c8ae59d8ef
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-13 22:49:03 -0400

    ingles yt | 8

---
+++ ingles-yt/8.js
@@ -0,0 +1,44 @@
+/* Colors
+
+Amarillo    yellow
+Rosa        pink
+Morado      purple
+azul        blue
+naranja     orange
+verde       green
+blanco      white
+gris        gray
+rojo        red
+negro       black
+
+----------------------------------
+What's your favorite color?
+
+My favorite color is green
+----------------------------------
+libro = book
+----------------------------------
+Mi libro azul ---> addjective after (caracteristica despues en español)
+
+My blue book ---> addjective before (caracteristica primero en ingles)
+----------------------------------
+hava
+
+I have
+you have
+he/she/it has
+we have
+you have
+they have
+----------------------------------
+manzana = apple
+----------------------------------
+platano = banana
+----------------------------------
+Tengo 20 manzanas y 22 platanos
+
+I have twenty apples and twenty-two bananas
+----------------------------------
+
+
+*/
