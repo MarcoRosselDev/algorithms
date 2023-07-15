d7dad82fa201783ad6f8bb75c599b3e82baa35d3
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-15 19:50:26 -0400

    ingles yt | 12

---
+++ ingles-yt/unidad-2/12.js
@@ -0,0 +1,47 @@
+/* can and can't
+
+Yo no puedo    =     I can not    =   I can't
+------------------------------
+Hoy = Today
+------------------------------
+Ella no puede cocinar hoy.
+
+She can't cook today.
+------------------------------
+Alex no puede bailar contigo
+
+Alex can't dance with you.
+------------------------------
+Puedo cocinar, pero no puedo bailar.
+
+I can cook, but I can't dance.
+------------------------------
+Ellos pueden trabajar, pero no pueden dormir.
+
+They can work, but they can't sleep.
+------------------------------
+Nosotros podemos comer, pero tu no puedes.
+
+We can eat, but you can't.
+------------------------------
+Ustedes pueden caminar con mi mama, pero ellas no pueden.
+
+You can walk with my mom, but they can't.
+------------------------------
+ver = see ----> sky or something big
+mirar = watch ----> a tv or something smoll
+------------------------------
+No puedo ver el cielo
+
+I can't see the sky.
+------------------------------
+No puedo ver television
+
+I can't watch T.V.
+------------------------------
+Mi papá puede bailar, pero mi mamá no.
+
+My dad can dance, but my mom can't.
+My father can dance, but my mother can't.
+------------------------------
+*/
