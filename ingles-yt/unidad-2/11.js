86cc263ea4351a22a05d83458a64ef945ec03b15
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-15 06:12:57 -0400

    ingles yt | 11  B

--- ingles-yt/unidad-2/11.js
+++ ingles-yt/unidad-2/11.js
@@ -41,9 +41,63 @@ Tu caiste desde el gran arbol
 
 You fell from the big tree.
 -------------------------
+Tu te caiste del arbol grande
+
+You fell from the big tree.
 -------------------------
+
+hogar = home    <--- sustantivo
+auto = car      <--- sustantivo
+
+conducir = drive = drove    <--- verbo irregular
 -------------------------
+Yo manejo el auto
+
+I drive the car.
 -------------------------
+Yo manejo el auto rojo
+
+I drive the red car.
 -------------------------
+Yo maneje el auto
+
+I drove the car.
+-------------------------
+Yo maneje el auto desde casa
+
+I drove the car from home.
+-------------------------
+desde = since <--- preposition
+
+desde una temporalidad usamos since,
+como desde pequeño, desde 1990, desde las 7 a.m.
+
+para todo lo demas usamos from.
+-------------------------
+El queso se produce en la fabrica desde 1985
+
+The cheese has been produced in the factory since 1985
+-------------------------
+El manejo el auto desde casa desde la mañana
+
+He drove the car from home since the morning.
+-------------------------
+Me gustan las manzanas desde 1999
+
+I like the apples since nineteen ninety-nine
+-------------------------
+Me gusta leer desde 1978
+
+I like to read since nineteen seventy-eight.
+-------------------------
+venir = come = came <---- verbo irregular
+-------------------------
+Ellos vienen a comer
+
+They come to eat.
+-------------------------
+Ellos vinieron a comer
+
+They came to eat.
 -------------------------
 */
