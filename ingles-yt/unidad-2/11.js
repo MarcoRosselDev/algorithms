30a3d9703e047f0eccff9183795ba1395bb5b73e
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-15 05:09:43 -0400

    ingles yt | 11

---
+++ ingles-yt/unidad-2/11.js
@@ -0,0 +1,49 @@
+/* 11
+
+arbol = tree
+desde = from
+
+-------------------------
+desde el arbol
+
+from the tree
+-------------------------
+Camine con mi hermana
+
+I walked with my sister
+-------------------------
+comi con mi hermana
+
+I ate with my sister
+-------------------------
+spa | verb | past
+
+caer = fall = fell
+-------------------------
+Ella cayo desde el arbol
+
+She fell from the tree.
+-------------------------
+Adjetivos
+
+grande = big
+pequeño = small
+
+adjetivo + sustantivo
+
+a big house
+-------------------------
+Arbol grande
+
+Big tree
+-------------------------
+Tu caiste desde el gran arbol
+
+You fell from the big tree.
+-------------------------
+-------------------------
+-------------------------
+-------------------------
+-------------------------
+-------------------------
+*/
