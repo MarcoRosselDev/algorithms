969d01206bc43e6193b754390f237618b88ecd1f
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-15 03:04:14 -0400

    ingles yt | 10  task

---
+++ ingles-yt/unidad-2/10-task.js
@@ -0,0 +1,30 @@
+/* task
+
+Where are the keys?
+
+keys = llaves
+table = mesa
+
+the keys are under the table
+The keys are on the table
+
+------------------------------------
+traducir
+
+en mayo, el viernes a las 5 pm.
+
+In May, on friday at 5 p.m.
+------------------------------------
+en diciembre, el jueves a las 7 30 p.m.
+
+In December on Thursday at 7 30 p.m.
+
+Monday
+Tuesday
+wednesday
+Thursday
+friday
+saturday
+sunday
+
+*/
