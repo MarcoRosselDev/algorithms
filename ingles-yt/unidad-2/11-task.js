0bfd97c958bddf2742b49709c920a4f2641718d4
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-15 06:18:46 -0400

    ingles yt | 11  task

---
+++ ingles-yt/unidad-2/11-task.js
@@ -0,0 +1,17 @@
+/* 11 task
+
+traducir las siguiente oraciones
+
+Magdalena cae desde el gran árbol.
+Magdalena fall from the big tree.
+
+Magdalena cayó desde el gran árbol.
+Magdalena fell from the big tree.
+
+Pedro condujo el auto desde casa, desde la mañana.
+Pedro drove the car from home, since the morning
+
+Yo maneje el auto desde la mañana.
+I drove the car since the morning.
+
+*/
