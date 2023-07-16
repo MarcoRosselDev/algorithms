769845e60dc7ba11b9c92ac00506368710e137ab
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-15 20:01:14 -0400

    ingles yt | 12 task

---
+++ ingles-yt/unidad-2/12-task.js
@@ -0,0 +1,20 @@
+/* Task
+
+traducir:
+
+Yo puedo trabajar.
+I can work.
+
+Yo puedo cocinar el pollo, pero tú no.
+I can cook the chicken, but you can't.
+
+Mi amigo puede caminar, pero no puede bailar.
+My friend can walk, but he can't dance.
+
+Ustedes no pueden dormir.
+You can't sleep.
+
+Ellas pueden caminar con mi hermana.
+They can walk with my sister.
+
+*/
