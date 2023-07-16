c17c4d40224ace491713ffb3143cdfa25740fa5a
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-15 22:15:28 -0400

    ingles yt | 13

---
+++ ingles-yt/unidad-2/13.js
@@ -0,0 +1,58 @@
+/* 13
+
+El pasado de can es could
+
+Yo pude = I could       <---------- como I cud
+Yo podría = I could
+
+-------------------------------------
+I could eat pizza
+
+I could eat pizza   <-----  como diferenciar cual dece
+                            Yo pude comer pizza y
+                            Yo podria comer pizza
+
+Con los indicadores de tiempo,
+
+I could eat pizza   yesterday
+                    last week
+                    last thursday
+                    last night
+
+-------------------------------------
+Para solucionar esto se usa
+Might | may    = es posible?
+
+Could para decir podria y poder en pasado.
+
+-------------------------------------
+Para ti = for you
+-------------------------------------
+Yo podria cocinar para ti.
+
+I could cook for you.
+I may cook for you.
+I might cook for you.
+
+Las tres son correectas.
+-------------------------------------
+Yo pude cocinar para ti anoche.
+
+I could cook for you last night.
+-------------------------------------
+negativo
+
+I could not   =  yo no podria./yo no pude.
+I couldn't === suena = cudnt
+-------------------------------------
+-------------------------------------
+-------------------------------------
+-------------------------------------
+-------------------------------------
+-------------------------------------
+-------------------------------------
+-------------------------------------
+-------------------------------------
+-------------------------------------
+-------------------------------------
+*/
