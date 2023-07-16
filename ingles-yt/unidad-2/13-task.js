37cf217d52262da393335b2f5b493b99efc14714
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-16 06:30:01 -0400

    ingles yt | 13  task B

--- ingles-yt/unidad-2/13-task.js
+++ ingles-yt/unidad-2/13-task.js
@@ -2,6 +2,22 @@
 
 traducir:
 
-Yo pude 
+Yo pude trabajar contigo ayer.
+I could work with you yesterday.
+
+Yo podría cocinar el pollo, pero tú no.
+I may cook the chicken, but you may not.
+
+Mi amigo puede caminar, pero no puede bailar.
+My friend can walk, but he can't dance.
+
+Ustedes no pudieron dormiar.
+You couldn't sleep.
+
+Ellas podrían caminar con mi hermana.
+They may walk with my sister.
+
+Es posible que yo coma contigo.
+I may eat with you.
 
 */
