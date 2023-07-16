f3608dda81f09d338d37b7cbee5e1f049c6c3252
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-16 06:48:26 -0400

    ingles yt | 14

---
+++ ingles-yt/unidad-2/14.js
@@ -0,0 +1,44 @@
+/* 14
+
+Yo podria cocinar para ti
+
+I could cook for you.
+-------------------------------------
+Podria yo cocinar para ti?
+
+Could I cook for you?
+-------------------------------------
+Esta bien que haga esto?
+podria hacer x por favor?
+
+could
+-------------------------------------
+existe la posibilidad.
+Es posible que camine contigo.
+Podria no ir a trabajar magniana.
+maria podria llamar esta tarde.
+
+may / might
+-------------------------------------
+abrir = open
+cerrar = close
+puerta = door
+-------------------------------------
+Podrias abrir la puerta porfavor?
+
+could you open the door please?
+-------------------------------------
+podrias cerrar la puerta porfavor?
+
+Could you close the door please?
+-------------------------------------
+Imaginar = imagine.
+yo = me
+tu y to = you and me
+juntos = together
+-------------------------------------
+Puedes imaginarnos a ti y a mi?
+
+Can you imagine you and me?
+-------------------------------------
+*/
