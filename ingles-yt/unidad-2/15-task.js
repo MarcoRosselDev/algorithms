a42d06b34813b836dc1aa8bd571213e6a945160f
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-17 03:52:00 -0400

    ingles yt | 15  task

---
+++ ingles-yt/unidad-2/15-task.js
@@ -0,0 +1,17 @@
+/* task
+
+Traducir:
+
+¿Necesitas comprar papas?
+Do you need to buy potatoes?
+
+¿Ellos necesitan trabajar hoy?
+Do they need to work today?
+
+ustedes no necesitan comer pollo.
+You don't need to eat chicken.
+
+Ella no necesita estar contigo.
+She doesn't need be with you.
+
+*/
