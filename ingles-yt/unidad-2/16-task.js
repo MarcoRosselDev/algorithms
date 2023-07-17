e46cd4cd832c239a04d829892f54247714c1f8cc
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-17 04:25:55 -0400

    ingles yt | 16   task

---
+++ ingles-yt/unidad-2/16-task.js
@@ -0,0 +1,24 @@
+/* 16 task
+
+traducir
+
+Tengo tu libro azul
+I have your blue book.
+
+ella tiene los zapatos
+She has the shoes.
+
+Mi hermano no tiene mi bufanda
+My brother doesn't have my scarf.
+
+alejandro tiene la corbata.
+Alejandro has the tie.
+
+ellos tienen mi dinero
+They have my money.
+
+mi mamá no tiene mi botella.
+My mother does't have my bottle.
+My mom does't have my bottle.
+
+*/
