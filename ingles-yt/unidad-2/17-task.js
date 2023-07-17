d38aeaee463b1dcaa36d226718ee33a9634a9b5e
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-17 05:54:06 -0400

    ingles yt | 17  task

---
+++ ingles-yt/unidad-2/17-task.js
@@ -0,0 +1,18 @@
+/* 17 task
+
+traducir:
+
+¿Dónde obtuviste tu bufanda rosa?
+Where did you get your pink scarf?
+
+Rápido, está oscureciendo.
+Quick!, It's getting dark.
+
+Rápido, se está haciendo tarde.
+Quick!, It's getting late.
+
+No entendí.
+I didn't get it!
+
+
+*/
