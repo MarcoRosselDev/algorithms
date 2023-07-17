6101b8820f389b1758d87e781b4dd57c0ff91321
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-17 05:37:31 -0400

    ingles yt | 17

---
+++ ingles-yt/unidad-2/17.js
@@ -0,0 +1,53 @@
+/* 17
+
+Comprar
+recibir
+comprar
+recuperar   = get. --> got en pasado
+
+---------------------------------------
+Yo obtengo
+
+I get
+---------------------------------------
+Tú obtienes
+
+You get.
+---------------------------------------
+Yo obtuve un empleo
+
+I got a job.
+---------------------------------------
+Yo obtuve un nuevo empleo
+
+I got a new job.
+---------------------------------------
+Obtuve un nuevo empleo con tu hermano y tu amigo.
+
+I got a new job with your brother and your friend.
+---------------------------------------
+No me gustó
+
+I didn't like it.
+---------------------------------------
+Obtuve un nuevo empleo con tu hermano y tu amigo, 
+pero no me gustó.
+
+I got a new job with your brother and your friend, 
+but I didn't like it.
+---------------------------------------
+I like your black shirt, where did you get it?
+
+Me gusta tu camisa negra, donde la compraste?
+---------------------------------------
+Pepito, get in! = pepito, entrate!
+Pepito, get out! = pepito, fuera!
+---------------------------------------
+Marco, get out of my car!
+
+Marco, salte de mi auto!
+---------------------------------------
+---------------------------------------
+---------------------------------------
+---------------------------------------
+*/
