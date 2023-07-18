0cde14b0f07d0fa9c04fecfa8352c407e73a044a
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-18 10:13:05 -0400

    ingles yt | 21   B

--- ingles-yt/unidad-2/21.js
+++ ingles-yt/unidad-2/21.js
@@ -41,8 +41,61 @@ Yo puedo comer pollo?
 
 Can I eat chicken?
 ----------------------------------
+Yo puedo ayudarte
+
+I can help you.
 ----------------------------------
+Yo puedo ayudarte?
+
+Can I help you?
 ----------------------------------
+Como puedo ayudarte?
+
+How can I help you?
+----------------------------------
+Como puedes ayudarme?
+
+How can you help me?
+----------------------------------
+How did I win?
+
+Como gane?
+----------------------------------
+Looking for = buscar 
+----------------------------------
+I'm looking for love
+
+Estoy buscando amor.
+----------------------------------
+Estoy buscando un libro negro
+
+I'm looking for a black book.
+----------------------------------
+Good morning, How can I help you?
+
+buenos dias, como te puedeo ayudar?
+----------------------------------
+Hello, I'm looking for a blue book.
+
+Estoy buscando un libro azul.
+----------------------------------
+I see, a blue book, mmm. 
+Here, a nice blue book for you.
+----------------------------------
+wow, I love it, thank you so much!
+----------------------------------
+you're welcome!
+----------------------------------
+which book do you prefer?
+
+cual libro prefieres?
+----------------------------------
+good night!
+good night, how can I help you?
+I'm looking for a dress.
+we have a red dress and a yellow dress
+mmmm, I like the yellow dress.
+sure!
 ----------------------------------
 ----------------------------------
 */
