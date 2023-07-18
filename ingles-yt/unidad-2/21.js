172df4eccd8c85272592120f0a164457851e4db3
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-18 10:01:03 -0400

    ingles yt | 21

---
+++ ingles-yt/unidad-2/21.js
@@ -0,0 +1,48 @@
+/* 21
+
+what    que
+which   cual
+how     como/cuanto
+who     quien
+when    cuando
+where   donde
+
+----------------------------------
+quien es ese muchacho?
+
+who is that boy?
+who's that boy?
+----------------------------------
+who's that girl?
+
+quien es esa chicha?
+----------------------------------
+where is my book?
+
+Donde esta mi libro?
+----------------------------------
+where are you?
+
+Donde estas?
+----------------------------------
+where did you go?
+
+Donde fuiste?
+----------------------------------
+Yo puedo?
+
+Can I?
+----------------------------------
+Yo puedo comer pollo.
+
+I can eat chicken.
+----------------------------------
+Yo puedo comer pollo?
+
+Can I eat chicken?
+----------------------------------
+----------------------------------
+----------------------------------
+----------------------------------
+----------------------------------
+*/
