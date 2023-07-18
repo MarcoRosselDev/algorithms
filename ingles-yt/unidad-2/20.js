cd5394d5480729b733afc1107862d920761ba883
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-18 07:56:15 -0400

    ingles yt | 20  B

--- ingles-yt/unidad-2/20.js
+++ ingles-yt/unidad-2/20.js
@@ -37,17 +37,62 @@ Yo pienso que comeremos hamburgesas
 I think we will eat burgers
 I think we shall eat burgers
 -----------------------------------
+I think she will be the dancer.
+
+yo creo que ella sera la vailarina.
 -----------------------------------
+will = you / they / he / she / it
+shall = porividos con los de arriba
 -----------------------------------
+negacion
+
+won't + verbo
+
+will not
+shall not
+
 -----------------------------------
+ponerse de pie = stand up
 -----------------------------------
+I won't stand up
+
+No me levantare.
 -----------------------------------
+I won't drink water.
+
+no bebere agua.
 -----------------------------------
+Shall we walk together?
+
+caminaremos juntos?
 -----------------------------------
+I shall not be late.
+
+no llegare tarde.
 -----------------------------------
+Ellos no comeran pollo con mi hermano
+
+Thet won't eat chicken with my brother.
 -----------------------------------
+Will you eat my bread with jelly?
+
+te comeras mi pan con mermelada?
+
+No, I won't, but thanks.
+
+No, no lo are, pero gracias.
 -----------------------------------
+They will be my friends ----> pasarlo a negativo
+
+They won't be my friends
+
+Ellos no seran mis amigos.
 -----------------------------------
+You will be my friend ---> pasarlo al negativo
+
+You won't be my friend
+
+Tu no seras mi amigo
 -----------------------------------
 -----------------------------------
 -----------------------------------
