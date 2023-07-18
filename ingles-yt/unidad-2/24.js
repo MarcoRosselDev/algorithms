9954979074f4b4a9f4920df318c5bced829e631f
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-18 14:18:03 -0400

    ingles yt | 24

---
+++ ingles-yt/unidad-2/24.js
@@ -0,0 +1,41 @@
+/* 24
+
+A piece of cake
+
+Una rebanada de pastel.
+----------------------------------------
+I would like a piece of cake
+
+Me gustaria una rebanada de pastel.
+----------------------------------------
+No me gustaria una rebanada de pastel.
+
+I wouldn't like a piece of cake.
+----------------------------------------
+I'd like a piece of cake.
+
+I would like a piece of cake.
+
+Me gustaria una rebanada de pastel.
+----------------------------------------
+Good morning, sir!
+good morning.
+would you like to order?
+Yes, I'd like!
+I would like a piece of cake.
+would you like some coffee?
+yes, please, that would be nice!
+----------------------------------------
+----------------------------------------
+----------------------------------------
+----------------------------------------
+----------------------------------------
+----------------------------------------
+----------------------------------------
+----------------------------------------
+----------------------------------------
+----------------------------------------
+----------------------------------------
+----------------------------------------
+----------------------------------------
+*/
