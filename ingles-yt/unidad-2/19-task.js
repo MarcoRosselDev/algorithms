abd9b083106f4d60e1724934097ecf93bd7aed39
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-18 07:23:32 -0400

    ingles yt | 19  task

---
+++ ingles-yt/unidad-2/19-task.js
@@ -0,0 +1,17 @@
+/* 19 task
+
+traducir:
+
+Will you eat my bread with jelly?
+te comeras mi pan con mermelada?
+
+will you cook the fish?
+cocinaras el pescado?
+
+will you work this morning?
+trabajaras esta magniana?
+
+will you read the notebook or the book?
+leeras la libreta o el libro?
+
+*/
