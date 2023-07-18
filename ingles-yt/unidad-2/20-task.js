b7d673c2eb87f6250b3d1132a87290eb392c4df9
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-18 09:16:32 -0400

    ingles yt | 20   task

---
+++ ingles-yt/unidad-2/20-task.js
@@ -0,0 +1,35 @@
+/* 20 task
+
+traducior
+contestar
+pasar al negativo
+contestar el negativo
+
+---------------------------------------
+You will be my friend
+You won't be my friend
+---------------------------------------
+Will you cook the fish?
+yes, I will. I will cook the fish.
+No, I won't. I won't cook the fish.
+
+won't you cook the fish?
+Yes, I won't cook the fish.
+No, I will cook the fish.
+
+Cocinaras el pescado?
+---------------------------------------
+will you work this morning?
+Yes, I will work this morning.
+No, I won't work this morning.
+
+won't you work this morning?
+yes, I won't work this morning.
+No, I will work this morning.
+
+trabajaras esta magniana?
+---------------------------------------
+will you read the notebook or the book?
+won't you read the notebook or the book?
+
+*/
