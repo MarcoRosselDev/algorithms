5d2d21cd5ded971f34b68bb939fbfa9e56c97618
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-18 13:04:07 -0400

    ingles yt | 23

---
+++ ingles-yt/unidad-2/23-task.js
@@ -0,0 +1,21 @@
+/* 23 task
+
+traducir:
+
+Me dormi.
+I did sleep
+
+No me dormi.
+I didn't sleep
+
+Me dormi?
+Did I sleep?
+
+te dormiste? si lo hice.
+Did you sleep? yes, I did.
+
+ella se durmio? no, no lo hizo.
+Did she sleep? No, she didn't.
+
+
+*/
---
+++ ingles-yt/unidad-2/23.js
@@ -0,0 +1,68 @@
+/* 23   do
+
+do = hacer
+auxiliar para presente simple
+auxiliar para preguntas
+--------------------------------
+I do
+
+Yo lo hago.
+--------------------------------
+we do
+
+nosotros lo hacemos
+--------------------------------
+Do you like pizza?
+
+te gusta la pizza?
+--------------------------------
+Do you like wine?
+
+Te gusta el vino?
+--------------------------------
+I do not
+I do not like wine
+I don't
+I don't like wine
+--------------------------------
+Me gusta el vino?
+
+Do I like wine?
+--------------------------------
+Does Fernanda live in Mexico?
+
+Fernanda vive en Mexico?
+------------
+Yes, she does.
+She doesn't.
+--------------------------------
+do en pasado = did
+--------------------------------
+I did cook. = I cooked
+
+Yo cocine.
+--------------------------------
+pasado simpel = I cooked
+pasado = I did cook
+pero significan lo mismo., la diferencia esta en el auxiliar did.
+--------------------------------
+I did walk = I walked
+--------------------------------
+Did you like that dress?
+
+Te gusto ese vestido?
+--------------------------------
+Do you like that dress?
+
+Te gusta ese vestido?
+--------------------------------
+I do not walk with Marco.
+I don't walk with Marco.
+
+No camino con Marco.
+--------------------------------
+I did't walk with Marco
+
+No camine con Marco
+--------------------------------
+*/
