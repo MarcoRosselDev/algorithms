8cbc40fa3be56a2a2298e829464461e7cf4b9893
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-18 07:05:34 -0400

    ingles yt | 19

---
+++ ingles-yt/unidad-2/19.js
@@ -0,0 +1,60 @@
+/* 19   Futuro simple
+
+Yo camino
+
+I walk
+---------------------------------------
+Yo estoy caminando
+
+I'm walking
+---------------------------------------
+Yo camine
+
+I walked
+---------------------------------------
+futuro simple
+
+Yo caminare =/= yo voy a caminar
+yo dormire
+
+will + verbo
+---------------------------------------
+Yo caminare
+
+I will walk
+---------------------------------------
+I will drink some water, please.
+
+yo tomare un poco de agua, porfavor.
+---------------------------------------
+will you walk with my sister?
+
+caminaras con mi hermana?
+
+no es un condicionar, no es
+caminarias con mi hermana? = would you walk with my sister?
+---------------------------------------
+will you do some exercise?
+
+haras algo de ejercicio?
+---------------------------------------
+I will get a book
+
+conseguire un libro.
+---------------------------------------
+metas = goals;
+---------------------------------------
+I will get my goals
+
+conseguire mis metas.
+---------------------------------------
+---------------------------------------
+---------------------------------------
+---------------------------------------
+---------------------------------------
+---------------------------------------
+---------------------------------------
+---------------------------------------
+---------------------------------------
+---------------------------------------
+*/
