f578d8208fb4067895b9898055bd81fa2dd7b86d
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-18 04:05:28 -0400

    ingles yt | 18  B

--- ingles-yt/unidad-2/18.js
+++ ingles-yt/unidad-2/18.js
@@ -24,17 +24,42 @@ I'm eating some meat with vegetables and beans
 
 estoy comiendo algo de carne con verduras y frijoles.
 ---------------------------------------
+Do you remember the name of the book?
+
+Recuerdas el nombre del libro?
 ---------------------------------------
+Do you remember the name of the street?
+
+Recuerdas el nombre de la calle?
 ---------------------------------------
+Do you remember the name of my friend?
+
+Recuerdas el nombre de mi amigo?
 ---------------------------------------
+Mesa = table
+sobre/encima = on
+lentes = glasses
+pluma/lapiz = pen
+cuaderno = notebook
+donde = where
 ---------------------------------------
+Where are my glasses?
+
+Donde estan mis lentes?
 ---------------------------------------
+where is my pen?
+
+donde esta mi lapiz?
 ---------------------------------------
+where is my book and my notebook?
+
+donde esta mi libro y mi libreta?
 ---------------------------------------
+sentarse = sit down
+levantarse = get up
 ---------------------------------------
----------------------------------------
----------------------------------------
----------------------------------------
----------------------------------------
+hi simon, how are you, could you get up, please?
+
+Holla simon, como estas, podrias levantarte porfavor?
 ---------------------------------------
 */
