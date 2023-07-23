8ecdae16236a02ab4083d3ea17ca4796d565d010
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-23 19:35:05 -0400

    ingles yt | examen 2 B

---
+++ ingles-yt/unidad-2/test.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const fecha = new Date();
+
+console.log(fecha.getDate());
+console.log(fecha.getDay());
+
+// get day =
+// domingo = 0
+// lunes = 1 asta
+// sabado = 6
+
+console.log(fecha.getFullYear());
+console.log(fecha.getHours());
+console.log(fecha.getMinutes());
+console.log(fecha.getSeconds());
