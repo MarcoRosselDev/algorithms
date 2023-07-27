2db5853f06fa2e89de535d30d422988ed6bb29d0
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-26 22:29:29 -0400

    ingles yt | examen 2 E

--- ingles-yt/unidad-2/examen-2.js
+++ ingles-yt/unidad-2/examen-2.js
@@ -43,6 +43,46 @@ Juan se cayo desde el arbol.
 
 Juan fell from the tree
 -----------------------------------
+Paul necesita comprar algo?
+
+Does Paul need to buy something?
+-----------------------------------
+I got a new job
+
+Consegui un nuevo trabajo.
+-----------------------------------
+Buenos dias, disculpe.
+Donde consiguio su mesa?
+
+Good morning, scuse me.
+Where did you get your table?
+-----------------------------------
+Estoy seguro, conseguire mis metas este agnio.
+
+I'm sure, I will get my goals this year.
+-----------------------------------
+Could I get your number?
+
+Puedo obtener tu numero?
+-----------------------------------
+Ann and Alex will be my friends,
+but Ben and Emma won't
+
+Ann y Alex seran mis amigos,
+pero Ben y Emma no lo seran.
+-----------------------------------
+Alex manejo el auto desde casa desde esta magniana.
+
+Alex drove the car from home since this morning.
+-----------------------------------
+I walked with you since nineteen eighty-five, and I loved it.
+
+Camine contigo desde 1985, y me encanto. = B
+-----------------------------------
+Necesito el libro azul pero tu no.
+
+I need the blue book, but you don't = A
+-----------------------------------
 -----------------------------------
 -----------------------------------
 -----------------------------------
