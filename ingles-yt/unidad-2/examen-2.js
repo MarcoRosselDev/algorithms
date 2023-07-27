094b8bec10adb70cb56ec45f476cc38a4dc8f0ce
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-26 22:20:24 -0400

    ingles yt | examen 2 D

--- ingles-yt/unidad-2/examen-2.js
+++ ingles-yt/unidad-2/examen-2.js
@@ -1,4 +1,51 @@
 /* 9- 20
+traducir:
 
+Compré un helado
 
+I bought an ice cream.
+-----------------------------------
+She were my friend, but you was my sister.
+
+Ella era mi amiga, pero tú eras mi hermana.
+-----------------------------------
+Do they need to eat?
+
+Ellos/Ellas necesitan comer?
+-----------------------------------
+Kass tiene noventa vestidos.
+
+Kass has ninety dresses.
+-----------------------------------
+Kass tenía noventa vestidos.
+
+Kass had ninety dresses.
+-----------------------------------
+Kass tendrá noventa vestidos
+
+Kass will have ninety dresses.
+-----------------------------------
+Michael, the apple was on the box.
+Not under the box.
+
+Michael, la manzana estaba sobre la caja.
+No debajo de la caja.
+-----------------------------------
+? El cocino el pollo contigo?
+
+Does he cooked the chicken with you?
+-----------------------------------
+Ellos no comeran vegetales con pollo.
+
+They won't eat vegetable with chicken.
+-----------------------------------
+Juan se cayo desde el arbol.
+
+Juan fell from the tree
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
 */
