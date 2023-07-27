dca993cd6284e917a65a4557713423a50fb35753
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-26 22:10:53 -0400

    ingles yt | examen 2 C

--- ingles-yt/unidad-2/examen-2.js
+++ ingles-yt/unidad-2/examen-2.js
@@ -1,46 +1,4 @@
 /* 9- 20
 
-traducir:
 
--------------------------------
-compre un helado
-
-I bough a ice cream
-I did buy a ice cream
--------------------------------
-She were my friend, but you was my sister.
-
-                    , but you were my sister. <-- esta mal escrito.
-Ella era mi amiga, pero tu eras mi hermano/a.
--------------------------------
-Do they need to eat?
-
-Ellos necesitan comer?
--------------------------------
-kass tiene noventa vestidos
-kass has ninety dresses.
-
-kass tenia noventa vestidos.
-kass had ninety dresses.
-kass did have ninety dresses.
-
-kass tendra noventa vestidos.
-kass will have ninety dresses.
--------------------------------
-Michael, the apple was on the box. Not under the box.
-
-Michael, la manzana estaba sobre la caja, no debajo de la caja.
--------------------------------
--------------------------------
--------------------------------
--------------------------------
--------------------------------
--------------------------------
--------------------------------
--------------------------------
--------------------------------
--------------------------------
--------------------------------
--------------------------------
--------------------------------
 */
