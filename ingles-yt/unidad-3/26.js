0d7d0cb9c5e118ddac6e5fd851d8512baf81e028
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-28 18:18:29 -0400

    ingles yt | 25

---
+++ ingles-yt/unidad-3/26.js
@@ -0,0 +1,37 @@
+/* 26
+
+to  auxiliar/para   persona|lugar|verbo infinitivo
+
+for para/por        persona|verbo ing
+-----------------------------------
+grammatically correct
+grammarly correct
+
+para expresar verbos infinitivos, es obligatorio el (to),
+si es que preguntan en un examen.
+
+expresa bailar, comer, caminar
+
+to dance, to eat, to walk.
+
+don't = dance, eat, walk
+-----------------------------------
+pero para verbos gerundios (end with ing) no es necesario el (for)
+
+exprese bailando, comiendo, caminando.
+
+dancing, eating, walking
+
+don't = for dancing, for eating, for walking.
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
+-----------------------------------
+*/
