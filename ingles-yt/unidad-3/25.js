47902abaaf18e7c88792c27a0e4283d0fc225ab1
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-27 20:51:59 -0400

    ingles yt | 24 A

--- ingles-yt/unidad-3/25.js
+++ ingles-yt/unidad-3/25.js
@@ -1,5 +1,17 @@
 /* 25
 
+I'd like some wine
+Me gustaria un poco de vino.
 
+I would like some water
+Me gustaria un poco de agua.
 
+I would like a cup of coffee.
+Me gustaria una taza de cafe.
+
+Would you like some milk?
+Te gustaria un poco de leche?
+
+Would you like some grape juice?
+Te gusaria un poco de jugo de uva?
 */
