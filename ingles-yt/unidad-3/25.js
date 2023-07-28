fd0cbb6b250f8193103678d31350ac8c76d625ed
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-28 02:36:51 -0400

    ingles yt | 24 B

--- ingles-yt/unidad-3/25.js
+++ ingles-yt/unidad-3/25.js
@@ -1,17 +1,16 @@
 /* 25
-
-I'd like some wine
-Me gustaria un poco de vino.
-
-I would like some water
-Me gustaria un poco de agua.
-
-I would like a cup of coffee.
-Me gustaria una taza de cafe.
-
-Would you like some milk?
-Te gustaria un poco de leche?
-
-Would you like some grape juice?
-Te gusaria un poco de jugo de uva?
+I'm hungry = tengo hambre.
+I'm thirsty = Tengo sed
+I'm starving = Estoy muy hambriento.
+----------------------------------
+----------------------------------
+----------------------------------
+----------------------------------
+----------------------------------
+----------------------------------
+----------------------------------
+----------------------------------
+----------------------------------
+----------------------------------
+----------------------------------
 */
