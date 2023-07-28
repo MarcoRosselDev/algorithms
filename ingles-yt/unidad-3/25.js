03d09dd56d88b927d7260b2690d4c8cfd1cf9745
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-28 04:06:05 -0400

    ingles yt | 24 C

--- ingles-yt/unidad-3/25.js
+++ ingles-yt/unidad-3/25.js
@@ -3,7 +3,20 @@ I'm hungry = tengo hambre.
 I'm thirsty = Tengo sed
 I'm starving = Estoy muy hambriento.
 ----------------------------------
+countable/uncountable
+contables/incontables
+
+how many? countable
+
+how much? uncountable
 ----------------------------------
+How much money do you have?
+
+How many books do you have?
+
+How many carrots do you have?
+
+Hoe much rice do you have?
 ----------------------------------
 ----------------------------------
 ----------------------------------
