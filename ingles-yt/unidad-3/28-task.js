e895ee2e4a6fe5e6baa6aa0620897f310d7ac478
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-29 18:41:01 -0400

    ingles yt | 28  task

---
+++ ingles-yt/unidad-3/28-task.js
@@ -0,0 +1,19 @@
+/* traducir
+
+Miguel has walked all day.
+
+Miguel ha caminado todo el dia.
+-----------------------------
+Marco has been working all day.
+
+Marco ha estado trabajando todo el dia.
+-----------------------------
+Yo he estado durmiendo todo el dia.
+
+I have been sleeping all day.
+-----------------------------
+Ella ha estado comprando libros con mi hermana.
+
+She has been buying books with my sister.
+-----------------------------
+*/
