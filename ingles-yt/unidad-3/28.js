9296c192d206f67b969e71875f8774e314732f43
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-29 00:26:13 -0400

    ingles yt | 28

---
+++ ingles-yt/unidad-3/28.js
@@ -0,0 +1,52 @@
+/* 28
+
+comprar = buy
+
+I buy
+you buy
+he/she/it buys
+we buy
+they buy
+------------------------------------
+Yo compro manzanas rojas
+
+I buy red apples.
+------------------------------------
+Ella compra un vestido rosa.
+
+Ella buys a pink dress.
+------------------------------------
+I am = yo soy/yo estoy
+------------------------------------
+I'm buying a red book.
+
+Estoy comprando un libro rojo.
+------------------------------------
+I'm buying a black book, and my friend is reading a yellow book.
+
+Esoy comrando un libro negro, y mi amigo esta leyendo un libro amarillo.
+------------------------------------
+buy ---> bought (pasado de buy)
+------------------------------------
+Alejandro compro un vestido.
+
+Alejandro bought a dress.
+------------------------------------
+Marcela bought fourty apples, and fourteen potatoes.
+
+Marcela compro 40 manzanas, y 14 papas.
+------------------------------------
+El no compro catorce papas.
+
+I didn't buy fourteen potatos
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+*/
