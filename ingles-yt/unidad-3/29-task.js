1e15a212c9c80880b3917cef42157758b624ae85
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-30 01:12:24 -0400

    ingles yt | 29  task

---
+++ ingles-yt/unidad-3/29-task.js
@@ -0,0 +1,25 @@
+/* 29 task
+traducir
+-----------------------------------
+What is her name?
+
+Cual es su nombre?
+Cual es el nombre de ella?
+-----------------------------------
+What is our house?
+
+Cual es nuestra casa?
+-----------------------------------
+What is his book?
+
+Cual es el libro de el?
+-----------------------------------
+her money
+
+el dinero de ella.
+-----------------------------------
+their apples.
+
+las manzanas de ellos.
+-----------------------------------
+*/
