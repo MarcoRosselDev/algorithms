7aabc44a265e6be07f209453252af8a092673ed5
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-30 23:23:20 -0400

    ingles yt | 31 B

--- ingles-yt/unidad-3/31.js
+++ ingles-yt/unidad-3/31.js
@@ -21,7 +21,24 @@ Sólo para ella.
 
 Just for her.
 ----------------------------------------
+venir = come | came
 ----------------------------------------
+Ellas recien vinieron
+
+They just came.
 ----------------------------------------
+llegar = arrive | arrived
+----------------------------------------
+Ellos recien llegaron
+
+They just arrived
+----------------------------------------
+I'm only speaking to her not to you.
+
+Yo solo estoy ablandole a ella, no a ti.
+----------------------------------------
+Sólo quiero estar solo.
+
+I just want to be alone.
 ----------------------------------------
 */
