e9539a9a6fb904a722795a3fa1636fa097e0ef20
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-30 22:34:47 -0400

    ingles yt | 30 task

--- ingles-yt/unidad-3/30.js
+++ ingles-yt/unidad-3/30.js
@@ -6,14 +6,33 @@ a University --> lo mismo suna yu
 a piano.
 a tie.
 -----------------------------------------
-a shock = un calzetin
+a sock = un calzetin
 a ghost = fantasma
 an aireplane = aeropuerto
 an industry = industria
 a document = documento
 -----------------------------------------
+an Island       isla
+an Ear.         oreja/oido
+an umbrella     paraguas o sombrilla
+a book          libro.
+an almond.      almendra.
 -----------------------------------------
+a key = llave
+a test = prueba
+an exam = un examen
+an emotion = emocion
+an id = identificacion
 -----------------------------------------
+homework
+
+Scarlet bought a strawberry ice cream and 
+also bought a chocolate ice cream
+and ... a potato.
+
+Scarlet compro un helado de frambuesa y 
+tambien compro un helado de chocolate
+y una papa.
 -----------------------------------------
 -----------------------------------------
 -----------------------------------------
