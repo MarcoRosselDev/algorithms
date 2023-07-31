9c8132f0e387e47b5d021c26f4269b1d21cc701e
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-30 23:42:31 -0400

    ingles yt | 31 D

---
+++ ingles-yt/unidad-3/32.js
@@ -0,0 +1,28 @@
+/* 32
+hay; singular = there is
+hay; plural   = there are
+------------------------------------
+Hay una manzana
+
+There is an apple
+There's an apple.
+------------------------------------
+pertenencia
+mio o mia ====> mine
+tuyo o tuya ==> yours
+------------------------------------
+There is an apple on the table, but it's not mine.
+
+hay una manzana sobre la mesa, pero no es mia.
+------------------------------------
+There are two apples under the table
+
+hay dos manzanas bajo la mesa.
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+------------------------------------
+*/
