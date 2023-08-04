29db4298bd29db2ecab56a901826a791fc3e4224
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-03 23:57:28 -0400

    ingles yt | 33 C

--- ingles-yt/unidad-3/33.js
+++ ingles-yt/unidad-3/33.js
@@ -58,7 +58,58 @@ Habian manzanas sobre la mesa?
 
 Were there some apples on the table?
 ---------------------------------------------
+There weren't two apples under the table.
+
+No habian dos manzanas bajo la mesa.
 ---------------------------------------------
+Hay un muchacho en la cocina?
+
+is there a boy in the kitchen?
 ---------------------------------------------
+habia un muchacho en la cocina?
+
+was there a boy in the kitchen?
+---------------------------------------------
+Hay un arbol en el jardin?
+
+Is there a tree in the garden?
+---------------------------------------------
+habia un arbol en el jardin?
+
+was there a tree in the garden?
+---------------------------------------------
+habian manzanas sobre la mesa?
+
+were there some apples on the table?
+---------------------------------------------
+habia una buena pelicula en TV esta noche?
+
+Was there a good film on TV tonight?
+---------------------------------------------
+Solo habian 18 chicas en mi escuela.
+
+there were only eighteen girls in my school.
+---------------------------------------------
+habia 17 chicos en la escuela?
+
+Were there seventeen boys in the school?
+---------------------------------------------
+Solo habia algunas flores.
+
+there were just some flowers.
+---------------------------------------------
+Solo habia algunas flores rojas
+
+there were just some red flowers.
+---------------------------------------------
+room = habitacion
+---------------------------------------------
+solo habia algunas flores blancas en mi habitacion.
+
+there were just some white flowers in my room.
+---------------------------------------------
+there was a boy...
+
+habia un muchacho...
 ---------------------------------------------
 */
