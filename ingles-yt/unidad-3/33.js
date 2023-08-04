6af042af92b6d0431f27f4436c0d88259459fee4
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-03 23:15:35 -0400

    ingles yt | 33 B

--- ingles-yt/unidad-3/33.js
+++ ingles-yt/unidad-3/33.js
@@ -24,13 +24,39 @@ habian cinco arboles en el jardin.
 
 There were five trees in your garden.
 ---------------------------------------------
+preguntas
+
+Habian cinco arboles en el jardin?
+
+Were there five trees in your garden?
 ---------------------------------------------
+There were some big trees in my garden?
+
+habian cinco arboles grandes en mi jardin?
 ---------------------------------------------
+There weren't two apples under the table.
+
+No habian dos manzanas bajo la mesa.
 ---------------------------------------------
+hay un muchacho en la cocina?
+
+Is there a boy in the kitchen?
 ---------------------------------------------
+habia un muchacho en la cocina?
+
+was there a boy in the kitchen?
 ---------------------------------------------
+hay un arbol en mi jardin?
+
+Is there a tree in my garden?
 ---------------------------------------------
+habia un arbol en mi jardin?
+
+Was there a tree in my garden?
 ---------------------------------------------
+Habian manzanas sobre la mesa?
+
+Were there some apples on the table?
 ---------------------------------------------
 ---------------------------------------------
 ---------------------------------------------
