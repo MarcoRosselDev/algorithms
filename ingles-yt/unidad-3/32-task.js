3ca86d2b9c8480311ca46befb1c4e1fddb91314e
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-03 22:37:15 -0400

    ingles yt | 32 task

---
+++ ingles-yt/unidad-3/32-task.js
@@ -0,0 +1,18 @@
+/* 32 task
+hay seis papas sobre la mesa y ademas hay dos zanahorias bajo la mesa.
+
+There are six potatoes on the table, and also are two carrots under the table.
+There are six potatoes on the table, and there are also two carrots under the table.
+---------------------------------------------
+hay algunos arboles en tu casa?
+
+Are there any trees in your house?
+---------------------------------------------
+No hay libros en el jardin.
+
+There aren't books in the garden.
+---------------------------------------------
+Solo hay noventa y dos tomates.
+
+There are only ninety two tomatoes
+*/
