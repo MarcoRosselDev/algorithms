7eed96a2005d8da1fbba4ad5a51f0d19088f07dd
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-03 21:55:23 -0400

    ingles yt | 32 C

--- ingles-yt/unidad-3/32.js
+++ ingles-yt/unidad-3/32.js
@@ -31,7 +31,42 @@ There are some big trees in my garden.
 Hay algunos arboles grandes en mi jardin.
 ------------------------------------
 There are not two apples under the table.
+There aren't two apples under the table.
+
+No hay dos manzanas bajo la mesa.
 ------------------------------------
+Como preguntar?---------------------
+
+hay un muchacho en la cicina?
+
+Is there a boy in the kitchen?
 ------------------------------------
+hay muchachas en la cocina?
+
+Are there girls in the kitchen?
+------------------------------------
+hay un arbol en mi jardin?
+
+Is there a tree in my garden?
+------------------------------------
+hay manzanas sobre la mesa?
+
+Are there apples on the table?
+------------------------------------
+would you like to see a good film with me?
+
+Te gustaria ver una buena pelicula con migo?
+------------------------------------
+There's a good film on TV tonight?
+
+hay una buena pelicula en TV esta noche?
+------------------------------------
+Solo hay 18 chicas en mi escuela.
+
+There are just eighteen girls in my school.
+------------------------------------
+Solo hay algunas flore.
+
+There are just some flowers.
 ------------------------------------
 */
