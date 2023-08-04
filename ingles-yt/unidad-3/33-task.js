f126fff84e32e4c7e06f8735149ae6f06ff74010
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-04 00:06:57 -0400

    ingles yt | 33 task

---
+++ ingles-yt/unidad-3/33-task.js
@@ -0,0 +1,23 @@
+/* 33 task
+
+habia una chica en mi casa.
+
+there was a girl in my house.
+------------------------------------------
+habia unos chocolates en mi habitacion.
+
+There were some chocolates in my room.
+------------------------------------------
+traducir | positivo | negativo | pregunta
+------------------------------------------
+habia una chica en mi casa.
++   there was a girl in my house
+-   there wasn't a girl in my house.
+?   was there a girl in my house?
+------------------------------------------
+habian unos chocolates en mi habitacion
++   There were some chocolates in my room.
+-   There weren't some chocolates in my room.
+?   Were there some chocolates in my room?
+------------------------------------------
+*/
