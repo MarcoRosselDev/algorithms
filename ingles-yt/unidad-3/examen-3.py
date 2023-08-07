4e8c8d3c54c6125a8ca2b59934fcef43a1ce11c3
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-07 13:22:05 -0400

    ingles yt | examen 3 B

--- ingles-yt/unidad-3/examen-3.py
+++ ingles-yt/unidad-3/examen-3.py
@@ -25,11 +25,41 @@ a el no le gusta estudiar.
 
 he doesn't like to study.
 ------------------------------------
+a ella le gusta trabajar
+
+she likes to work
 ------------------------------------
+marco has been working all day
+
+marco a estado trabajando todo el dia.
 ------------------------------------
+la manzana es para el
+
+The apple is for him.
 ------------------------------------
+mi amigo esta comprando un libro negro, pero yo compre el libro amarillo la semana pasada.
+
+my friend is buying a black book, but I bought the yellow book last week.
 ------------------------------------
+ellos no compraron 14 manzanas, pero maria compro 18 platanos.
+
+They didn't buy fourteen apples, but maria bougth eighteen bananas.
 ------------------------------------
+alex a estado bailando con juan, pero yo he estado bailando con maria.
+
+Alex has been dancing with juan, but I have been dancing with maria.
+------------------------------------
+listening
+------------------------------------
+It was an apple on my beed
+------------------------------------
+Hello
+did you see a book on the table?
+I'm sure it was there.
+
+Hola
+viste un libro sobre la mesa?
+Estoy seguro de que estaba alli.
 ------------------------------------
 ------------------------------------
 """
