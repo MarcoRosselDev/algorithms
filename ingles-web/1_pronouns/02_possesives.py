883893f03f36a719c5730b852ac136413d4a4cba
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-08 18:10:13 -0400

    ingles web | 02 possesive  -2

--- ingles-web/1_pronouns/02_possesives.py
+++ ingles-web/1_pronouns/02_possesives.py
@@ -17,6 +17,46 @@ __________________________________________________
 her                         This is her dress
 su, de ella                 Este es su vestido
 __________________________________________________
+Its                         The dog doesn't like to be on its own.
+su                          El perro no le gusta estar solo
+__________________________________________________
+our                         these are our suitcases
+nuestra/o, de nosotros      Estas son nuestras maletas.
+__________________________________________________
+your                        these are your seats.
+vuestro/a, su, de ustedes   Estos son vuestros asientos.
+__________________________________________________
+their                       these are their books.
+su, de ellos                Estos son sus libros.
+__________________________________________________
+---Solo utilizamos el pronombre posesivo "its" con el adjetivo "own" 
+__________________________________________________
+Pronombres posesivos        ejemplos
+en funcion de pronombres
+
+mine                        this book is mine
+mio                         Este libro es mio.
+__________________________________________________
+yours                       Is this book yours?
+tuyo/s, suyo/s              Este libro es tuyo?/Este libro es suyo?
+__________________________________________________
+his                         this bicycle is his.
+suyo                        Esta bicicleta es de el
+__________________________________________________
+hers                        The dress is hers
+suyo/s                      El vestido es de ella.
+__________________________________________________
+its                         The house is its(the cat's)
+su/s                        La casa se suya(del gato)
+__________________________________________________
+ours                        The suitcases are ours
+nuestro/s                   Las maletas son nuestras
+__________________________________________________
+yours                       These seats are yours
+vuestro/s, suyo/s           Estos asientos son vuestros
+__________________________________________________
+theirs                      this pencil is theirs
+suyo/s                      Este lapiz es de ellos.
 __________________________________________________
 
 """
