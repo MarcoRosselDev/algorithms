9f483774fcd6f3f3aec73c2224603390cd1ca2fc
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-08 17:56:47 -0400

    ingles web | 02 possesive

---
+++ ingles-web/1_pronouns/02_possesives.py
@@ -0,0 +1,22 @@
+""" Possesives
+
+Los posesivos se clasifican en dos tipos segun su funcion en la oracion
+
+Pronombres posesivos        Ejemplo
+En funcion de determinantes
+
+my                          this is my house
+mi                          Esta es mi casa
+__________________________________________________
+your                        This is your book
+tu                          Este es tu libro/Este es su libro
+__________________________________________________
+his                         this is his bicycle
+su/de el                    Esta es su bicicleta
+__________________________________________________
+her                         This is her dress
+su, de ella                 Este es su vestido
+__________________________________________________
+__________________________________________________
+
+"""
