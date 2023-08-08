4415aa51752c54800c13fa5943f897b6079f7e5c
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-08 18:49:36 -0400

    ingles web | 02 possesive  - 3

--- ingles-web/1_pronouns/02_possesives.py
+++ ingles-web/1_pronouns/02_possesives.py
@@ -60,3 +60,91 @@ suyo/s                      Este lapiz es de ellos.
 __________________________________________________
 
 """
+
+""" Function
+
+1. En función de artículo determinativo del sujeto
+
+Gramaticalmente funciona como un artículo determinativo del sustantivo al que acompaña, por lo tanto siempre va antes del objeto.
+
+Ejemplos:
+
+
+My car is blue.
+(Mi coche es azul.)
+
+His house is big.
+(Su casa es grande.)
+2. En función de pronombre
+
+Gramaticalmente funciona como un pronombre en función de complemento directo. Siempre va al final de la oración.
+
+Ejemplos:
+
+The blue car is mine.
+(El coche azul es mío.)
+
+The big house is his.
+(La casa grande es suya.)
+Nota: Estos ejemplos responden a la pregunta, “¿De quién es?”
+
+Possessive Pronouns vs. the Genitive Case
+(Pronombres posesivos y el genitivo)
+El genitivo “-s” no se ha de confundir con los pronombres posesivos. Así podemos decir:
+
+Her dress…
+(Su vestido…[de ella])
+O podemos decir:
+
+Andrea’s dress…
+(El vestido de Andrea…)
+En ambos casos expresamos un poseedor y un poseído, en este caso el vestido. En la primera frase, el pronombre posesivo femenino “her” indica que tanto el hablante como el oyente acaban de hacer referencia a la misma persona. En cambio en el segundo caso, el hablante quiere dar a conocer el nombre del poseedor.
+
+Grammatical Rules (Reglas gramaticales)
+1. Siempre utilizamos el genitivo para referirnos a personas:
+
+Paul’s house…
+(La casa de Paul…)
+
+Mary’s bike…
+(La bicicleta de Maria…)
+2. Cuando nos referimos a cosas o lugares utilizaremos la preposición “of”:
+
+The wheel of the bike…
+(La rueda de la bicicleta…)
+
+Washington is the capital of the United States.
+(Washington es la capital de los Estados Unidos.)
+3. También podemos utilizar el genitivo al final de la oración que acostumbra a ser la respuesta a una pregunta anterior. En estos casos no necesitamos el nombre.
+
+Question (pregunta):
+
+Where is your sister?
+(¿Dónde está tu hermana?)
+Answer (repuesta):
+
+She is at my parents’.
+(Está en la casa de mis padres.)
+Question (pregunta):
+
+Whose house is this?
+(¿De quién es esta casa?)
+Answer (repuesta):
+
+It is Michael’s.
+(Es de Miguel.)
+4. Cuando hay más de un poseedor, el apóstrofo viene después de la “s”.
+
+Ejemplos:
+
+The girls’ toys….
+(los juguetes de las niñas…)
+
+The students’ exams…
+(los exámenes de los estudiantes…)
+WARNING!
+Cuando el nombre del poseedor termina en una “s”, el genitivo se añade a final del poseedor, pero sólo añadiendo el apóstrofo y no la “-s” del genitivo.
+
+Luis’ house…
+(La casa de Luis….)
+"""
