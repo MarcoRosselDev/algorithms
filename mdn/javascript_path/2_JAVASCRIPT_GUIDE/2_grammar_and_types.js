5084a8822398454676b1039df43b356c4cb56cc0
Marco Rossel <andresmarcorossel@gmail.com>
2026-04-15 05:45:39 -0400

    js guide, grammar and types

---
+++ mdn/javascript_path/2_JAVASCRIPT_GUIDE/2_grammar_and_types.js
@@ -0,0 +1,17 @@
+(function () {
+  console.log("another phras");
+  
+  /* comments 
+  in blocks.
+  2. we can scape the character *\/ with this \ sign.
+  3. let x is a declaration variable
+  4. = 33 is the asignation variable
+  5. before that asignation the value of the variable x was undefined
+  6. the variable declared with const can not be declared without assign a value
+      if you do that, there will be a sintax error.
+  7. var is a variable that can be accesed trought all the scope in the document, so be carrefull.
+  */
+  let x = 33;
+
+  
+})()
