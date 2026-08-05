5817836b8c4109eb8ffaf3aacf12f194af060ef4
Marco Rossel <andresmarcorossel@gmail.com>
2026-08-04 21:16:24 -0400

    fcc js 2026 | objects

---
+++ fcc_26/objetos/Object has own .js
@@ -0,0 +1,14 @@
+let obj = {
+  name: "Marco",
+  age: 33,
+  "civil state": "noviasgo"
+}
+
+const a = Object.hasOwn(obj, "name") // true
+const b = Object.hasOwn(obj, "age") // true
+const c = Object.hasOwn(obj, "favorite color") // false
+const d = Object.hasOwn(obj, "civil state") // true
+
+console.log(a,b,c,d);
+
+// Los certificados de avalúo fiscal disponibles para la propiedad ubicada en CALLE NUEVA 522 VILLA VICTORIA de la comuna de SAN PEDRO DE LA PAZ, cuyo N° de Rol de Avalúo es 16854-00022 se despliegan a continuación; para lo cual seleccione el tipo de certificado que usted desee obtener. 
